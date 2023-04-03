f328120b41d77d9e68fb897d4f3228d2431fc8ee
Daniel <dwmcabinets@yahoo.com>
2023-04-04 07:23:06 +0800

    updated version number added command pyrxver to show the version

--- PyRx/acrxEntryPoint.cpp
+++ PyRx/acrxEntryPoint.cpp
@@ -112,7 +112,7 @@ public:
     {
         constexpr TCHAR MAJOR1 = '1';
         constexpr TCHAR MINOR1 = '1';
-        constexpr TCHAR REVISION1 = '1', REVISION2 = '0', REVISION3 = '2';
+        constexpr TCHAR REVISION1 = '1', REVISION2 = '0', REVISION3 = '3';
 
         constexpr unsigned int compileYear = (__DATE__[7] - '0') * 1000 + (__DATE__[8] - '0') * 100 + (__DATE__[9] - '0') * 10 + (__DATE__[10] - '0');
         constexpr unsigned int compileMonth = (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? 1 : ((__DATE__[2] == 'n') ? 6 : 7))    // Jan, Jun or Jul
@@ -327,6 +327,11 @@ public:
         }
     }
 
+    static void AcRxPyApp_pyrxver(void)
+    {
+        PRINTVER();
+    }
+
     static void AcRxPyApp_pyfunc(void)
     {
         PyErr_Clear();
@@ -362,3 +367,4 @@ public:
 IMPLEMENT_ARX_ENTRYPOINT(AcRxPyApp)
 ACED_ARXCOMMAND_ENTRY_AUTO(AcRxPyApp, AcRxPyApp, _pyload, pyload, ACRX_CMD_TRANSPARENT, NULL)
 ACED_ARXCOMMAND_ENTRY_AUTO(AcRxPyApp, AcRxPyApp, _pyreload, pyreload, ACRX_CMD_TRANSPARENT, NULL)
+ACED_ARXCOMMAND_ENTRY_AUTO(AcRxPyApp, AcRxPyApp, _pyrxver, pyrxver, ACRX_CMD_TRANSPARENT, NULL)
