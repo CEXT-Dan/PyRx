14e704169ee4b239fe1a822dafd6c74f9179d851
Daniel <dwmcabinets@yahoo.com>
2023-04-03 16:36:55 +0800

    insert block sample

---
+++ PySamples/insertblock.py
@@ -0,0 +1,36 @@
+
+import os
+
+import PyRxApp# = all the global methods like acutPrintf, 
+import PyRx# = Runtime runtime 
+import PyGe# = Geometry
+import PyGi# = Graphics interface
+import PyDb# = database
+import PyAp# = application, document classes services 
+import PyEd# = editor 
+
+
+def OnPyInitApp():
+    print("\nOnPyInitApp")
+	
+def OnPyUnloadApp():
+    print("\nOnPyUnloadApp")
+        
+def OnPyLoadDwg():
+    print("\nOnPyLoadDwg")
+	
+def OnPyUnloadDwg():
+    print("\nOnPyUnloadDwg")
+         
+def PyRxCmd_pyinsert():
+    try: 
+        db = PyDb.DbHostApplicationServices().workingDatabase()
+        db2 = PyDb.DbDatabase(False, False)
+        if db2.readDwgFile("./dwg/18X36RP.dwg") != PyDb.ErrorStatus.Ok:
+             print("fail")
+        oid = PyDb.DbObjectId()
+        if db.insert(oid,"WOOHOO", db2, True) != PyDb.ErrorStatus.Ok:
+              print("fail")
+
+    except Exception as err:
+        print(err)
