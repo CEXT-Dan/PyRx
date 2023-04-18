4b23e5fed21fdadd1a5c5fa094c4420778facfa6
Daniel <dwmcabinets@yahoo.com>
2023-04-19 07:33:01 +0800

    added quick test

---
+++ PySamples/testPyMtext.py
@@ -0,0 +1,23 @@
+import os
+
+import PyRxApp# = all the global methods like acutPrintf,
+import PyRx# = Runtime runtime
+import PyGe# = Geometry
+import PyGi# = Graphics interface
+import PyDb# = database
+import PyAp# = application, document classes services
+import PyEd# = editor
+
+def PyRxCmd_pycreate_mtext():
+    try: 
+        db = PyAp.Application().docManager().curDocument().database()
+        model = PyDb.BlockTableRecord(db.modelSpaceId(), PyDb.OpenMode.kForWrite)
+        mtext = PyDb.MText()
+        mtext.setLocation(PyGe.Point3d(100,100,0))
+        mtext.setContents("This is a test")
+        print(mtext.contents())
+        print(mtext.textHeight())
+        mtext.setRotation(1.5708)
+        model.appendAcDbEntity(mtext)
+    except Exception as err:
+        PyRxApp.Printf(err)
