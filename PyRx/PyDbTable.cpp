b9365f7c568d7126b925384004a40feea47414b1
Daniel <dwmcabinets@yahoo.com>
2023-04-19 16:39:35 +0800

    PyDbTable progress

--- PyRx/PyDbTable.cpp
+++ PyRx/PyDbTable.cpp
@@ -1,2 +1,366 @@
 #include "stdafx.h"
 #include "PyDbTable.h"
+#include "PyDbObjectId.h"
+
+PyDbTable::PyDbTable()
+    : PyDbTable(new AcDbTable(),true)
+{
+}
+
+PyDbTable::PyDbTable(AcDbTable* ptr, bool autoDelete)
+    : PyDbBlockReference(ptr, autoDelete)
+{
+}
+
+PyDbTable::PyDbTable(const PyDbObjectId& id, AcDb::OpenMode mode)
+{
+    AcDbTable* pobj = nullptr;
+    if (auto es = acdbOpenObject<AcDbTable>(pobj, id.m_id, mode); es != eOk)
+        throw PyAcadErrorStatus(es);
+    this->resetImp(pobj, false, true);
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+}
+
+PyDbObjectId PyDbTable::tableStyle() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return PyDbObjectId(imp->tableStyle());
+}
+
+Acad::ErrorStatus PyDbTable::setTableStyle(const PyDbObjectId& id)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setTableStyle(id.m_id);
+}
+
+AcGeVector3d PyDbTable::direction() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->direction();
+}
+
+Acad::ErrorStatus PyDbTable::setDirection(const AcGeVector3d& horzVec)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setDirection(horzVec);
+}
+
+Adesk::UInt32 PyDbTable::numRows() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->numRows();
+}
+
+Adesk::UInt32 PyDbTable::numColumns() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->numColumns();
+}
+
+double PyDbTable::width() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->width();
+}
+
+Acad::ErrorStatus PyDbTable::setWidth(double width)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setWidth(width);
+}
+
+double PyDbTable::columnWidth(int col) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->columnWidth(col);
+}
+
+Acad::ErrorStatus PyDbTable::setColumnWidth1(int col, double width)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setColumnWidth(col, width);
+}
+
+Acad::ErrorStatus PyDbTable::setColumnWidth2(double width)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setColumnWidth(width);
+}
+
+double PyDbTable::height() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->height();
+}
+
+Acad::ErrorStatus PyDbTable::setHeight(double height)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setHeight(height);
+}
+
+double PyDbTable::rowHeight(int row) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->rowHeight(row);
+}
+
+Acad::ErrorStatus PyDbTable::setRowHeight1(int row, double height)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setRowHeight(row, height);
+}
+
+Acad::ErrorStatus PyDbTable::setRowHeight2(double height)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setRowHeight(height);
+}
+
+double PyDbTable::minimumColumnWidth(int col) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->minimumColumnWidth(col);
+}
+
+double PyDbTable::minimumRowHeight(int row) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->minimumColumnWidth(row);
+}
+
+double PyDbTable::minimumTableWidth() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->minimumTableWidth();
+}
+
+double PyDbTable::minimumTableHeight() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->minimumTableHeight();
+}
+
+double PyDbTable::horzCellMargin() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->horzCellMargin();
+}
+
+Acad::ErrorStatus PyDbTable::setHorzCellMargin(double gap)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setHorzCellMargin(gap);
+}
+
+double PyDbTable::vertCellMargin() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->vertCellMargin();
+}
+
+Acad::ErrorStatus PyDbTable::setVertCellMargin(double gap)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setVertCellMargin(gap);
+}
+
+AcDb::FlowDirection PyDbTable::flowDirection() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->flowDirection();
+}
+
+Acad::ErrorStatus PyDbTable::setFlowDirection(AcDb::FlowDirection flow)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setFlowDirection(flow);
+}
+
+bool PyDbTable::isTitleSuppressed() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->isTitleSuppressed();
+}
+
+Acad::ErrorStatus PyDbTable::suppressTitleRow(bool value)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->suppressTitleRow(value);
+}
+
+bool PyDbTable::isHeaderSuppressed() const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->isHeaderSuppressed();
+}
+
+Acad::ErrorStatus PyDbTable::suppressHeaderRow(bool value)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->suppressHeaderRow(value);
+}
+
+AcDb::CellAlignment PyDbTable::alignment(AcDb::RowType type) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->alignment(type);
+}
+
+Acad::ErrorStatus PyDbTable::setAlignment(AcDb::CellAlignment align, AcDb::RowType rowTypes)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setAlignment(align, rowTypes);
+}
+
+bool PyDbTable::isBackgroundColorNone(AcDb::RowType type) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->isBackgroundColorNone(type);
+}
+
+Acad::ErrorStatus PyDbTable::setBackgroundColorNone(bool value, AcDb::RowType type)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setBackgroundColorNone(value,type);
+}
+
+AcCmColor PyDbTable::backgroundColor(AcDb::RowType type) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->backgroundColor(type);
+}
+
+Acad::ErrorStatus PyDbTable::setBackgroundColor(const AcCmColor& color, AcDb::RowType type)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setBackgroundColor(color,type);
+}
+
+AcCmColor PyDbTable::contentColor(AcDb::RowType type) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->contentColor(type);
+}
+
+Acad::ErrorStatus PyDbTable::setContentColor(const AcCmColor& color, AcDb::RowType type)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setContentColor(color, type);
+}
+
+boost::python::tuple PyDbTable::getDataType(AcDb::RowType type) const
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    AcValue::DataType nDataType;
+    AcValue::UnitType nUnitType;
+    if (auto es = imp->getDataType(nDataType, nUnitType, type); es != eOk)
+        throw PyAcadErrorStatus(es);
+    return boost::python::make_tuple(nDataType, nUnitType);
+}
+
+Acad::ErrorStatus PyDbTable::setDataType1(AcValue::DataType nDataType, AcValue::UnitType nUnitType)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setDataType(nDataType, nUnitType);
+}
+
+Acad::ErrorStatus PyDbTable::setDataType2(AcValue::DataType nDataType, AcValue::UnitType nUnitType, AcDb::RowType type)
+{
+    auto imp = impObj();
+    if (imp == nullptr)
+        throw PyNullObject();
+    return imp->setDataType(nDataType, nUnitType, type);
+}
+
+std::string PyDbTable::className()
+{
+    return "AcDbTable";
+}
+
+AcDbTable* PyDbTable::impObj() const
+{
+    return static_cast<AcDbTable*>(m_pImp.get());
+}
--- PyRx/PyDbTable.h
+++ PyRx/PyDbTable.h
@@ -1,5 +1,75 @@
 #pragma once
-class PyDbTable
+#include "PyDbEnts.h"
+
+class PyDbTable : public PyDbBlockReference
 {
+public:
+    PyDbTable();
+    PyDbTable(AcDbTable* ptr, bool autoDelete);
+    PyDbTable(const PyDbObjectId& id, AcDb::OpenMode mode);
+
+    PyDbObjectId        tableStyle() const;
+    Acad::ErrorStatus   setTableStyle(const PyDbObjectId& id);
+
+    AcGeVector3d        direction() const;
+    Acad::ErrorStatus   setDirection(const AcGeVector3d& horzVec);
+
+    Adesk::UInt32  numRows() const;
+    Adesk::UInt32  numColumns() const;
+
+    double              width() const;
+    Acad::ErrorStatus   setWidth(double width);
+
+    double              columnWidth(int col) const;
+    Acad::ErrorStatus   setColumnWidth1(int col, double width);
+    Acad::ErrorStatus   setColumnWidth2(double width);
+
+    double              height() const;
+    Acad::ErrorStatus   setHeight(double height);
+
+    double              rowHeight(int row) const;
+    Acad::ErrorStatus   setRowHeight1(int row, double height);
+    Acad::ErrorStatus   setRowHeight2(double height);
+
+    double              minimumColumnWidth(int col) const;
+    double              minimumRowHeight(int row) const;
+    double              minimumTableWidth() const;
+    double              minimumTableHeight() const;
+
+    double horzCellMargin() const;
+    Acad::ErrorStatus setHorzCellMargin(double gap);
+
+    virtual double vertCellMargin() const;
+    virtual Acad::ErrorStatus setVertCellMargin(double gap);
+
+    AcDb::FlowDirection flowDirection() const;
+    Acad::ErrorStatus   setFlowDirection(AcDb::FlowDirection flow);
+
+    bool isTitleSuppressed() const;
+    Acad::ErrorStatus suppressTitleRow(bool value);
+
+    bool isHeaderSuppressed() const;
+    Acad::ErrorStatus suppressHeaderRow(bool value);
+
+    AcDb::CellAlignment alignment(AcDb::RowType type) const;
+    Acad::ErrorStatus   setAlignment(AcDb::CellAlignment align, AcDb::RowType rowTypes);
+
+    bool isBackgroundColorNone(AcDb::RowType type) const;
+    Acad::ErrorStatus setBackgroundColorNone(bool value, AcDb::RowType type);
+
+    AcCmColor backgroundColor(AcDb::RowType type) const;
+    Acad::ErrorStatus setBackgroundColor(const AcCmColor& color, AcDb::RowType type);
+
+    AcCmColor contentColor(AcDb::RowType type) const;
+    Acad::ErrorStatus setContentColor(const AcCmColor& color, AcDb::RowType type);
+
+    boost::python::tuple getDataType(AcDb::RowType type) const;
+    Acad::ErrorStatus setDataType1(AcValue::DataType nDataType, AcValue::UnitType nUnitType);
+    Acad::ErrorStatus setDataType2(AcValue::DataType nDataType, AcValue::UnitType nUnitType, AcDb::RowType type);
+
+
+    static std::string className();
+public:
+    AcDbTable* impObj() const;
 };
 
