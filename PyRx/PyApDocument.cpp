c18692806bd8cc108cfddad59ffd5f242dc0a095
Daniel <dwmcabinets@yahoo.com>
2023-05-02 15:09:51 +0800

    added more PyApDocument methods

--- PyRx/PyApDocument.cpp
+++ PyRx/PyApDocument.cpp
@@ -11,6 +11,21 @@ void makeAcApDocumentWrapper()
         .def("className", &PyApDocument::className).staticmethod("className")
         .def("database", &PyApDocument::database)
         .def("editor", &PyApDocument::editor)
+        .def("fileName", &PyApDocument::fileName)
+        .def("formatForSave", &PyApDocument::formatForSave)
+        .def("lockMode", &PyApDocument::lockMode1)
+        .def("lockMode", &PyApDocument::lockMode2)
+        .def("myLockMode", &PyApDocument::myLockMode)
+        .def("isQuiescent", &PyApDocument::isQuiescent)
+        .def("docTitle", &PyApDocument::docTitle)
+        .def("setDocTitle", &PyApDocument::setDocTitle)
+        .def("getCountOfLispList", &PyApDocument::getCountOfLispList)
+        .def("getItemOfLispList", &PyApDocument::getItemOfLispList)
+        .def("isCommandInputInactive", &PyApDocument::isCommandInputInactive)
+        .def("pushDbmod", &PyApDocument::pushDbmod)
+        .def("popDbmod", &PyApDocument::popDbmod)
+        .def("upgradeDocOpen", &PyApDocument::upgradeDocOpen)
+        .def("downgradeDocOpen", &PyApDocument::downgradeDocOpen)
         ;
 }
 
@@ -21,12 +36,92 @@ PyApDocument::PyApDocument(AcApDocument* ptr, bool autoDelete)
 {
 }
 
+std::string PyApDocument::fileName() const
+{
+   return wstr_to_utf8(impObj()->fileName());
+}
+
 PyDbDatabase PyApDocument::database() const
 {
-    auto imp = impObj();
-    if (imp != nullptr)
-        return PyDbDatabase(imp->database());
-    throw PyNullObject();
+    return PyDbDatabase(impObj()->database());
+}
+
+AcApDocument::SaveFormat PyApDocument::formatForSave() const
+{
+   return impObj()->formatForSave();
+}
+
+AcAp::DocLockMode PyApDocument::lockMode1() const
+{
+    return impObj()->lockMode();
+}
+
+AcAp::DocLockMode PyApDocument::lockMode2(bool bIncludeMyLocks) const
+{
+    return impObj()->lockMode(bIncludeMyLocks);
+}
+
+AcAp::DocLockMode PyApDocument::myLockMode() const
+{
+    return impObj()->myLockMode();
+}
+
+bool PyApDocument::isQuiescent() const
+{
+    return impObj()->isQuiescent();
+}
+
+std::string PyApDocument::docTitle() const
+{
+    return wstr_to_utf8(impObj()->docTitle());
+}
+
+void PyApDocument::setDocTitle(const std::string& title)
+{
+    impObj()->setDocTitle(utf8_to_wstr(title).c_str());
+}
+
+bool PyApDocument::isReadOnly() const
+{
+    return impObj()->isReadOnly();
+}
+
+int PyApDocument::getCountOfLispList() const
+{
+    return impObj()->GetCountOfLispList();
+}
+
+boost::python::tuple PyApDocument::getItemOfLispList(int nIndex) const
+{
+    AcLispAppInfo* info = impObj()->GetItemOfLispList(nIndex);
+    if(info == nullptr)
+        throw PyNullObject();
+    return boost::python::make_tuple(wstr_to_utf8(info->appFileName), info->bUnloadable);
+}
+
+bool PyApDocument::isCommandInputInactive() const
+{
+    return impObj()->isCommandInputInactive();
+}
+
+void PyApDocument::pushDbmod()
+{
+    return impObj()->pushDbmod();
+}
+
+Acad::ErrorStatus PyApDocument::popDbmod()
+{
+    return impObj()->popDbmod();
+}
+
+Acad::ErrorStatus PyApDocument::upgradeDocOpen()
+{
+    return impObj()->upgradeDocOpen();
+}
+
+Acad::ErrorStatus PyApDocument::downgradeDocOpen(bool bPromptForSave)
+{
+    return impObj()->downgradeDocOpen(bPromptForSave);
 }
 
 PyAcEditor PyApDocument::editor()
--- PyRx/PyApDocument.h
+++ PyRx/PyApDocument.h
@@ -13,8 +13,32 @@ class PyApDocument : public PyRxObject
 public:
     PyApDocument(AcApDocument* ptr, bool autoDelete);
     virtual ~PyApDocument() override = default;
-    PyDbDatabase database() const;
     PyAcEditor editor();
+    PyDbDatabase database() const;
+    std::string fileName() const;
+    AcApDocument::SaveFormat formatForSave() const;
+
+    AcAp::DocLockMode lockMode1() const;
+    AcAp::DocLockMode lockMode2(bool bIncludeMyLocks) const;
+    AcAp::DocLockMode myLockMode() const;
+    bool    isQuiescent() const;
+
+    std::string docTitle() const;
+    void setDocTitle(const std::string& title);
+
+    bool isReadOnly() const;
+
+    int getCountOfLispList() const;
+    boost::python::tuple getItemOfLispList(int nIndex) const;
+
+    bool isCommandInputInactive() const;
+
+    void              pushDbmod();
+    Acad::ErrorStatus popDbmod();
+
+    Acad::ErrorStatus upgradeDocOpen();
+    Acad::ErrorStatus downgradeDocOpen(bool bPromptForSave);
+
     static std::string className();
 public:
     AcApDocument* impObj() const;
