5c8a13e49376fac8288fab8b0c80d391d67b1910
Daniel <dwmcabinets@yahoo.com>
2023-05-08 15:50:40 +0800

    updated sample

--- PySamples/Reactors/DocManagerReactor.py
+++ PySamples/Reactors/DocManagerReactor.py
@@ -1,60 +1,74 @@
 
-import PyRxApp 
+import PyRxApp
 import PyRx
 import PyAp
 
-inst = None #global scope
+inst = None  # global scope
+
+
+#Check if the doc is null, it can happen, i.e. on the start tab
+
 
 def OnPyInitApp():
   global inst
   inst = DocReactor()
 
-#TODO check doc.isNull()
 
 class DocReactor(PyAp.DocManagerReactor):
     def __init__(self):
         PyAp.DocManagerReactor.__init__(self)
 
     def documentCreateStarted(self, dwgdoc):
-        print("\ndocumentCreateStarted-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentCreateStarted-{}".format(dwgdoc.docTitle()))
 
     def documentCreated(self, dwgdoc):
-        print("\ndocumentCreated-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentCreated-{}".format(dwgdoc.docTitle()))
 
     def documentToBeDestroyed(self, dwgdoc):
-        print("\ndocumentToBeDestroyed-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentToBeDestroyed-{}".format(dwgdoc.docTitle()))
 
     def documentDestroyed(self, filename):
         print("\ndocumentDestroyed-{}".format(filename))
 
     def documentCreateCanceled(self, dwgdoc):
-        print("\ndocumentCreateCanceled-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentCreateCanceled-{}".format(dwgdoc.docTitle()))
 
     def documentLockModeWillChange(self, dwgdoc, myCurrentMode, myNewMode, currentMode, pGlobalCmdName):
-        print("\ndocumentLockModeWillChange-{} {} {} {} {}".format(dwgdoc.docTitle(),
-              myCurrentMode, myNewMode, currentMode, pGlobalCmdName))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentLockModeWillChange-{} {} {} {} {}".format(dwgdoc.docTitle(),
+                myCurrentMode, myNewMode, currentMode, pGlobalCmdName))
 
     def documentLockModeChangeVetoed(self, dwgdoc, pGlobalCmdName):
-        print("\ndocumentLockModeChangeVetoed-{} {}".format(dwgdoc.docTitle(), pGlobalCmdName))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentLockModeChangeVetoed-{} {}".format(dwgdoc.docTitle(), pGlobalCmdName))
 
     def documentLockModeChanged(self, dwgdoc, myPreviousMode, myCurrentMode, currentMode, pGlobalCmdName):
-        print("\ndocumentLockModeChanged-{} {} {} {} {}".format(dwgdoc.docTitle(),
+        if dwgdoc.isNull() == False:
+            print("\ndocumentLockModeChanged-{} {} {} {} {}".format(dwgdoc.docTitle(),
               myCurrentMode, myPreviousMode, currentMode, pGlobalCmdName))
 
     def documentBecameCurrent(self, dwgdoc):
-        print("\ndocumentBecameCurrent-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentBecameCurrent-{}".format(dwgdoc.docTitle()))
 
     def documentToBeActivated(self, dwgdoc):
-        print("\ndocumentToBeActivated-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentToBeActivated-{}".format(dwgdoc.docTitle()))
 
     def documentToBeDeactivated(self, dwgdoc):
-        print("\ndocumentToBeDeactivated-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentToBeDeactivated-{}".format(dwgdoc.docTitle()))
 
     def documentActivationModified(self, bActivation):
         print("\ndocumentToBeDeactivated-{}".format(bActivation))
 
     def documentToBeDeactivated(self, dwgdoc):
-        print("\ndocumentToBeDeactivated-{}".format(dwgdoc.docTitle()))
+        if dwgdoc.isNull() == False:
+            print("\ndocumentToBeDeactivated-{}".format(dwgdoc.docTitle()))
 
 def PyRxCmd_pyregdocreactor():
     try:
