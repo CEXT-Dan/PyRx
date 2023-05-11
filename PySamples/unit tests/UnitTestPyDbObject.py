86a1383f79009723305f3fe2353ea0c9cd8936fc
Daniel <dwmcabinets@yahoo.com>
2023-05-11 19:21:50 +0800

    added more tests

--- PySamples/unit tests/UnitTestPyDbObject.py
+++ PySamples/unit tests/UnitTestPyDbObject.py
@@ -32,6 +32,20 @@ class TestDbObject(unittest.TestCase):
                 #
                 self.assertEqual(bdo.close(), PyDb.ErrorStatus.Ok)  
                 self.assertEqual(bdo.isReadEnabled(), False)
+                
+        def test_propertys(self):
+                db = PyDb.HostApplicationServices().workingDatabase()
+                model = PyDb.BlockTableRecord(db.modelSpaceId(),PyDb.OpenMode.ForRead)
+                oid = model.ownerId()
+                dbo = PyDb.DbObject(oid, PyDb.OpenMode.ForRead)
+                self.assertEqual(dbo.isA().name(),"AcDbBlockTable") 
+                self.assertEqual(dbo.database(),db) 
+                self.assertEqual(dbo.isAProxy(),False) 
+                self.assertEqual(dbo.isErased(),False)
+                dbo.disableUndoRecording(True)
+                self.assertEqual(dbo.isUndoRecordingDisabled(),True)
+                dbo.disableUndoRecording(False)
+                self.assertEqual(dbo.isUndoRecordingDisabled(),False)
 
         def test_xdata(self):
                 #regapp
@@ -44,20 +58,21 @@ class TestDbObject(unittest.TestCase):
                 self.assertEqual(dbo.isWriteEnabled(), True) 
                 xd = [(PyDb.DxfCode.kDxfRegAppName, "PYTHONTEST"),(PyDb.DxfCode.kDxfXdXCoord, PyGe.Point3d(1,10,100))]
                 dbo.setXData(xd)
-
+                
                 #get
                 xdres = dbo.xData("PYTHONTEST")
                 p = xdres[1][1]
                 self.assertEqual(p.x, 1)
                 self.assertEqual(p.y, 10)
                 self.assertEqual(p.z, 100)
+                
+                
                
 
 
 def PyRxCmd_pydbobject():
         try:
                 suite = unittest.TestLoader().loadTestsFromTestCase(TestDbObject)
-                print('TestDbObject')
                 print(unittest.TextTestRunner(verbosity=0).run(suite))
         except Exception as err:
                 PyRxApp.Printf(err)
