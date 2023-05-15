4fb5122e7bba1923a5e05aa9762e78580ab1fc01
Daniel <dwmcabinets@yahoo.com>
2023-05-15 17:25:24 +0800

    Added global methods

--- PyRx/DbCore.h
+++ PyRx/DbCore.h
@@ -9,11 +9,15 @@ void makeDbCoreWrapper();
 class DbCore
 {
 public:
+    static bool entDel(const PyDbObjectId& id);
     static boost::python::list entGet(const PyDbObjectId& id);
-    static int entMod(const boost::python::list& list);
+    static PyDbObjectId entLast();
+    static bool entMod(const boost::python::list& list);
+    static PyDbObjectId entNext(const PyDbObjectId& id);
+    static bool entUpd(const PyDbObjectId& id);
     static PyDbObject openDbObject(const PyDbObjectId& id, AcDb::OpenMode mode);
     static PyDbEntity openDbEntity(const PyDbObjectId& id, AcDb::OpenMode mode);
-    static int regApp(const std::string& app);
+    static bool regApp(const std::string& app);
     static Acad::ErrorStatus updateDimension(const PyDbObjectId& id);
 };
 
