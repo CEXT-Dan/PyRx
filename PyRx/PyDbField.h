ec5a3ed22bf0883d3bba5a40b41ff0b70d06d65e
Daniel <dwmcabinets@yahoo.com>
2023-05-21 14:50:07 +0800

    improve PyDbField.. added another constructor

--- PyRx/PyDbField.h
+++ PyRx/PyDbField.h
@@ -8,6 +8,7 @@ class PyDbField : public PyDbObject
 {
 public:
     PyDbField();
+    PyDbField(const std::string& pszFieldCode);
     PyDbField(const std::string& pszFieldCode, bool bTextField);
     PyDbField(AcDbField* ptr, bool autoDelete);
     PyDbField(const PyDbObjectId& id, AcDb::OpenMode mode);
