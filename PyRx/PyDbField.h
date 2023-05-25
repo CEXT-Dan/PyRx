cdd8efcd5f32daab733961adcf9738f43f451b72
Daniel <dwmcabinets@yahoo.com>
2023-05-25 12:23:58 +0800

    added  static PyDbField    cloneFrom(PyRxObject& src); format code

--- PyRx/PyDbField.h
+++ PyRx/PyDbField.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "PyDbObject.h"
-
 void makeAcDbFieldtWrapper();
 //---------------------------------------------------------------------------------------- -
 //PyDbField
@@ -13,34 +12,26 @@ public:
     PyDbField(AcDbField* ptr, bool autoDelete);
     PyDbField(const PyDbObjectId& id, AcDb::OpenMode mode);
     virtual ~PyDbField() override = default;
-
-    Acad::ErrorStatus setInObject(PyDbObject& pObj,const std::string& pszPropName);
-    Acad::ErrorStatus postInDatabase(PyDbDatabase& pDb);
-    AcDbField::State state(void) const;
-
+    Acad::ErrorStatus   setInObject(PyDbObject& pObj,const std::string& pszPropName);
+    Acad::ErrorStatus   postInDatabase(PyDbDatabase& pDb);
+    AcDbField::State    state(void) const;
     AcDbField::EvalStatus evaluationStatus() const;
     AcDbField::EvalOption evaluationOption(void) const;
-    Acad::ErrorStatus setEvaluationOption(AcDbField::EvalOption nEvalOption);
-
-    std::string evaluatorId(void) const;
-    Acad::ErrorStatus setEvaluatorId(const std::string& pszEvaluatorId);
-
-    bool            isTextField(void) const;
-    Acad::ErrorStatus convertToTextField(void);
-
-    int  childCount(void) const;
-    std::string getFormat(void) const;
-    Acad::ErrorStatus setFormat(const std::string& pszFormat);
-
-    std::string getValue(void) const;
-
-	Acad::ErrorStatus evaluate1();
-    Acad::ErrorStatus evaluate2(AcDbField::EvalContext nContext);
-    Acad::ErrorStatus evaluate3(AcDbField::EvalContext nContext, PyDbDatabase& db);
-
-    static std::string className();
-    static PyRxClass desc();
+    Acad::ErrorStatus   setEvaluationOption(AcDbField::EvalOption nEvalOption);
+    std::string         evaluatorId(void) const;
+    Acad::ErrorStatus   setEvaluatorId(const std::string& pszEvaluatorId);
+    bool                isTextField(void) const;
+    Acad::ErrorStatus   convertToTextField(void);
+    int                 childCount(void) const;
+    std::string         getFormat(void) const;
+    Acad::ErrorStatus   setFormat(const std::string& pszFormat);
+    std::string         getValue(void) const;
+	Acad::ErrorStatus   evaluate1();
+    Acad::ErrorStatus   evaluate2(AcDbField::EvalContext nContext);
+    Acad::ErrorStatus   evaluate3(AcDbField::EvalContext nContext, PyDbDatabase& db);
+    static std::string  className();
+    static PyRxClass    desc();
+    static PyDbField    cloneFrom(PyRxObject& src);
 public:
     AcDbField* impObj(const std::source_location& src = std::source_location::current()) const;
 };
-
