314264a903cb8ac0ed4280f869004aaf626ed78f
Daniel <dwmcabinets@yahoo.com>
2023-06-05 15:03:16 +0800

    PyDbRasterImageDef and PyDbRasterImage

--- PyRx/PyDbImage.h
+++ PyRx/PyDbImage.h
@@ -2,6 +2,7 @@
 #include "PyDbEntity.h"
 
 class PyDbObjectId;
+class PyDbDictionary;
 
 //-----------------------------------------------------------------------------------
 //PyDbImage
@@ -33,6 +34,28 @@ public:
     PyDbRasterImageDef(AcDbRasterImageDef* ptr, bool autoDelete);
     PyDbRasterImageDef(const PyDbObjectId& id, AcDb::OpenMode mode);
     virtual ~PyDbRasterImageDef() override = default;
+    Acad::ErrorStatus   setSourceFileName(const std::string& pPathName);
+    Acad::ErrorStatus   setActiveFileName(const std::string& pPathName);
+    std::string         searchForActivePath();
+    std::string         sourceFileName() const;
+    std::string         activeFileName() const;
+    Acad::ErrorStatus   embed();
+    Adesk::Boolean      isEmbedded() const;
+    Acad::ErrorStatus   load();
+    Acad::ErrorStatus   unload1();
+    Acad::ErrorStatus   unload2(Adesk::Boolean modifyDatabase);
+    Adesk::Boolean      isLoaded() const;
+    AcGeVector2d        size() const;
+    std::string         fileType() const;
+    int                 entityCount() const;
+    void                updateEntities() const;
+    Acad::ErrorStatus   setResolutionMMPerPixel(const AcGeVector2d& resMMPerPixel);
+    AcGeVector2d        resolutionMMPerPixel() const;
+    void                setResolutionUnits(AcDbRasterImageDef::Units units);
+    AcDbRasterImageDef::Units resolutionUnits() const;
+    static PyDbObjectId createImageDictionary(PyDbDatabase& pDb);
+    static PyDbObjectId imageDictionary(PyDbDatabase& pDb);
+    static std::string  suggestName(PyDbDictionary& dict, const std::string& pPathName);
 public:
     static std::string  className();
     static PyRxClass    desc();
@@ -53,7 +76,46 @@ public:
     PyDbRasterImage(AcDbRasterImage* ptr, bool autoDelete);
     PyDbRasterImage(const PyDbObjectId& id, AcDb::OpenMode mode);
     virtual ~PyDbRasterImage() override = default;
-
+    Acad::ErrorStatus   setImageDefId(const PyDbObjectId& imageId);
+    PyDbObjectId        imageDefId() const;
+    void                setReactorId(const PyDbObjectId& reactorId);
+    PyDbObjectId        reactorId() const;
+    Adesk::Boolean      setOrientation(const AcGePoint3d& origin,const AcGeVector3d& uCorner, const AcGeVector3d& vOnPlane);
+    void                getOrientation(AcGePoint3d& origin, AcGeVector3d& u, AcGeVector3d& v) const;
+    AcGeVector2d        scale() const;
+    AcGeVector2d        imageSize1() const;
+    AcGeVector2d        imageSize2(Adesk::Boolean bGetCachedValue) const;
+    Acad::ErrorStatus   setClipBoundaryToWholeImage(AcGeVector2d& size);
+    boost::python::list clipBoundary() const;
+    AcDbRasterImage::ClipBoundaryType  clipBoundaryType() const;
+    Adesk::Boolean      isClipped() const;
+    boost::python::list getVertices() const;
+    AcGeMatrix3d        getPixelToModelTransform() const;
+    void                setDisplayOpt(AcDbRasterImage::ImageDisplayOpt option, Adesk::Boolean bValue);
+    Adesk::Boolean      isSetDisplayOpt(AcDbRasterImage::ImageDisplayOpt option) const;
+    Acad::ErrorStatus   setBrightness(Adesk::Int8 value);
+    Adesk::Int8         brightness() const;
+    Acad::ErrorStatus   setContrast(Adesk::Int8 value);
+    Adesk::Int8         contrast() const;
+    Acad::ErrorStatus   setFade(Adesk::Int8 value);
+    Adesk::Int8         fade() const;
+    Adesk::Boolean      isClipInverted() const;
+    void                setClipInverted(Adesk::Boolean newVal);
+    double              width() const;
+    double              height() const;
+    double              imageHeight() const;
+    double              imageWidth() const;
+    Acad::ErrorStatus   setHeight(double val);
+    Acad::ErrorStatus   setWidth(double val);
+    AcGePoint3d         position() const;
+    double              rotation() const;
+    Acad::ErrorStatus   setRotation(double rotation);
+    bool                isImageShown() const;
+    void                setShowImage(bool value);
+    bool                isImageTransparent() const;
+    void                setImageTransparency(bool value);
+    bool                isShownClipped() const;
+    void                setShowClipped(bool value);
 public:
     static std::string  className();
     static PyRxClass    desc();
