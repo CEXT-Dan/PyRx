db2825fa342fce187b39422799e42cf1df658e4a
Daniel <dwmcabinets@yahoo.com>
2023-06-14 07:17:15 +0800

    added more selection options

--- PyRx/PyAcEditor.h
+++ PyRx/PyAcEditor.h
@@ -32,10 +32,18 @@ public:
     static boost::python::tuple select4(const std::string& add, const std::string& remove, const boost::python::list& filter);
     static boost::python::tuple selectAll1();
     static boost::python::tuple selectAll2(const boost::python::list& filter);
-    static boost::python::tuple selectCrossingWindow1(const AcGePoint3d& pt1, const AcGePoint3d& pt2);
-    static boost::python::tuple selectCrossingWindow2(const AcGePoint3d& pt1, const AcGePoint3d& pt2, const boost::python::list& filter);
+    static boost::python::tuple selectWindow1(const AcGePoint3d& pt1, const AcGePoint3d& pt2);
+    static boost::python::tuple selectWindow2(const AcGePoint3d& pt1, const AcGePoint3d& pt2, const boost::python::list& filter);
     static boost::python::tuple selectFence1(const boost::python::list& points);
     static boost::python::tuple selectFence2(const boost::python::list& points, const boost::python::list& filter);
+    static boost::python::tuple selectWindowPolygon1(const boost::python::list& points);
+    static boost::python::tuple selectWindowPolygon2(const boost::python::list& points, const boost::python::list& filter);
+    static boost::python::tuple selectPrevious1();
+    static boost::python::tuple selectPrevious2(const boost::python::list& filter);
+    static boost::python::tuple selectLast1();
+    static boost::python::tuple selectLast2(const boost::python::list& filter);
+    static boost::python::tuple ssget1(std::string args, const boost::python::object& arg1, const boost::python::object& arg2);
+    static boost::python::tuple ssget2(std::string args, const boost::python::object& arg1, const boost::python::object& arg2, const boost::python::list& filter);
     static AcGeMatrix3d         curUCS();//
     static Acad::ErrorStatus    setCurUCS(const AcGeMatrix3d& mat);//
     static PyDbObjectId         activeViewportId();//
@@ -43,8 +51,6 @@ public:
     static boost::python::tuple getKword(const std::string& skwl);
     static boost::python::object getVar(const std::string& sym);//
     static bool                 setVar(const std::string& sym, const  boost::python::object&);//
-
-
 
     static std::string className();
 };
