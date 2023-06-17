34636a908ad36a513421b112ff20deae72521e42
Daniel <dwmcabinets@yahoo.com>
2023-06-17 08:06:46 +0800

    added PyDbDynBlockReferenceProperty

--- PyRxStubs/PyDb.pyi
+++ PyRxStubs/PyDb.pyi
@@ -19288,6 +19288,219 @@ convertToStaticBlock( (DynBlockReference)arg1, (str)arg2) -> ErrorStatus :
         enum Acad::ErrorStatus resetBlock(class PyDbDynBlockReference {lvalue})'''
     ...
 
+class DynBlockReferenceProperty:
+    def __init__ (self, *args, **kwargs)-> None :
+      '''__init__( (object)arg1) -> None :
+
+    C++ signature :
+        void __init__(struct _object * __ptr64)
+
+__init__( (object)arg1) -> None :
+
+    C++ signature :
+        void __init__(struct _object * __ptr64)'''
+    ...
+    def blockId (self, *args, **kwargs)-> ObjectId :
+      '''blockId( (DynBlockReferenceProperty)arg1) -> ObjectId :
+
+    C++ signature :
+        class PyDbObjectId blockId(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def className (self, *args, **kwargs)-> str :
+      '''className() -> str :
+
+    C++ signature :
+        class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > className()'''
+    ...
+    def description (self, *args, **kwargs)-> str :
+      '''description( (DynBlockReferenceProperty)arg1) -> str :
+
+    C++ signature :
+        class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > description(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def getAllowedValues (self, *args, **kwargs)-> list :
+      '''getAllowedValues( (DynBlockReferenceProperty)arg1) -> list :
+
+    C++ signature :
+        class boost::python::list getAllowedValues(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def propertyName (self, *args, **kwargs)-> str :
+      '''propertyName( (DynBlockReferenceProperty)arg1) -> str :
+
+    C++ signature :
+        class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> > propertyName(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def propertyType (self, *args, **kwargs)-> DwgDataType :
+      '''propertyType( (DynBlockReferenceProperty)arg1) -> DwgDataType :
+
+    C++ signature :
+        enum AcDb::DwgDataType propertyType(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def readOnly (self, *args, **kwargs)-> bool :
+      '''readOnly( (DynBlockReferenceProperty)arg1) -> bool :
+
+    C++ signature :
+        bool readOnly(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def setValue (self, *args, **kwargs)-> ErrorStatus :
+      '''setValue( (DynBlockReferenceProperty)arg1, (EvalVariant)arg2) -> ErrorStatus :
+
+    C++ signature :
+        enum Acad::ErrorStatus setValue(class PyDbDynBlockReferenceProperty {lvalue},class PyDbEvalVariant)'''
+    ...
+    def show (self, *args, **kwargs)-> bool :
+      '''show( (DynBlockReferenceProperty)arg1) -> bool :
+
+    C++ signature :
+        bool show(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def unitsType (self, *args, **kwargs)-> DynUnitsType :
+      '''unitsType( (DynBlockReferenceProperty)arg1) -> DynUnitsType :
+
+    C++ signature :
+        enum AcDbDynBlockReferenceProperty::UnitsType unitsType(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def value (self, *args, **kwargs)-> EvalVariant :
+      '''value( (DynBlockReferenceProperty)arg1) -> EvalVariant :
+
+    C++ signature :
+        class PyDbEvalVariant value(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+    def visibleInCurrentVisibilityState (self, *args, **kwargs)-> bool :
+      '''visibleInCurrentVisibilityState( (DynBlockReferenceProperty)arg1) -> bool :
+
+    C++ signature :
+        bool visibleInCurrentVisibilityState(class PyDbDynBlockReferenceProperty {lvalue})'''
+    ...
+
+class DynUnitsType:
+    def __init__ (self, /, *args, **kwargs):
+      '''Initialize self.  See help(type(self)) for accurate signature.'''
+    ...
+    def as_integer_ratio (self, /):
+      '''Return integer ratio.
+
+Return a pair of integers, whose ratio is exactly equal to the original int
+and with a positive denominator.
+
+>>> (10).as_integer_ratio()
+(10, 1)
+>>> (-10).as_integer_ratio()
+(-10, 1)
+>>> (0).as_integer_ratio()
+(0, 1)'''
+    ...
+    def bit_count (self, /):
+      '''Number of ones in the binary representation of the absolute value of self.
+
+Also known as the population count.
+
+>>> bin(13)
+'0b1101'
+>>> (13).bit_count()
+3'''
+    ...
+    def bit_length (self, /):
+      '''Number of bits necessary to represent self in binary.
+
+>>> bin(37)
+'0b100101'
+>>> (37).bit_length()
+6'''
+    ...
+    def conjugate (self, *args, **kwargs):
+      '''Returns self, the complex conjugate of any int.'''
+    ...
+    def denominator (self, *args, **kwargs):
+      '''the denominator of a rational number in lowest terms'''
+    ...
+    def from_bytes (bytes, byteorder, *, signed=False):
+      '''Return the integer represented by the given array of bytes.
+
+  bytes
+    Holds the array of bytes to convert.  The argument must either
+    support the buffer protocol or be an iterable object producing bytes.
+    Bytes and bytearray are examples of built-in objects that support the
+    buffer protocol.
+  byteorder
+    The byte order used to represent the integer.  If byteorder is 'big',
+    the most significant byte is at the beginning of the byte array.  If
+    byteorder is 'little', the most significant byte is at the end of the
+    byte array.  To request the native byte order of the host system, use
+    `sys.byteorder' as the byte order value.
+  signed
+    Indicates whether two's complement is used to represent the integer.'''
+    ...
+    def imag (self, *args, **kwargs):
+      '''the imaginary part of a complex number'''
+    ...
+    def kAngular (self, *args, **kwargs):
+      '''None'''
+    ...
+    def kArea (self, *args, **kwargs):
+      '''None'''
+    ...
+    def kDistance (self, *args, **kwargs):
+      '''None'''
+    ...
+    def kNoUnits (self, *args, **kwargs):
+      '''None'''
+    ...
+    def name (self, *args, **kwargs):
+      '''None'''
+    ...
+    def names (self, *args, **kwargs)-> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via:
+      '''dict() -> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via:
+    d = {}
+    for k, v in iterable:
+        d[k] = v
+dict(**kwargs) -> new dictionary initialized with the name=value pairs
+    in the keyword argument list.  For example:  dict(one=1, two=2)'''
+    ...
+    def numerator (self, *args, **kwargs):
+      '''the numerator of a rational number in lowest terms'''
+    ...
+    def real (self, *args, **kwargs):
+      '''the real part of a complex number'''
+    ...
+    def to_bytes (self, /, length, byteorder, *, signed=False):
+      '''Return an array of bytes representing an integer.
+
+  length
+    Length of bytes object to use.  An OverflowError is raised if the
+    integer is not representable with the given number of bytes.
+  byteorder
+    The byte order used to represent the integer.  If byteorder is 'big',
+    the most significant byte is at the beginning of the byte array.  If
+    byteorder is 'little', the most significant byte is at the end of the
+    byte array.  To request the native byte order of the host system, use
+    `sys.byteorder' as the byte order value.
+  signed
+    Determines whether two's complement is used to represent the integer.
+    If signed is False and a negative integer is given, an OverflowError
+    is raised.'''
+    ...
+    def values (self, *args, **kwargs)-> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via:
+      '''dict() -> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via:
+    d = {}
+    for k, v in iterable:
+        d[k] = v
+dict(**kwargs) -> new dictionary initialized with the name=value pairs
+    in the keyword argument list.  For example:  dict(one=1, two=2)'''
+    ...
+
 class EndCaps:
     def __init__ (self, /, *args, **kwargs):
       '''Initialize self.  See help(type(self)) for accurate signature.'''
