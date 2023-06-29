69f0c5d74f249c332b94d8693dc684356cd85931
Daniel <dwmcabinets@yahoo.com>
2023-06-29 21:20:36 +0800

    progress on PyEdUIContext

--- PyRx/PyEdUserInteraction.h
+++ PyRx/PyEdUserInteraction.h
@@ -1,4 +1,5 @@
 #pragma once
+class PyRxClass;
 
 //TODO:
 //expose this as functions to python
@@ -9,7 +10,7 @@
 
 
 //-----------------------------------------------------------------------------------------
-// WxUserInteraction
+// PyEdUserInteraction
 class PyEdUserInteraction
 {
     HWND m_activeWindow;
@@ -23,3 +24,26 @@ protected:
     void undoUserInteraction();
 };
 
+
+//-----------------------------------------------------------------------------------------
+// PyEdUIContext
+class PyEdUIContext : AcEdUIContext, public boost::python::wrapper<PyEdUIContext>
+{
+    bool isAlive = false;
+public:
+    PyEdUIContext();
+    virtual ~PyEdUIContext() override;
+    virtual void* getMenuContext(const AcRxClass* pClass, const AcDbObjectIdArray& ids);
+    virtual void  onCommand(Adesk::UInt32 cmd);
+    virtual void  OnUpdateMenu();
+
+    int64_t getMenuContextWr(const PyRxClass& pyclass, const boost::python::list& pyids);
+    void  onCommandWr(Adesk::UInt32);
+    void  OnUpdateMenuWr();
+
+    static bool addObjectContextMenu(PyRxClass& pClass, PyEdUIContext& pContext);
+    static bool removeObjectContextMenu(PyRxClass& pClass, PyEdUIContext& pContext);
+    static bool addDefaultContextMenu1(PyEdUIContext& pContext);
+    static bool addDefaultContextMenu2(PyEdUIContext& pContext, const std::string& appName);
+    static bool removeDefaultContextMenu(PyEdUIContext& pContext);
+};
