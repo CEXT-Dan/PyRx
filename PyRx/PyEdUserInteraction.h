6d532e63017813d2e4875d497633ddedc185bd2a
Daniel <dwmcabinets@yahoo.com>
2023-06-30 15:29:36 +0800

    added PyEdUIContext::calcHitPoint()

--- PyRx/PyEdUserInteraction.h
+++ PyRx/PyEdUserInteraction.h
@@ -30,18 +30,22 @@ protected:
 void makePyEdUIContextWrapper();
 class PyEdUIContext : AcEdUIContext, public boost::python::wrapper<PyEdUIContext>
 {
-    bool m_isAlive = false;
+public:
+    AcGePoint3d m_hitPoint;
     HMENU m_hmenu = 0;
+    bool m_isAlive = false;
 public:
     PyEdUIContext();
     virtual ~PyEdUIContext() override;
     virtual void* getMenuContext(const AcRxClass* pClass, const AcDbObjectIdArray& ids);
     virtual void  onCommand(Adesk::UInt32 cmd);
     virtual void  OnUpdateMenu();
+    AcGePoint3d   hitPoint() const;
 
     boost::python::object getMenuContextWr(const PyRxClass& pyclass, const boost::python::list& pyids);
-    void  onCommandWr(Adesk::UInt32);
-    void  OnUpdateMenuWr();
+    void    onCommandWr(Adesk::UInt32);
+    void    OnUpdateMenuWr();
+    void    calcHitPoint();
 
     static bool addObjectContextMenu(PyRxClass& pClass, PyEdUIContext& pContext);
     static bool removeObjectContextMenu(PyRxClass& pClass, PyEdUIContext& pContext);
