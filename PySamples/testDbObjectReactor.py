5e4fea3a5d24c009ba6915a4095b1cd216198f7a
Daniel <dwmcabinets@yahoo.com>
2023-07-27 20:59:18 +0800

    added DbObjectReactor sample

---
+++ PySamples/testDbObjectReactor.py
@@ -0,0 +1,63 @@
+import PyRx as Rx
+import PyGe as Ge
+import PyGi as Gi
+import PyDb as Db
+import PyAp as Ap
+import PyEd as Ed
+import PyGs as Gs
+
+
+class MyReactor(Db.DbObjectReactor):
+    def __init__(self):
+        Db.DbObjectReactor.__init__(self)
+
+    def cancelled(self, Obj: Db.DbObject) -> None:
+        print("cancelled", Obj.isA().dxfName())
+
+    def copied(self, Obj: Db.DbObject, newObj: Db.DbObject) -> None:
+        print("copied", Obj.isA().dxfName(), newObj.isA().dxfName())
+
+    def erased(self, Obj: Db.DbObject, flag: bool) -> None:
+        print("erased", Obj.isA().dxfName(), flag)
+
+    def goodbye(self, Obj: Db.DbObject) -> None:
+        print("goodbye", Obj.isA().dxfName())
+
+    def openedForModify(self, Obj: Db.DbObject) -> None:
+        print("openedForModify", Obj.isA().dxfName())
+
+    def modified(self, Obj: Db.DbObject) -> None:
+        print("modified", Obj.isA().dxfName())
+
+    def subObjModified(self, Obj: Db.DbObject, subObj: Db.DbObject):
+        print("subObjModified", Obj.isA().dxfName(), subObj.isA().dxfName())
+
+    def modifyUndone(self, Obj: Db.DbObject) -> None:
+        print("modifyUndone", Obj.isA().dxfName())
+        
+    def modifiedXData(self, Obj: Db.DbObject) -> None:
+        print("modifiedXData", Obj.isA().dxfName())
+        
+    def unappended(self, Obj: Db.DbObject) -> None:
+        print("unappended", Obj.isA().dxfName())
+        
+    def reappended(self, Obj: Db.DbObject) -> None:
+        print("reappended", Obj.isA().dxfName())
+
+    def objectClosed(self, id: Db.ObjectId) -> None:
+        print("objectClosed", id.objectClass().dxfName())
+
+
+myReactor = MyReactor()
+
+
+def PyRxCmd_doit():
+    try:
+        db = Db.curDb()
+        entRes = Ed.Editor.entSel("\nSelect Ent: ")
+        if (entRes[0] != Ed.PromptStatus.eNormal):
+            return
+        ent = Db.Entity(entRes[1])
+        ent.addReactor(myReactor)
+    except Exception as err:
+        print(err)
