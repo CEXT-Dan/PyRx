fed32910db06242a206a3d3145fca6c069a83456
Daniel <dwmcabinets@yahoo.com>
2023-07-30 08:26:29 +0800

    progress on filers

--- PyRx/PyDbFiler.cpp
+++ PyRx/PyDbFiler.cpp
@@ -102,6 +102,151 @@ void PyDbDwgFiler::writeSoftPointerId(const PyDbObjectId& id)
     PyThrowBadEs(impObj()->writeSoftPointerId(id.m_id));
 }
 
+Adesk::Int8 PyDbDwgFiler::readInt8()
+{
+    Adesk::Int8 val;
+    PyThrowBadEs(impObj()->readInt8(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeInt8(Adesk::Int8 val)
+{
+    PyThrowBadEs(impObj()->writeInt8(val));
+}
+
+std::string PyDbDwgFiler::readString()
+{
+    AcString val;
+    PyThrowBadEs(impObj()->readString(val));
+    return wstr_to_utf8(val);
+}
+
+void PyDbDwgFiler::writeString(const std::string& pVal)
+{
+    PyThrowBadEs(impObj()->writeString(utf8_to_wstr(pVal).c_str()));
+}
+
+//TODO TEST this!!!
+boost::python::object PyDbDwgFiler::readBChunk()
+{
+    ads_binary adsval = { 0 };
+    PyThrowBadEs(impObj()->readBChunk(&adsval));
+    PyAutoLockGIL lock;
+    PyObject* py_buf = PyMemoryView_FromMemory(adsval.buf, (size_t)adsval.clen, PyBUF_WRITE);
+    return boost::python::object(boost::python::handle<>(py_buf));
+}
+
+//TODO TEST this!!!
+void PyDbDwgFiler::writeBChunk(const boost::python::object& val)
+{
+    if (!PyObject_CheckBuffer(val.ptr()))
+        PyThrowBadEs(eInvalidInput);
+    Py_buffer view;
+    if(PyObject_GetBuffer(val.ptr(), &view, PyBUF_ANY_CONTIGUOUS) == -1)
+        PyThrowBadEs(eInvalidInput);
+    ads_binary adsval = { 0 };
+    adsval.clen = view.len;
+    adsval.buf = static_cast<char*>(view.buf);
+    PyThrowBadEs(impObj()->writeBChunk(adsval));
+    PyBuffer_Release(&view);
+}
+
+PyDbHandle PyDbDwgFiler::readAcDbHandle()
+{
+    AcDbHandle val;
+    PyThrowBadEs(impObj()->readAcDbHandle(&val));
+    return PyDbHandle(val);
+}
+
+void PyDbDwgFiler::writeAcDbHandle(const PyDbHandle& val)
+{
+    PyThrowBadEs(impObj()->writeAcDbHandle(val.m_hnd));
+}
+
+Adesk::Int64 PyDbDwgFiler::readInt64()
+{
+    Adesk::Int64 val;
+    PyThrowBadEs(impObj()->readInt64(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeInt64(Adesk::Int64 val)
+{
+    PyThrowBadEs(impObj()->writeInt64(val));
+}
+
+Adesk::Int32 PyDbDwgFiler::readInt32()
+{
+    Adesk::Int32 val;
+    PyThrowBadEs(impObj()->readInt32(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeInt32(Adesk::Int32 val)
+{
+    PyThrowBadEs(impObj()->writeInt32(val));
+}
+
+Adesk::Int16 PyDbDwgFiler::readInt16()
+{
+    Adesk::Int16 val;
+    PyThrowBadEs(impObj()->readInt16(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeInt16(Adesk::Int16 val)
+{
+    PyThrowBadEs(impObj()->writeInt16(val));
+}
+
+Adesk::UInt64 PyDbDwgFiler::readUInt64()
+{
+    Adesk::UInt64 val;
+    PyThrowBadEs(impObj()->readUInt64(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeUInt64(Adesk::UInt64 val)
+{
+    PyThrowBadEs(impObj()->writeUInt64(val));
+}
+
+Adesk::UInt32 PyDbDwgFiler::readUInt32()
+{
+    Adesk::UInt32 val;
+    PyThrowBadEs(impObj()->readUInt32(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeUInt32(Adesk::UInt32 val)
+{
+    PyThrowBadEs(impObj()->writeUInt32(val));
+}
+
+Adesk::UInt16 PyDbDwgFiler::readUInt16()
+{
+    Adesk::UInt16 val;
+    PyThrowBadEs(impObj()->readUInt16(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeUInt16(Adesk::UInt16 val)
+{
+    PyThrowBadEs(impObj()->writeUInt16(val));
+}
+
+Adesk::UInt8 PyDbDwgFiler::readUInt8()
+{
+    Adesk::UInt8 val;
+    PyThrowBadEs(impObj()->readUInt8(&val));
+    return val;
+}
+
+void PyDbDwgFiler::writeUInt8(Adesk::UInt8 val)
+{
+    PyThrowBadEs(impObj()->writeUInt8(val));
+}
+
 PyRxClass PyDbDwgFiler::desc()
 {
     return PyRxClass(AcDbDwgFiler::desc(), false);
--- PyRx/PyDbFiler.h
+++ PyRx/PyDbFiler.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "PyRxObject.h"
 
+class PyDbHandle;
 class PyDbObjectId;
 class PyDbSoftOwnershipId;
 class PyDbHardOwnershipId;
@@ -28,6 +29,64 @@ class PyDbDwgFiler : public PyRxObject
     void                writeHardPointerId(const PyDbObjectId& id);
     PyDbObjectId        readSoftPointerId();
     void                writeSoftPointerId(const PyDbObjectId& id);
+    Adesk::Int8         readInt8();
+    void                writeInt8(Adesk::Int8 val);
+    std::string         readString();
+    void                writeString(const std::string& pVal);
+
+    boost::python::object   readBChunk();
+    void                    writeBChunk(const boost::python::object& val);
+
+    PyDbHandle          readAcDbHandle();
+    void                writeAcDbHandle(const PyDbHandle& val);
+    Adesk::Int64        readInt64();
+    void                writeInt64(Adesk::Int64 val);
+    Adesk::Int32        readInt32();
+    void                writeInt32(Adesk::Int32 val);
+    Adesk::Int16        readInt16();
+    void                writeInt16(Adesk::Int16 val);
+    Adesk::UInt64       readUInt64();
+    void                writeUInt64(Adesk::UInt64 val);
+    Adesk::UInt32       readUInt32();
+    void                writeUInt32(Adesk::UInt32 val);
+    Adesk::UInt16       readUInt16();
+    void                writeUInt16(Adesk::UInt16 val);
+    Adesk::UInt8        readUInt8();
+    void                writeUInt8(Adesk::UInt8 val);
+
+
+
+
+    Acad::ErrorStatus   readBoolean(Adesk::Boolean* pVal);
+    Acad::ErrorStatus   writeBoolean(Adesk::Boolean val);
+
+    Acad::ErrorStatus   readBool(bool* pVal);
+    Acad::ErrorStatus   writeBool(bool val);
+
+    Acad::ErrorStatus   readDouble(double* pVal);
+    Acad::ErrorStatus   writeDouble(double val);
+
+    Acad::ErrorStatus   readPoint2d(AcGePoint2d* pVal);
+    Acad::ErrorStatus   writePoint2d(const AcGePoint2d& val);
+
+    Acad::ErrorStatus   readPoint3d(AcGePoint3d* pVal);
+    Acad::ErrorStatus   writePoint3d(const AcGePoint3d& val);
+
+    Acad::ErrorStatus   readVector2d(AcGeVector2d* pVal);
+    Acad::ErrorStatus   writeVector2d(const AcGeVector2d& val);
+
+    Acad::ErrorStatus   readVector3d(AcGeVector3d* pVal);
+    Acad::ErrorStatus   writeVector3d(const AcGeVector3d& val);
+
+    Acad::ErrorStatus   readScale3d(AcGeScale3d* pVal);
+    Acad::ErrorStatus   writeScale3d(const AcGeScale3d& val);
+
+    Acad::ErrorStatus   readBytes(void* pDest, Adesk::UIntPtr nBytes);
+    Acad::ErrorStatus   writeBytes(const void* pSrc, Adesk::UIntPtr nBytes);
+
+    Acad::ErrorStatus   readAddress(void** pVal);
+    Acad::ErrorStatus   writeAddress(const void* pVal);
+
 
     static PyRxClass    desc();
     static std::string  className();
