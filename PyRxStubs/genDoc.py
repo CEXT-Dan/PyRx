cc8f516577db67f80874ad5ecd9025bbb1dcc131
Daniel <dwmcabinets@yahoo.com>
2023-08-05 06:25:51 +0800

    work on stubs, add module prefix to return types

--- PyRxStubs/genDoc.py
+++ PyRxStubs/genDoc.py
@@ -12,12 +12,19 @@ import PyDb  # = database
 import PyAp  # = application, document classes services
 import PyEd  # = editor
 
+class_types = {}
+
+all_modules = [("PyRx", PyRx), ("PyGe", PyGe), ("PyGi", PyGi),
+              ("PyGs", PyGs), ("PyDb", PyDb), ("PyAp", PyAp), ("PyEd", PyEd)]
+
 all_modules_names = ["PyRx", "PyGe", "PyGi", "PyGs", "PyDb", "PyAp", "PyEd"]
 
-#TODO iterate all the above modules, and add imports ant type aliasing 
-#to each
+# TODO iterate all the above modules, and add imports ant type aliasing
+# to each
 
 # just some ideas on getting help, work in progress
+
+
 def include_attr(name):
     try:
         if name == '__init__':
@@ -27,14 +34,15 @@ def include_attr(name):
         return True
     except:
         return True
-    
+
+
 def removeArgStr(sig):
     try:
         argb = sig.find('![(')
         arge = sig.find(')]!')
         if argb != -1:
             argstr = sig[argb:arge+3]
-            newDocString = sig.replace(argstr,"")
+            newDocString = sig.replace(argstr, "")
             return newDocString.strip('\n')
         return sig.strip('\n')
     except:
@@ -51,21 +59,32 @@ def findArgs(sig):
     except:
         return ""
 
+def findReturnTypeModlue(sig):
+    if sig in class_types:
+        return class_types[sig]+"."+sig
+    else:
+        return sig
+    
 def findReturnType(sig):
     try:
         ib = sig.find('->')
         ie = sig.find(':')
         if ib != -1:
-            return sig[ib:ie]
+            return '-> ' + findReturnTypeModlue(sig[ib+2:ie].strip())
         return "-> None"
     except:
         return "-> None"
+    
+def buildClassDict(moduleName, module):
+    for name, obj in inspect.getmembers(module):
+        if inspect.isclass(obj):
+            class_types[name] = moduleName
 
 # todo: boost generates a doc string that has the function signature
 # it should be able to parse this, or add something in the doc user string
 def generate_pyi(moduleName, module):
     with open(moduleName, 'w') as f:
-        
+
         for mname in all_modules_names:
             f.write(f'import {mname}\n')
 
@@ -80,18 +99,22 @@ def generate_pyi(moduleName, module):
                         args = findArgs(sig)
                         returnType = findReturnType(sig)
                         newDocString = removeArgStr(sig)
-                        
+
                         try:
-                            f.write(f'    def {func_name} {inspect.signature(func)}:\n')
+                            f.write(
+                                f'    def {func_name} {inspect.signature(func)}:\n')
                             f.write(f"      '''{newDocString}'''")
                         except:
                             if len(args) != 0:
-                                f.write(f'    def {func_name} {args}{returnType}:\n')
-                                f.write(f"      '''                             '''")
+                                f.write(
+                                    f'    def {func_name} {args}{returnType}:\n')
+                                f.write(
+                                    f"      '''                             '''")
                             else:
-                                f.write(f'    def {func_name} (self, *args, **kwargs){returnType}:\n')
+                                f.write(
+                                    f'    def {func_name} (self, *args, **kwargs){returnType}:\n')
                                 f.write(f"      '''{newDocString}'''")
-                                
+
                         f.write('\n    ...\n')
 
             elif inspect.isbuiltin(obj):
@@ -123,30 +146,12 @@ def generate_txt_help(moduleName, module):
 
 
 def PyRxCmd_pygenpyi():
-    generate_pyi('PyRx.pyi', PyRx)
-    generate_pyi('PyGe.pyi', PyGe)
-    generate_pyi('PyGi.pyi', PyGi)
-    generate_pyi('PyGs.pyi', PyGs)
-    generate_pyi('PyDb.pyi', PyDb)
-    generate_pyi('PyAp.pyi', PyAp)
-    generate_pyi('PyEd.pyi', PyEd)
-
+    for module in all_modules:
+        buildClassDict(module[0], module[1])
+    for module in all_modules:
+        generate_pyi(module[0] + ".pyi", module[1])
 
 def PyRxCmd_pygenhtmlhelp():
-    generate_html_help('PyRx.html', PyRx)
-    generate_html_help('PyGe.html', PyGe)
-    generate_html_help('PyGi.html', PyGi)
-    generate_html_help('PyGs.html', PyGs)
-    generate_html_help('PyDb.html', PyDb)
-    generate_html_help('PyAp.html', PyAp)
-    generate_html_help('PyEd.html', PyEd)
+      for module in all_modules:
+        generate_html_help(module[0] + ".html", module[1])
 
-
-def PyRxCmd_pygentxthelp():
-    generate_txt_help('PyRx.txt', PyRx)
-    generate_txt_help('PyGe.txt', PyGe)
-    generate_txt_help('PyGi.txt', PyGi)
-    generate_txt_help('PyGs.txt', PyGs)
-    generate_txt_help('PyDb.txt', PyDb)
-    generate_txt_help('PyAp.txt', PyAp)
-    generate_txt_help('PyEd.txt', PyEd)
