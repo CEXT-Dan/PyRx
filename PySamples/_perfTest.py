a9af7c83824573b6d2885695b5223656dd47cc13
Daniel <dwmcabinets@yahoo.com>
2023-08-06 22:07:50 +0800

    added performance test

---
+++ PySamples/_perfTest.py
@@ -0,0 +1,64 @@
+import PyRx as Rx
+import PyGe as Ge
+import PyGi as Gi
+import PyDb as Db
+import PyAp as Ap
+import PyEd as Ed
+import PyGs as Gs
+
+from timeit import default_timer as timer
+
+# 0.2008480000004056 time to beat
+def move_points():
+    mat = Ge.Matrix3d()
+    mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
+
+    db = Db.HostApplicationServices().workingDatabase()
+    model = Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.kForRead)
+
+    desc = Db.Point.desc()
+    for id in model.objectIds():
+        if id.isDerivedFrom(desc):
+            ent = Db.Entity(id, Db.OpenMode.ForWrite)
+            ent.transformBy(mat)
+      
+# 0.2265791000027093 time to beat      
+def move_points_old():
+    try:
+        mat = Ge.Matrix3d()
+        mat.setToTranslation(Ge.Point3d(100,100,0).asVector())
+       
+        db = Db.HostApplicationServices().workingDatabase()
+        model =  Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.kForRead)
+ 
+        for id in model.objectIds():
+            if id.objectClass().isDerivedFrom(Db.Point.desc()):
+                ent = Db.Entity(id, Db.OpenMode.ForWrite)
+                ent.transformBy(mat)
+       
+    except Exception as err:
+        print(err)
+
+
+def PyRxCmd_pydoit():
+    try:
+        for i in range(20):
+            start = timer()
+            move_points()
+            end = timer()
+            print(end - start)
+
+    except Exception as err:
+        print(err)
+        
+def PyRxCmd_pydoit2():
+    try:
+        for i in range(20):
+            start = timer()
+            move_points_old()
+            end = timer()
+            print(end - start)
+
+    except Exception as err:
+        print(err)
+
