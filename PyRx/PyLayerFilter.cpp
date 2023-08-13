7f174965f4684fa6b69d9a1d5191691da1b52d44
Daniel <dwmcabinets@yahoo.com>
2023-08-13 20:19:00 +0800

    class outlines for layer filters

--- PyRx/PyLayerFilter.cpp
+++ PyRx/PyLayerFilter.cpp
@@ -1,2 +1,120 @@
 #include "stdafx.h"
 #include "PyLayerFilter.h"
+#include "PyDbDatabase.h"
+
+using namespace boost::python;
+
+//------------------------------------------------------------------------------------
+//PyLyLayerFilter
+void makePyLyLayerFilterWrapper()
+{
+    PyDocString DS("PyLyLayerFilter");
+    class_<PyLyLayerFilter, bases<PyRxObject>>("LayerFilter")
+        .def(init<>())
+        .def("desc", &PyLyLayerFilter::desc, DS.SARGS()).staticmethod("desc")
+        .def("className", &PyLyLayerFilter::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyLyLayerFilter::PyLyLayerFilter()
+    : PyRxObject(new AcLyLayerFilter(),true,false)
+{
+}
+
+PyLyLayerFilter::PyLyLayerFilter(AcLyLayerFilter* pt, bool autoDelete)
+    : PyRxObject(pt, autoDelete, false)
+{
+}
+
+PyRxClass PyLyLayerFilter::desc()
+{
+    return PyRxClass(AcLyLayerFilter::desc(), false);
+}
+
+std::string PyLyLayerFilter::className()
+{
+    return "AcLyLayerFilter";
+}
+
+AcLyLayerFilter* PyLyLayerFilter::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]]
+        throw PyNullObject(src);
+        return static_cast<AcLyLayerFilter*>(m_pyImp.get());
+}
+
+//------------------------------------------------------------------------------------
+//PyLyLayerGroup
+void makePyLyLayerGroupWrapper()
+{
+    PyDocString DS("PyLyLayerFilter");
+    class_<PyLyLayerGroup, bases<PyLyLayerFilter>>("LayerGroup")
+        .def(init<>())
+        .def("desc", &PyLyLayerGroup::desc, DS.SARGS()).staticmethod("desc")
+        .def("className", &PyLyLayerGroup::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+
+PyLyLayerGroup::PyLyLayerGroup()
+    : PyLyLayerGroup(new AcLyLayerGroup(),true)
+{
+}
+
+PyLyLayerGroup::PyLyLayerGroup(AcLyLayerGroup* pt, bool autoDelete)
+    : PyLyLayerFilter(pt, autoDelete)
+{
+}
+
+PyRxClass PyLyLayerGroup::desc()
+{
+    return PyRxClass(AcLyLayerGroup::desc(), false);
+}
+
+std::string PyLyLayerGroup::className()
+{
+    return "AcLyLayerGroup";
+}
+
+AcLyLayerGroup* PyLyLayerGroup::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]]
+        throw PyNullObject(src);
+        return static_cast<AcLyLayerGroup*>(m_pyImp.get());
+}
+
+//------------------------------------------------------------------------------------
+//PyLayerFilterManager
+void makePyLayerFilterManagerWrapper()
+{
+    class_<PyLayerFilterManager>(":LayerFilterManager")
+        .def(init<>())
+        .def(init<PyDbDatabase&>())
+        .def("getFilters", &PyLayerFilterManager::getFilters)
+        .def("setFilters", &PyLayerFilterManager::setFilters)
+        ;
+}
+
+PyLayerFilterManager::PyLayerFilterManager()
+    : imp(aclyGetLayerFilterManager(acdbHostApplicationServices()->workingDatabase()))
+{
+}
+
+PyLayerFilterManager::PyLayerFilterManager(PyDbDatabase& db)
+    : imp(aclyGetLayerFilterManager(db.impObj()))
+{
+
+}
+
+boost::python::list PyLayerFilterManager::getFilters()
+{
+    PyAutoLockGIL lock;
+    boost::python::list pyList;
+    //
+    return pyList;
+}
+
+void PyLayerFilterManager::setFilters(boost::python::list& pyList)
+{
+    //
+}
--- PyRx/PyLayerFilter.h
+++ PyRx/PyLayerFilter.h
@@ -1,5 +1,57 @@
 #pragma once
-class PyLayerFilter
+#include "PyRxObject.h"
+
+class PyDbDatabase;
+
+//------------------------------------------------------------------------------------
+//PyLyLayerFilter
+void makePyLyLayerFilterWrapper();
+
+class PyLyLayerFilter : public PyRxObject
 {
+public:
+    PyLyLayerFilter();
+    PyLyLayerFilter(AcLyLayerFilter* pt, bool autoDelete);
+    inline virtual ~PyLyLayerFilter() = default;
+    static PyRxClass    desc();
+    static std::string  className();
+
+public:
+    inline AcLyLayerFilter* impObj(const std::source_location& src = std::source_location::current()) const;
 };
+
+//------------------------------------------------------------------------------------
+//PyLyLayerGroup
+void makePyLyLayerGroupWrapper();
+class PyLyLayerGroup : public PyLyLayerFilter
+{
+public:
+    PyLyLayerGroup();
+    PyLyLayerGroup(AcLyLayerGroup* pt, bool autoDelete);
+    inline virtual ~PyLyLayerGroup() = default;
+    static PyRxClass    desc();
+    static std::string  className();
+
+public:
+    inline AcLyLayerGroup* impObj(const std::source_location& src = std::source_location::current()) const;
+};
+
+//------------------------------------------------------------------------------------
+//PyLayerFilterManager
+void makePyLayerFilterManagerWrapper();
+
+class PyLayerFilterManager
+{
+public:
+    PyLayerFilterManager();
+    PyLayerFilterManager(PyDbDatabase& db);
+    virtual ~PyLayerFilterManager() = default;
+    boost::python::list getFilters();
+    void setFilters(boost::python::list& pyList);
+public:
+    AcLyLayerFilterManager* imp = nullptr;
+};
+
+
+
 
