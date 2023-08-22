30dcf7c1bc3a49705b447c60a3d886181763d535
Daniel <dwmcabinets@yahoo.com>
2023-08-22 15:36:45 +0800

    progress on AcDbGroup

--- PyRx/PyDbGroup.h
+++ PyRx/PyDbGroup.h
@@ -1,5 +1,25 @@
 #pragma once
-class PyDbGroup
-{
-};
+#include "PyDbObject.h"
 
+//----------------------------------------------------------------------------------------
+//PyDbGroup
+void makePyDbGroupWrapper();
+class PyDbGroup : public PyDbObject
+{
+public:
+    PyDbGroup();
+    PyDbGroup(const std::string& grpDesc);
+    PyDbGroup(const std::string& grpDesc, bool selectable);
+    PyDbGroup(AcDbGroup* ptr, bool autoDelete);
+    PyDbGroup(const PyDbObjectId&);
+    PyDbGroup(const PyDbObjectId& id, AcDb::OpenMode mode);
+    inline virtual ~PyDbGroup() override = default;
+
+public:
+    static PyRxClass    desc();
+    static std::string  className();
+    static PyDbGroup    cloneFrom(const PyRxObject& src);
+    static PyDbGroup    cast(const PyRxObject& src);
+public:
+    inline AcDbGroup* impObj(const std::source_location& src = std::source_location::current()) const;
+};
