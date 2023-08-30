70710d36f3fdc0c76ea0772c9226c48173d14993
Daniel <dwmcabinets@yahoo.com>
2023-08-30 18:02:14 +0800

    update PlotEngine

--- PySamples/testPlotwindow.py
+++ PySamples/testPlotwindow.py
@@ -17,7 +17,6 @@ def PyRxCmd_doit():
 
         doc = Ap.curDoc()
         db = doc.database()
-        
 
         ppr = Ed.Editor.getPoint("\nSelect first corner of plot area: ")
         if ppr[0] != Ed.PromptStatus.eOk:
@@ -37,14 +36,12 @@ def PyRxCmd_doit():
         ext = Db.Extents2d()
         ext.addPoint(Ge.Point2d(p1.x, p1.y))
         ext.addPoint(Ge.Point2d(p2.x, p2.y))
-        
-        #breakpoint()
 
         btr = Db.BlockTableRecord(db.currentSpaceId())
         lo = Db.Layout(btr.getLayoutId())
 
         pi = Pl.PlotInfo()
-        pi.setLayout(lo.objectId())
+        pi.setLayout(btr.getLayoutId())
 
         ps = Db.PlotSettings(lo.modelType())
         ps.copyFrom(lo)
@@ -58,50 +55,62 @@ def PyRxCmd_doit():
         psv.setStdScaleType(ps, Db.StdScaleType.kScaleToFit)
         psv.setPlotCentered(ps, True)
         psv.setPlotCfgName(
-            ps, "DWF6 ePlot.pc3", "ANSI_A_(8.50_x_11.00_Inches)")
+            ps, "DWG to PDF.pc3", "ANSI_A_(8.50_x_11.00_Inches)")
         
+        psv.setPlotRotation(ps, Db.PlotRotation.k0degrees)
+
         pi.setOverrideSettings(ps)
-        
+
         piv = Pl.PlotInfoValidator()
         piv.setMediaMatchingPolicy(Pl.MatchingPolicy.kMatchEnabled)
-        
+        piv.validate(pi)
+
         if Pl.PlotFactory.processPlotState() != Pl.ProcessPlotState.kNotPlotting:
             return
-        
+
         pe = Pl.PlotFactory.createPublishEngine()
 
-        ppd = Pl.PlotProgressDialog(False, 1, True)
-        ppd.setPlotMsgString(Pl.PlotMSGIndex.kDialogTitle,"Custom Plot Progress")
-        ppd.setPlotMsgString(Pl.PlotMSGIndex.kCancelSheetBtnMsg,"Cancel Job")
-        ppd.setPlotMsgString(Pl.PlotMSGIndex.kCancelSheetBtnMsg,"Cancel Sheet")
-        ppd.setPlotMsgString(Pl.PlotMSGIndex.kSheetSetProgressCaption,"Sheet Set Progress")
-        ppd.setPlotMsgString(Pl.PlotMSGIndex.kSheetProgressCaption,  "Sheet Progress")
-        
-        ppd.setPlotProgressRange(0,100)
+        ppd = Pl.PlotProgressDialog(True, 1, True)
+        ppd.setPlotMsgString(
+            Pl.PlotMSGIndex.kDialogTitle, "Custom Plot Progress")
+
+        ppd.setPlotMsgString(
+            Pl.PlotMSGIndex.kCancelSheetBtnMsg, "Cancel Job")
+
+        ppd.setPlotMsgString(
+            Pl.PlotMSGIndex.kCancelSheetBtnMsg, "Cancel Sheet")
+
+        ppd.setPlotMsgString(
+            Pl.PlotMSGIndex.kSheetSetProgressCaption, "Sheet Set Progress")
+
+        ppd.setPlotMsgString(
+            Pl.PlotMSGIndex.kSheetProgressCaption,  "Sheet Progress")
+
+        ppd.setPlotProgressRange(0, 100)
         ppd.setPlotProgressPos(0)
-        
+
         pe.beginPlot(ppd)
-        pe.beginDocument(pi,"HAY",1,True, "E:\\test-output")
-        
+        pe.beginDocument(pi, doc.fileName(), 1, True, "E:\\test-output")
+
         ppd.onBeginSheet()
-        ppd.setSheetProgressRange(0,100)
+
+        ppd.setSheetProgressRange(0, 100)
         ppd.setSheetProgressPos(0)
-        
+
         ppi = Pl.PlotPageInfo()
-        pe.beginPage(ppi,pi,True)
-        
+        pe.beginPage(ppi, pi, True)
+
         pe.beginGenerateGraphics()
         pe.endGenerateGraphics()
-        
+
         pe.endPage()
         ppd.setSheetProgressPos(100)
         ppd.onEndSheet()
-        
+
         pe.endDocument()
         ppd.setPlotProgressPos(100)
         ppd.onEndPlot()
         pe.endPlot()
-    
 
     except Exception as err:
         print(traceback.format_exc())
