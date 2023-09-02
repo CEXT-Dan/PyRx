9aa6a2b8340573726b69228d9af7d3f545ea65f6
Daniel <dwmcabinets@yahoo.com>
2023-09-02 21:19:16 +0800

    update to version 1.1.133

--- PyRx/Resource.h
+++ PyRx/Resource.h
@@ -1,15 +1,14 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by PyRxApp.rc
+// Used by ArxBrxTest.rc
 //
-#define IDS_PROJNAME 100
-#define MENUITEM_ID_1 10000
-#define MENUITEM_ID_2 10001
-#define MENUITEM_ID_3 10002
-#define MENUITEM_ID_4 10003
-#define MENUITEM_ID_5 10004
-#define MENUITEM_ID_6 10005
-
+#define IDS_PROJNAME                    100
+#define MENUITEM_ID_1                   10000
+#define MENUITEM_ID_2                   10001
+#define MENUITEM_ID_3                   10002
+#define MENUITEM_ID_4                   10003
+#define MENUITEM_ID_5                   10004
+#define MENUITEM_ID_6                   10005
 
 // Next default values for new objects
 // 
