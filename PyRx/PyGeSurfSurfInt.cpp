bd73877cc9367e56d43d910d263bba6e1b013a34
Daniel <dwmcabinets@yahoo.com>
2023-09-11 16:23:53 +0800

    work on PyGeSurfSurfInt

--- PyRx/PyGeSurfSurfInt.cpp
+++ PyRx/PyGeSurfSurfInt.cpp
@@ -1,18 +1,36 @@
 #include "stdafx.h"
 #include "PyGeSurfSurfInt.h"
+#include "PyAcGeSurface.h"
 
 using namespace boost::python;
 
-//TODO LINK ERROR @CTOR
 void makePyGeSurfSurfIntWrapper()
 {
-    class_<PyGeSurfSurfInt, bases<PyGeEntity3d>>("SurfSurfInt", boost::python::no_init)
+    class_<PyGeSurfSurfInt, bases<PyGeEntity3d>>("SurfSurfInt")
+        .def(init<>())
+        .def(init<const PyAcGeSurface&, const PyAcGeSurface&>())
+        .def(init<const PyAcGeSurface&, const PyAcGeSurface&, const AcGeTol&>())
         .def("className", &PyGeSurfSurfInt::className).staticmethod("className")
         ;
 }
 
+PyGeSurfSurfInt::PyGeSurfSurfInt()
+    : PyGeEntity3d(new AcGeSurfSurfInt())
+{
+}
+
 PyGeSurfSurfInt::PyGeSurfSurfInt(AcGeEntity3d* pEnt)
     : PyGeEntity3d(pEnt)
+{
+}
+
+PyGeSurfSurfInt::PyGeSurfSurfInt(const PyAcGeSurface& srf1, const PyAcGeSurface& srf2)
+    : PyGeEntity3d(new AcGeSurfSurfInt(*srf1.impObj(), *srf2.impObj()))
+{
+}
+
+PyGeSurfSurfInt::PyGeSurfSurfInt(const PyAcGeSurface& srf1, const PyAcGeSurface& srf2, const AcGeTol& tol)
+    : PyGeEntity3d(new AcGeSurfSurfInt(*srf1.impObj(), *srf2.impObj(), tol))
 {
 }
 
@@ -21,7 +39,9 @@ std::string PyGeSurfSurfInt::className()
     return "AcGeSurfSurfInt";
 }
 
-AcGeSurfSurfInt* PyGeSurfSurfInt::impObj() const
+AcGeSurfSurfInt* PyGeSurfSurfInt::impObj(const std::source_location& src /*= std::source_location::current()*/) const
 {
-    return static_cast<AcGeSurfSurfInt*>(m_imp.get());
+    if (m_imp == nullptr) [[unlikely]]
+        throw PyNullObject(src);
+        return static_cast<AcGeSurfSurfInt*>(m_imp.get());
 }
--- PyRx/PyGeSurfSurfInt.h
+++ PyRx/PyGeSurfSurfInt.h
@@ -1,15 +1,20 @@
 #pragma once
 #include "PyGeEntity3d.h"
 
-//TODO LINK ERROR @CTOR
+class PyAcGeSurface;
+
 void makePyGeSurfSurfIntWrapper();
 
 class PyGeSurfSurfInt :public PyGeEntity3d
 {
 public:
+    PyGeSurfSurfInt();
+    PyGeSurfSurfInt(const PyAcGeSurface& srf1, const PyAcGeSurface& srf2);
+    PyGeSurfSurfInt(const PyAcGeSurface& srf1,  const PyAcGeSurface& srf2, const AcGeTol& tol);
+
     PyGeSurfSurfInt(AcGeEntity3d* pEnt);
     static std::string className();
 public:
-    AcGeSurfSurfInt* impObj() const;
+    AcGeSurfSurfInt* impObj(const std::source_location& src = std::source_location::current()) const;
 };
 
