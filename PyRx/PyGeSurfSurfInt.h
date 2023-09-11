8db3350961946a2accc88057c8fc6b644de14bbe
Daniel <dwmcabinets@yahoo.com>
2023-09-11 20:07:04 +0800

    work on PyAcGeSurface and PyGeEntity3d

--- PyRx/PyGeSurfSurfInt.h
+++ PyRx/PyGeSurfSurfInt.h
@@ -11,8 +11,10 @@ public:
     PyGeSurfSurfInt();
     PyGeSurfSurfInt(const PyAcGeSurface& srf1, const PyAcGeSurface& srf2);
     PyGeSurfSurfInt(const PyAcGeSurface& srf1,  const PyAcGeSurface& srf2, const AcGeTol& tol);
+    PyGeSurfSurfInt(AcGeSurfSurfInt* pEnt, bool autoDelete = false);
+    PyAcGeSurface surface1() const;
+    PyAcGeSurface surface2() const;
 
-    PyGeSurfSurfInt(AcGeEntity3d* pEnt);
     static std::string className();
 public:
     AcGeSurfSurfInt* impObj(const std::source_location& src = std::source_location::current()) const;
