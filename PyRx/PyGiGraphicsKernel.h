c810ab2f81691014813cf9480a9c287f148adb6e
Daniel <dwmcabinets@yahoo.com>
2023-09-11 08:02:33 +0800

    fixed typo

--- PyRx/PyGiGraphicsKernel.h
+++ PyRx/PyGiGraphicsKernel.h
@@ -3,3 +3,7 @@ class PyGiGraphicsKernel
 {
 };
 
+class PyGiContext
+{
+
+};
--- PySamples/pyautocadtest.py
+++ PySamples/pyautocadtest.py
@@ -1,17 +1,66 @@
-import pyautocad as PYCAD
-from timeit import default_timer as timer
+import PyRx as Rx
+import PyGe as Ge
+import PyGi as Gi
+import PyDb as Db
+import PyAp as Ap
+import PyEd as Ed
+ 
+import traceback
 
-acad = PYCAD.Autocad(create_if_not_exists=True)
-        
-start = timer()
-        
-mspace = acad.ActiveDocument.ModelSpace
-for i in range(0, mspace.Count):
-    obj = mspace[i]
-    if obj.ObjectName != "AcDbPoint":
-        continue
-    obj.Layer = "Tin"
+from pyautocad import Autocad, APoint
+
+
+#pyautoacad from a command context
+
+def PyRxCmd_pyautoacad():
+    try:
+        acad = Autocad()
+        acad.prompt("Hello, Autocad from Python\n")
+        print(acad.doc.Name)
+
+        p1 = APoint(0, 0)
+        p2 = APoint(50, 25)
+        for i in range(5):
+            text = acad.model.AddText('Hi %s!' % i, p1, 2.5)
+            acad.model.AddLine(p1, p2)
+            acad.model.AddCircle(p1, 10)
+            p1.y += 10
             
-end = timer()
+    except Exception as err:
+        traceback.print_exception(err)
         
-print(end - start)
+
+def PyRxCmd_pyrx():
+    try:
+        #calls acutPrintf
+        print("Hello, Autocad from Python\n")
+        
+        #db = Db.curDb() or
+        db = Db.HostApplicationServices().workingDatabase()
+        model = Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.kForWrite)
+        
+        p1 = Ge.Point3d(0,0,0)
+        p2 = Ge.Point3d(50,25,0)
+        
+        #move to the right so we don't overlap pyautoacad
+        p1 += (Ge.Vector3d.kXAxis * 100)
+        p2 += (Ge.Vector3d.kXAxis * 100)
+        
+        for i in range(5):
+            
+            mt = Db.MText()
+            mt.setLocation(p1)
+            mt.setContents('PyHi %s!' % i)
+            mt.setTextHeight(2.5)
+            model.appendAcDbEntity(mt)
+            
+            line = Db.Line(p1, p2)
+            model.appendAcDbEntity(line)
+            
+            circle = Db.Circle(p1,Ge.Vector3d.kZAxis, 10)
+            model.appendAcDbEntity(circle)
+            p1.y += 10
+            
+    except Exception as err:
+        traceback.print_exception(err)
+        
