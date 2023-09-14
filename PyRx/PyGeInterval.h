cbe41406da46bde21c1fd5b5458a064c48a0b98b
Daniel <dwmcabinets@yahoo.com>
2023-09-14 12:34:57 +0800

    progress on PyGeInterval

--- PyRx/PyGeInterval.h
+++ PyRx/PyGeInterval.h
@@ -8,8 +8,58 @@ class PyGeInterval
 {
 public:
     PyGeInterval();
+    PyGeInterval(double lower, double upper);
+    PyGeInterval(Adesk::Boolean boundedBelow, double bound);
     PyGeInterval(const AcGeInterval& src);
     static std::string className();
+
+    double               lowerBound() const;
+    double               upperBound() const;
+    double               element() const;
+    boost::python::tuple getBounds() const;
+    double               length() const;
+    double               tolerance() const;
+
+    void                 set(double lower, double upper);
+    void                 set(Adesk::Boolean boundedBelow, double bound);
+    void                 set();
+    void                 setUpper(double upper);
+    void                 setLower(double lower);
+    void                 setTolerance(double tol);
+    void                 getMerge(const PyGeInterval& otherInterval, PyGeInterval& result) const;
+    int                  subtract(const PyGeInterval& otherInterval, PyGeInterval& lInterval, PyGeInterval& rInterval) const;
+    Adesk::Boolean       intersectWith(const PyGeInterval& otherInterval, PyGeInterval& result) const;
+    Adesk::Boolean       isBounded() const;
+    Adesk::Boolean       isBoundedAbove() const;
+    Adesk::Boolean       isBoundedBelow() const;
+    Adesk::Boolean       isUnBounded() const;
+    Adesk::Boolean       isSingleton() const;
+    Adesk::Boolean       isDisjoint(const PyGeInterval& otherInterval) const;
+    Adesk::Boolean       contains1(const PyGeInterval& otherInterval) const;
+    Adesk::Boolean       contains2(double val) const;
+    Adesk::Boolean       isContinuousAtUpper(const PyGeInterval& otherInterval) const;
+    Adesk::Boolean       isOverlapAtUpper(const PyGeInterval& otherInterval, PyGeInterval& overlap) const;
+    Adesk::Boolean       isEqualAtUpper1(const PyGeInterval& otherInterval) const;
+    Adesk::Boolean       isEqualAtUpper2(double value) const;
+    Adesk::Boolean       isEqualAtLower1(const PyGeInterval& otherInterval) const;
+    Adesk::Boolean       isEqualAtLower2(double value) const;
+    boost::python::tuple isPeriodicallyOn(double period);
+
+    bool                 isGreater1(double val);
+    bool                 isGreater2(const PyGeInterval& otherInterval);
+    bool                 isGreaterOrEqual1(double val);
+    bool                 isGreaterOrEqual2(const PyGeInterval& otherInterval);
+
+    bool                 isLess1(double val);
+    bool                 isLess2(const PyGeInterval& otherInterval);
+    bool                 isLessOrEqual1(double val);
+    bool                 isLessOrEqual2(const PyGeInterval& otherInterval);
+
+
+    Adesk::Boolean       operator ==      (const PyGeInterval& otherInterval) const;
+    Adesk::Boolean       operator !=      (const PyGeInterval& otherInterval) const;
+
+
 public:
     AcGeInterval imp;
 };
--- PyRx/PyGeSurfSurfInt.h
+++ PyRx/PyGeSurfSurfInt.h
@@ -14,21 +14,21 @@ public:
     PyGeSurfSurfInt(const PyGeSurface& srf1, const PyGeSurface& srf2);
     PyGeSurfSurfInt(const PyGeSurface& srf1,  const PyGeSurface& srf2, const AcGeTol& tol);
     PyGeSurfSurfInt(AcGeSurfSurfInt* pEnt, bool autoDelete = false);
-    PyGeSurface       surface1() const;
-    PyGeSurface       surface2() const;
-    AcGeTol             tolerance() const;
-    int                 numResults();
+    PyGeSurface          surface1() const;
+    PyGeSurface          surface2() const;
+    AcGeTol              tolerance() const;
+    int                  numResults();
 
-    PyGeCurve3d         intCurve(int intNum, Adesk::Boolean isExternal) const;
-    PyGeCurve2d         intParamCurve(int num, Adesk::Boolean isExternal, Adesk::Boolean isFirst) const;
-    AcGePoint3d         intPoint(int intNum) const;
+    PyGeCurve3d          intCurve(int intNum, Adesk::Boolean isExternal) const;
+    PyGeCurve2d          intParamCurve(int num, Adesk::Boolean isExternal, Adesk::Boolean isFirst) const;
+    AcGePoint3d          intPoint(int intNum) const;
     boost::python::tuple getIntPointParams(int intNum) const;
     boost::python::tuple getIntConfigs(int intNum) const;
 
-    int		            getDimension(int intNum) const;
-    int                 getType(int intNum) const;
-    void                set1(const PyGeSurface& srf1, const PyGeSurface& srf2);
-    void                set2(const PyGeSurface& srf1, const PyGeSurface& srf2, const AcGeTol& tol);
+    int		             getDimension(int intNum) const;
+    int                  getType(int intNum) const;
+    void                 set1(const PyGeSurface& srf1, const PyGeSurface& srf2);
+    void                 set2(const PyGeSurface& srf1, const PyGeSurface& srf2, const AcGeTol& tol);
 
     static std::string className();
 public:
