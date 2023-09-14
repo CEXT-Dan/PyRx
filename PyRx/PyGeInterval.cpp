8b9c3b280d03ee9de0d7a5b5700e22ad92dcf9f8
Daniel <dwmcabinets@yahoo.com>
2023-09-14 12:34:57 +0800

    progress on PyGeInterval

--- PyRx/PyGeInterval.cpp
+++ PyRx/PyGeInterval.cpp
@@ -20,7 +20,226 @@ PyGeInterval::PyGeInterval(const AcGeInterval& src)
 {
 }
 
+PyGeInterval::PyGeInterval(double lower, double upper)
+    : imp(lower, upper)
+{
+
+}
+
+PyGeInterval::PyGeInterval(Adesk::Boolean boundedBelow, double bound)
+    : imp(boundedBelow, bound)
+{
+
+}
+
 std::string PyGeInterval::className()
 {
     return "AcGeInterval";
+}
+
+double PyGeInterval::lowerBound() const
+{
+    return imp.lowerBound();
+}
+
+double PyGeInterval::upperBound() const
+{
+    return imp.upperBound();
+}
+
+double PyGeInterval::element() const
+{
+    return imp.element();
+}
+
+boost::python::tuple PyGeInterval::getBounds() const
+{
+    PyAutoLockGIL lock;
+    double lower = 0;
+    double upper = 0;
+    imp.getBounds(lower, upper);
+    return boost::python::make_tuple(lower, upper);
+}
+
+double PyGeInterval::length() const
+{
+    return imp.length();
+}
+
+double PyGeInterval::tolerance() const
+{
+    return imp.tolerance();
+}
+
+void PyGeInterval::set(double lower, double upper)
+{
+    imp.set(lower, upper);
+}
+
+void PyGeInterval::set(Adesk::Boolean boundedBelow, double bound)
+{
+    imp.set(boundedBelow, bound);
+}
+
+void PyGeInterval::set()
+{
+    imp.set();
+}
+
+void PyGeInterval::setUpper(double upper)
+{
+    imp.setUpper(upper);
+}
+
+void PyGeInterval::setLower(double lower)
+{
+    imp.setLower(lower);
+}
+
+void PyGeInterval::setTolerance(double tol)
+{
+    imp.setTolerance(tol);
+}
+
+void PyGeInterval::getMerge(const PyGeInterval& otherInterval, PyGeInterval& result) const
+{
+    imp.getMerge(otherInterval.imp, result.imp);
+}
+
+int PyGeInterval::subtract(const PyGeInterval& otherInterval, PyGeInterval& lInterval, PyGeInterval& rInterval) const
+{
+    return imp.subtract(otherInterval.imp, lInterval.imp, rInterval.imp);
+}
+
+Adesk::Boolean PyGeInterval::intersectWith(const PyGeInterval& otherInterval, PyGeInterval& result) const
+{
+    return imp.intersectWith(otherInterval.imp, result.imp);
+}
+
+Adesk::Boolean PyGeInterval::isBounded() const
+{
+    return imp.isBounded();
+}
+
+Adesk::Boolean PyGeInterval::isBoundedAbove() const
+{
+    return imp.isBoundedAbove();
+}
+
+Adesk::Boolean PyGeInterval::isBoundedBelow() const
+{
+    return imp.isBoundedBelow();
+}
+
+Adesk::Boolean PyGeInterval::isUnBounded() const
+{
+    return imp.isUnBounded();
+}
+
+Adesk::Boolean PyGeInterval::isSingleton() const
+{
+    return imp.isSingleton();
+}
+
+Adesk::Boolean PyGeInterval::isDisjoint(const PyGeInterval& otherInterval) const
+{
+    return imp.isDisjoint(otherInterval.imp);
+}
+
+Adesk::Boolean PyGeInterval::contains1(const PyGeInterval& otherInterval) const
+{
+    return imp.contains(otherInterval.imp);
+}
+
+Adesk::Boolean PyGeInterval::contains2(double val) const
+{
+    return imp.contains(val);
+}
+
+Adesk::Boolean PyGeInterval::isContinuousAtUpper(const PyGeInterval& otherInterval) const
+{
+    return imp.isContinuousAtUpper(otherInterval.imp);
+}
+
+Adesk::Boolean PyGeInterval::isOverlapAtUpper(const PyGeInterval& otherInterval, PyGeInterval& overlap) const
+{
+    return imp.isOverlapAtUpper(otherInterval.imp, overlap.imp);
+}
+
+Adesk::Boolean PyGeInterval::isEqualAtUpper1(const PyGeInterval& otherInterval) const
+{
+    return imp.isEqualAtUpper(otherInterval.imp);
+}
+
+Adesk::Boolean PyGeInterval::isEqualAtUpper2(double value) const
+{
+    return imp.isEqualAtUpper(value);
+}
+
+Adesk::Boolean PyGeInterval::isEqualAtLower1(const PyGeInterval& otherInterval) const
+{
+    return imp.isEqualAtLower(otherInterval.imp);
+}
+
+Adesk::Boolean PyGeInterval::isEqualAtLower2(double value) const
+{
+    return imp.isEqualAtLower(value);
+}
+
+boost::python::tuple PyGeInterval::isPeriodicallyOn(double period)
+{
+    PyAutoLockGIL lock;
+    double val = 0;
+    auto res = imp.isPeriodicallyOn(period, val);
+    return boost::python::make_tuple(res, val);
+}
+
+bool PyGeInterval::isGreater1(double val)
+{
+    return imp > val;
+}
+
+bool PyGeInterval::isGreater2(const PyGeInterval& otherInterval)
+{
+    return imp > otherInterval.imp;
+}
+
+bool PyGeInterval::isGreaterOrEqual1(double val)
+{
+    return imp >= val;
+}
+
+bool PyGeInterval::isGreaterOrEqual2(const PyGeInterval& otherInterval)
+{
+    return imp >= otherInterval.imp;
+}
+
+bool PyGeInterval::isLess1(double val)
+{
+    return imp < val;
+}
+
+bool PyGeInterval::isLess2(const PyGeInterval& otherInterval)
+{
+    return imp < otherInterval.imp;
+}
+
+bool PyGeInterval::isLessOrEqual1(double val)
+{
+    return imp <= val;
+}
+
+bool PyGeInterval::isLessOrEqual2(const PyGeInterval& otherInterval)
+{
+    return imp <= otherInterval.imp;
+}
+
+Adesk::Boolean PyGeInterval::operator!=(const PyGeInterval& otherInterval) const
+{
+    return imp != otherInterval.imp;
+}
+
+Adesk::Boolean PyGeInterval::operator==(const PyGeInterval& otherInterval) const
+{
+    return imp == otherInterval.imp;
 }
--- PyRx/PyGePlane.h
+++ PyRx/PyGePlane.h
@@ -20,18 +20,17 @@ public:
     PyGePlane(double a, double b, double c, double d);
     PyGePlane(AcGeEntity3d* pEnt);
     ~PyGePlane() = default;
-    double     signedDistanceTo(const AcGePoint3d& pnt) const;
-    boost::python::tuple intersectWith1(const PyGeLinearEnt3d& linEnt) const;
-    boost::python::tuple intersectWith2(const PyGeLinearEnt3d& linEnt, const AcGeTol& tol) const;
-    boost::python::tuple intersectWith3(const PyGePlane& otherPln) const;
-    boost::python::tuple intersectWith4(const PyGePlane& otherPln, const AcGeTol& tol) const;
-    boost::python::tuple intersectWith5(const PyGeBoundedPlane& bndPln) const;
-    boost::python::tuple intersectWith6(const PyGeBoundedPlane& bndPln, const AcGeTol& tol) const;
-    void set1(const AcGePoint3d& pnt, const AcGeVector3d& normal);
-    void set2(const AcGePoint3d& pntU, const AcGePoint3d& org, const AcGePoint3d& pntV);
-    void set3(double a, double b, double c, double d);
-    void set4(const AcGePoint3d& org, const AcGeVector3d& uAxis, const AcGeVector3d& vAxis);
-
+    double                  signedDistanceTo(const AcGePoint3d& pnt) const;
+    boost::python::tuple    intersectWith1(const PyGeLinearEnt3d& linEnt) const;
+    boost::python::tuple    intersectWith2(const PyGeLinearEnt3d& linEnt, const AcGeTol& tol) const;
+    boost::python::tuple    intersectWith3(const PyGePlane& otherPln) const;
+    boost::python::tuple    intersectWith4(const PyGePlane& otherPln, const AcGeTol& tol) const;
+    boost::python::tuple    intersectWith5(const PyGeBoundedPlane& bndPln) const;
+    boost::python::tuple    intersectWith6(const PyGeBoundedPlane& bndPln, const AcGeTol& tol) const;
+    void                    set1(const AcGePoint3d& pnt, const AcGeVector3d& normal);
+    void                    set2(const AcGePoint3d& pntU, const AcGePoint3d& org, const AcGePoint3d& pntV);
+    void                    set3(double a, double b, double c, double d);
+    void                    set4(const AcGePoint3d& org, const AcGeVector3d& uAxis, const AcGeVector3d& vAxis);
     static std::string className();
 public:
     AcGePlane* impObj(const std::source_location& src = std::source_location::current()) const;
@@ -55,9 +54,9 @@ public:
     boost::python::tuple intersectWith4(const PyGePlane& otherPln, const AcGeTol& tol) const;
     boost::python::tuple intersectWith5(const PyGeBoundedPlane& bndPln) const;
     boost::python::tuple intersectWith6(const PyGeBoundedPlane& bndPln, const AcGeTol& tol) const;
-    void set1(const AcGePoint3d& origin, const AcGeVector3d& uVec,const AcGeVector3d& vVec);
-    void set2(const AcGePoint3d& p1, const AcGePoint3d& origin, const AcGePoint3d& p2);
-    static std::string className();
+    void                 set1(const AcGePoint3d& origin, const AcGeVector3d& uVec,const AcGeVector3d& vVec);
+    void                 set2(const AcGePoint3d& p1, const AcGePoint3d& origin, const AcGePoint3d& p2);
+    static std::string  className();
 public:
     AcGeBoundedPlane* impObj(const std::source_location& src = std::source_location::current()) const;
 };
