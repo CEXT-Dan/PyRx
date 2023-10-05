59c3b1f923612f3d915071236706853c6c1ffbb2
Daniel <dwmcabinets@yahoo.com>
2023-10-05 19:47:38 +0800

    added sample

--- PyRxStubs/AcadComTypes24.py
+++ PyRxStubs/AcadComTypes24.py
@@ -1,28 +1,28 @@
 # import
 import PyGe as Ge
-import win32com.client as com
+import win32com.client
 import pythoncom
 import ComAcadApplication24 as AcadApp
 
 def getApp() -> AcadApp.IAcadApplication:
     id = AcadApp.AcadApplication.CLSID
-    app: AcadApp.IAcadApplication = com.Dispatch(id)
+    app: AcadApp.IAcadApplication = win32com.client.Dispatch(id)
     return app
 
 def comPnt2d(val: Ge.Point2d):
-    return com.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
+    return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
 
 def comVec2d(val: Ge.Vector2d):
-    return com.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
+    return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
 
 def comMatrix2d(val: Ge.Matrix2d):
-    return com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
+    return win32com.client.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
 
 def comPnt3d(val: Ge.Point3d):
-    return com.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
+    return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
 
 def comVec3d(val: Ge.Vector3d):
-    return com.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
+    return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
 
 def comMatrix3d(val: Ge.Matrix3d):
-    return com.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
+    return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
