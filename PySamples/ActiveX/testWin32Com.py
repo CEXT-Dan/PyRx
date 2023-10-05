c6203c4edafbca389193acc07f349e2eccd93806
Daniel <dwmcabinets@yahoo.com>
2023-10-05 14:19:44 +0800

    update sample

--- PySamples/ActiveX/testWin32Com.py
+++ PySamples/ActiveX/testWin32Com.py
@@ -7,45 +7,55 @@ import PyDb as Db
 import PyAp as Ap
 import PyEd as Ed
 
-#these are in the stub file
-#requires win32com
+# these are in the stub file
+# requires win32com
 import win32com.client
-import AcadComTypes24 as Ac 
+import AcadComTypes24 as Ac
 import ComAcadApplication24 as AcadApp
 
 import traceback
 from timeit import default_timer as timer
 
 
+# optional, global instance of IAcadApplication
+# there's only one app, might as well cache it
 theApp = Ac.getApp()
-#global instance of IAcadApplication, optional
 
-def PyRxCmd_comMenuGroups():
-    try:
-        menuGroups : AcadApp.IAcadMenuGroups = theApp.MenuGroups
-        print("Menu.Count",  menuGroups.Count)
-        
-        menuGroup : AcadApp.IAcadMenuGroup = menuGroups.Item(0)
-        pops : AcadApp.IAcadPopupMenus = menuGroup.Menus
-    
-        for i in range(pops.Count):
-            pop :  AcadApp.IAcadPopupMenu = pops.Item(i)
-            print(pop.Name)
-            
-    except Exception as err:
-        traceback.print_exception(err)
 
 def PyRxCmd_comAddLine():
     try:
         model: AcadApp.IAcadBlock = theApp.ActiveDocument.ModelSpace
-        line : AcadApp.IAcadLine = model.AddLine(
-            Ac.comPnt3d(Ge.Point3d(0, 0, 0)), 
+        line: AcadApp.IAcadLine = model.AddLine(
+            Ac.comPnt3d(Ge.Point3d(0, 0, 0)),
             Ac.comPnt3d(Ge.Point3d(100, 100, 0)))
-        
-        clr : AcadApp.IAcadAcCmColor = AcadApp.AcadAcCmColor()
-        clr.SetRGB(255,255,0)
+
+        clr: AcadApp.IAcadAcCmColor = AcadApp.AcadAcCmColor()
+        clr.SetRGB(255, 255, 0)
         line.TrueColor = clr
-        
+
+        # convert to a wrapper
+        newId = Db.ObjectId()
+        newId.setFromOldId(line.ObjectID)
+
+        rxLine = Db.Line(newId)
+        print(rxLine.startPoint(), rxLine.endPoint())
+
+    except Exception as err:
+        traceback.print_exception(err)
+
+
+def PyRxCmd_comMenuGroups():
+    try:
+        menuGroups: AcadApp.IAcadMenuGroups = theApp.MenuGroups
+        print("Menu.Count",  menuGroups.Count)
+
+        menuGroup: AcadApp.IAcadMenuGroup = menuGroups.Item(0)
+        pops: AcadApp.IAcadPopupMenus = menuGroup.Menus
+
+        for i in range(pops.Count):
+            pop: AcadApp.IAcadPopupMenu = pops.Item(i)
+            print(pop.Name)
+
     except Exception as err:
         traceback.print_exception(err)
 
@@ -74,20 +84,20 @@ def PyRxCmd_comGetang():
 
     except Exception as err:
         traceback.print_exception(err)
-        
-#0.9011179999997694
+
+# 0.9011179999997694!
 def PyRxCmd_comPerf():
     try:
         start = timer()
-        
+
         mat = Ge.Matrix3d()
-        mat.setToTranslation(Ge.Point3d(100,100,0).asVector())
+        mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
         comMat = Ac.comMatrix3d(mat)
-        
+
         for ent in theApp.ActiveDocument.ModelSpace:
             if ent.ObjectName == "AcDbPoint":
                 ent.TransformBy(comMat)
-                
+
         end = timer()
         print(end - start)
 
