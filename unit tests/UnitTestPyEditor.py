29e16d6652b50fe11e974598f51a8bcae6ca9668
Daniel <dwmcabinets@yahoo.com>
2023-10-06 16:02:47 +0800

    added test for autoSysVar

--- unit tests/UnitTestPyEditor.py
+++ unit tests/UnitTestPyEditor.py
@@ -11,6 +11,11 @@ import PyEd as Ed
 
 print("testname = pyeditor")
 
+def autoSysVarOSMODE():
+    var = Ed.Core.autoSetVar("OSMODE", 0)
+    
+def autoSysVarHPORIGIN():
+    var = Ed.Core.autoSetVar("HPORIGIN", Ge.Point2d(100, 100))
 
 class TestEditor(unittest.TestCase):
 
@@ -20,8 +25,14 @@ class TestEditor(unittest.TestCase):
         self.assertEqual(Ed.Core.getVar("HPORIGIN"), Ge.Point2d(0, 0))
         self.assertEqual(Ed.Core.getVar("UCSORG"), Ge.Point3d(0, 0, 0))
         self.assertEqual(Ed.Core.getVar("DIMTXSTY"),  "Standard")
-
-
+        
+    def test_autosysvar(self):
+        Ed.Core.setVar("OSMODE", 191)
+        autoSysVarOSMODE()
+        self.assertEqual(Ed.Core.getVar("OSMODE"), 191)
+        autoSysVarHPORIGIN()
+        self.assertEqual(Ed.Core.getVar("HPORIGIN"), Ge.Point2d(0, 0))
+        
 def PyRxCmd_pyeditor():
     try:
         suite = unittest.TestLoader().loadTestsFromTestCase(TestEditor)
