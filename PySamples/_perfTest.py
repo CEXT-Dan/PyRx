b55c306ebc2685540b1b2f029786b62704ea6615
Daniel <dwmcabinets@yahoo.com>
2023-10-06 08:35:22 +0800

    updated test

--- PySamples/_perfTest.py
+++ PySamples/_perfTest.py
@@ -10,31 +10,10 @@ import timeit
 #run this before release!!!
 #PySamples\dwg\TestPoints.dwg
 
-def move_points():
-    mat = Ge.Matrix3d()
-    mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
+import AcadComTypes24 as Ac
+import ComAcadApplication24 as AcadApp
 
-    db = Db.HostApplicationServices().workingDatabase()
-    model = Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.kForRead)
-
-    desc = Db.Point.desc()
-    for id in model.objectIds():
-        if id.isDerivedFrom(desc):
-            ent = Db.Entity(id, Db.OpenMode.ForWrite)
-            ent.transformBy(mat)
-
-def move_points_new():
-    mat = Ge.Matrix3d()
-    mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
-
-    db = Db.HostApplicationServices().workingDatabase()
-    model = Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.kForRead)
-
-    desc = Db.Point.desc()
-    for id in model.objectIds(desc):
-        ent = Db.Entity(id, Db.OpenMode.ForWrite)
-        ent.transformBy(mat)
-
+# calls Db.Point.desc() every loop
 def move_points_old():
     try:
         mat = Ge.Matrix3d()
@@ -51,11 +30,52 @@ def move_points_old():
     except Exception as err:
         print(err)
 
+# caches Db.Point.desc()
+def move_points():
+    mat = Ge.Matrix3d()
+    mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
+
+    db = Db.HostApplicationServices().workingDatabase()
+    model = Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.kForRead)
+
+    desc = Db.Point.desc()
+    for id in model.objectIds():
+        if id.isDerivedFrom(desc):
+            ent = Db.Entity(id, Db.OpenMode.ForWrite)
+            ent.transformBy(mat)
+
+# uses model.objectIds(desc) instead of 
+# if id.isDerivedFrom(desc):
+def move_points_new():
+    mat = Ge.Matrix3d()
+    mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
+
+    db = Db.curDb()
+    model = Db.BlockTableRecord(db.modelSpaceId())
+
+    for id in model.objectIds(Db.Point.desc()):
+        ent = Db.Entity(id, Db.OpenMode.ForWrite)
+        ent.transformBy(mat)
+
+
+theApp = Ac.getApp()
+
+def move_points_com():
+    
+        mat = Ge.Matrix3d()
+        mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
+        comMat = Ac.comMatrix3d(mat)
+
+        for ent in theApp.ActiveDocument.ModelSpace:
+            if ent.ObjectName == "AcDbPoint":
+                ent.TransformBy(comMat)
+
 
 print("OldTest:..")
 print("test move_points_new....     time = 3.7837103999991086")
 print("test move_points........     time = 3.9697672000038438")
 print("test move_points_old....     time = 4.44495850000385")
+print("test move_points_com....     time = 20.55892379999932")
 
 print("\nrun command pyperftest")
 
@@ -64,6 +84,7 @@ def PyRxCmd_pyperftest():
         print("test move_points_new....\t time = {}".format(timeit.timeit(move_points_new, number=20)))
         print("test move_points........\t time = {}".format(timeit.timeit(move_points, number=20)))
         print("test move_points_old....\t time = {}".format(timeit.timeit(move_points_old, number=20)))
+        print("test move_points_com....\t time = {}".format(timeit.timeit(move_points_com, number=20)))
     except Exception as err:
         print(err)
 
