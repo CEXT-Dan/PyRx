a8ce26726c45f27f9b3b6f926dc054d5faa8e831
Daniel <dwmcabinets@yahoo.com>
2023-10-08 21:16:33 +0800

    updated sample

--- PySamples/Play/Delaunator/DelaunatorCmdX.py
+++ PySamples/Play/Delaunator/DelaunatorCmdX.py
@@ -27,34 +27,30 @@ def get_2dpointd(pnt3ds):
 
 def PyRxCmd_pydelaunatorx():
     try:
+        
         doc = app.ActiveDocument
         ss = doc.SelectionSets.Add('mysset')
- 
         ss.SelectOnScreen(AcUtils.ssfilterType([0]),
                           AcUtils.ssfilterData(["POINT"]))
         
         t1_start = perf_counter()
         pnt3ds = get_3dpointds(ss)
         pnt2ds = get_2dpointd(pnt3ds)
+        
+        #doit
         t = Delaunator(pnt2ds).triangles
         
         model = doc.ModelSpace
         
-        cnt = len(t)
-        for i in range(0, cnt, 3):
-            ca =  AcUtils.coords(pnt3ds[t[i]])   
-            cb =  AcUtils.coords(pnt3ds[t[i + 1]])
-            cc =  AcUtils.coords(pnt3ds[t[i + 2]]) 
-                      
-            model.Add3DFace(
-                AcUtils.coords(ca),
-                AcUtils.coords(cb),
-                AcUtils.coords(cc),
-                AcUtils.coords(ca))
+        for i in range(0, len(t), 3):
+            a = AcUtils.coords(pnt3ds[t[i]])
+            b = AcUtils.coords(pnt3ds[t[i + 1]])
+            c = AcUtils.coords(pnt3ds[t[i + 2]])
+            
+            model.Add3DFace(a,b,c,a)
 
         t1_stop = perf_counter()
-        r = "Elapsed time: {t:.4f}".format(t=t1_stop-t1_start)
-        print(r)
+        print("Elapsed time: {t:.4f}".format(t=t1_stop-t1_start))
     except Exception as err:
         traceback.print_exception(err)
     finally:
