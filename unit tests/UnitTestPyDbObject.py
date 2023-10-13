39dcc0e547546ab09b8881c83cbe3ca5f4d32662
Daniel <dwmcabinets@yahoo.com>
2023-10-13 18:04:09 +0800

    update test

--- unit tests/UnitTestPyDbObject.py
+++ unit tests/UnitTestPyDbObject.py
@@ -1,82 +1,76 @@
-import os 
+import os
 import unittest
 
-import PyRx# = Runtime runtime 
-import PyGe# = Geometry
-import PyGi# = Graphics interface
-import PyDb# = database
-import PyAp# = application, document classes services 
-import PyEd# = editor 
+import PyRx  # = Runtime runtime
+import PyGe  # = Geometry
+import PyGi  # = Graphics interface
+import PyDb  # = database
+import PyAp  # = application, document classes services
+import PyEd  # = editor
 print("testname = pydbobject")
 
 
 class TestDbObject(unittest.TestCase):
-        def test_property_ids(self):
-                id = PyDb.HostApplicationServices().workingDatabase().blockTableId()
-                bdo = PyDb.DbObject(id, PyDb.OpenMode.ForRead)
-                self.assertEqual(bdo.className(),"AcDbObject") 
-                self.assertEqual(bdo.isA().name(),"AcDbBlockTable") 
-                self.assertEqual(bdo.isReadEnabled(), True) 
-                self.assertEqual(bdo.isWriteEnabled(), False) 
-                self.assertEqual(bdo.isAProxy(), False) 
-                self.assertEqual(bdo.isNewObject(), False)
-                #
-                bdo.upgradeOpen()
-                self.assertEqual(bdo.isWriteEnabled(), True) 
-                #
-                bdo.downgradeOpen()
-                self.assertEqual(bdo.isWriteEnabled(), False)
-                #
-                self.assertEqual(bdo.database(), PyDb.HostApplicationServices().workingDatabase())  
-                #
-                bdo.close()
-                self.assertEqual(bdo.isReadEnabled(), False)
-                
-        def test_propertys(self):
-                db = PyDb.HostApplicationServices().workingDatabase()
-                model = PyDb.BlockTableRecord(db.modelSpaceId(),PyDb.OpenMode.ForRead)
-                oid = model.ownerId()
-                dbo = PyDb.DbObject(oid, PyDb.OpenMode.ForRead)
-                self.assertEqual(dbo.isA().name(),"AcDbBlockTable") 
-                self.assertEqual(dbo.database(),db) 
-                self.assertEqual(dbo.isAProxy(),False) 
-                self.assertEqual(dbo.isErased(),False)
-                dbo.disableUndoRecording(True)
-                self.assertEqual(dbo.isUndoRecordingDisabled(),True)
-                dbo.disableUndoRecording(False)
-                self.assertEqual(dbo.isUndoRecordingDisabled(),False)
+    def test_property_ids(self):
+        id = PyDb.HostApplicationServices().workingDatabase().blockTableId()
+        bdo = PyDb.DbObject(id, PyDb.OpenMode.ForRead)
+        self.assertEqual(bdo.className(), "AcDbObject")
+        self.assertEqual(bdo.isA().name(), "AcDbBlockTable")
+        self.assertEqual(bdo.isReadEnabled(), True)
+        self.assertEqual(bdo.isWriteEnabled(), False)
+        self.assertEqual(bdo.isAProxy(), False)
+        self.assertEqual(bdo.isNewObject(), False)
+        #
+        bdo.upgradeOpen()
+        self.assertEqual(bdo.isWriteEnabled(), True)
+        #
+        bdo.downgradeOpen()
+        self.assertEqual(bdo.isWriteEnabled(), False)
+        #
+        self.assertEqual(
+            bdo.database(), PyDb.HostApplicationServices().workingDatabase())
+        #
+        bdo.close()
+        self.assertEqual(bdo.isReadEnabled(), False)
 
-        def test_xdata(self):
-                #regapp
-                success = PyDb.Core.regApp("PYTHONTEST")
-                self.assertEqual(success, True)
+    def test_propertys(self):
+        db = PyDb.HostApplicationServices().workingDatabase()
+        model = PyDb.BlockTableRecord(db.modelSpaceId(), PyDb.OpenMode.ForRead)
+        oid = model.ownerId()
+        dbo = PyDb.DbObject(oid, PyDb.OpenMode.ForRead)
+        self.assertEqual(dbo.isA().name(), "AcDbBlockTable")
+        self.assertEqual(dbo.database(), db)
+        self.assertEqual(dbo.isAProxy(), False)
+        self.assertEqual(dbo.isErased(), False)
+        dbo.disableUndoRecording(True)
+        self.assertEqual(dbo.isUndoRecordingDisabled(), True)
+        dbo.disableUndoRecording(False)
+        self.assertEqual(dbo.isUndoRecordingDisabled(), False)
 
-                #set
-                id = PyDb.HostApplicationServices().workingDatabase().blockTableId()
-                dbo = PyDb.DbObject(id, PyDb.OpenMode.kForWrite)
-                self.assertEqual(dbo.isWriteEnabled(), True) 
-                xd = [(PyDb.DxfCode.kDxfRegAppName, "PYTHONTEST"),(PyDb.DxfCode.kDxfXdXCoord, PyGe.Point3d(1,10,100))]
-                dbo.setXData(xd)
-                
-                #get
-                xdres = dbo.xData("PYTHONTEST")
-                p = xdres[1][1]
-                self.assertEqual(p.x, 1)
-                self.assertEqual(p.y, 10)
-                self.assertEqual(p.z, 100)
-                
-                
-                
-               
+    def test_xdata(self):
+        # regapp
+        success = PyDb.Core.regApp("PYTHONTEST")
+        self.assertEqual(success, True)
+        # set
+        id = PyDb.HostApplicationServices().workingDatabase().blockTableId()
+        dbo = PyDb.DbObject(id, PyDb.OpenMode.kForWrite)
+        self.assertEqual(dbo.isWriteEnabled(), True)
+        xd = [(PyDb.DxfCode.kDxfRegAppName, "PYTHONTEST"),
+              (PyDb.DxfCode.kDxfXdXCoord, PyGe.Point3d(1, 10, 100))]
+        dbo.setXData(xd)
+
+        # get
+        xdres = dbo.xData("PYTHONTEST")
+        p = xdres[1][1]
+        self.assertEqual(p.x, 1)
+        self.assertEqual(p.y, 10)
+        self.assertEqual(p.z, 100)
 
 
 def PyRxCmd_pydbobject():
-        try:
-                suite = unittest.TestLoader().loadTestsFromTestCase(TestDbObject)
-                print(unittest.TextTestRunner(verbosity=0).run(suite))
-        except Exception as err:
-                print.Printf(err)
-
-
-    
-
+    try:
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestDbObject)
+        print('pydbobject')
+        print(unittest.TextTestRunner(verbosity=0).run(suite))
+    except Exception as err:
+        print.Printf(err)
