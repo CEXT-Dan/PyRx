7dce82b6952b55c96441ba9b3e1c44960d7aba10
Daniel <dwmcabinets@yahoo.com>
2023-10-13 20:29:22 +0800

    stubs

--- PyRxStubs/PyDb.pyi
+++ PyRxStubs/PyDb.pyi
@@ -13162,6 +13162,155 @@ __init__( (object)arg1) -> None :
       '''                             '''
     ...
 
+class DeepCloneType:
+    def __init__ (self, /, *args, **kwargs) :
+      '''Initialize self.  See help(type(self)) for accurate signature.'''
+    ...
+    def as_integer_ratio (self, /) :
+      '''Return integer ratio.
+
+Return a pair of integers, whose ratio is exactly equal to the original int
+and with a positive denominator.
+
+>>> (10).as_integer_ratio()
+(10, 1)
+>>> (-10).as_integer_ratio()
+(-10, 1)
+>>> (0).as_integer_ratio()
+(0, 1)'''
+    ...
+    def bit_count (self, /) :
+      '''Number of ones in the binary representation of the absolute value of self.
+
+Also known as the population count.
+
+>>> bin(13)
+'0b1101'
+>>> (13).bit_count()
+3'''
+    ...
+    def bit_length (self, /) :
+      '''Number of bits necessary to represent self in binary.
+
+>>> bin(37)
+'0b100101'
+>>> (37).bit_length()
+6'''
+    ...
+    def conjugate (self, *args, **kwargs)-> None :
+      '''Returns self, the complex conjugate of any int.'''
+    ...
+    def denominator (self, *args, **kwargs)-> None :
+      '''the denominator of a rational number in lowest terms'''
+    ...
+    def from_bytes (bytes, byteorder, *, signed=False) :
+      '''Return the integer represented by the given array of bytes.
+
+  bytes
+    Holds the array of bytes to convert.  The argument must either
+    support the buffer protocol or be an iterable object producing bytes.
+    Bytes and bytearray are examples of built-in objects that support the
+    buffer protocol.
+  byteorder
+    The byte order used to represent the integer.  If byteorder is 'big',
+    the most significant byte is at the beginning of the byte array.  If
+    byteorder is 'little', the most significant byte is at the end of the
+    byte array.  To request the native byte order of the host system, use
+    `sys.byteorder' as the byte order value.
+  signed
+    Indicates whether two's complement is used to represent the integer.'''
+    ...
+    def imag (self, *args, **kwargs)-> None :
+      '''the imaginary part of a complex number'''
+    ...
+    def kDcBlock (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcCopy (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcExplode (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcInsert (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcInsertCopy (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcObjects (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcSymTableMerge (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcWblkObjects (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcWblock (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcXrefBind (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def kDcXrefInsert (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def name (self, *args, **kwargs)-> None :
+      '''None'''
+    ...
+    def names (self, *args, **kwargs)-> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via :
+      '''dict() -> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via:
+    d = {}
+    for k, v in iterable:
+        d[k] = v
+dict(**kwargs) -> new dictionary initialized with the name=value pairs
+    in the keyword argument list.  For example:  dict(one=1, two=2)'''
+    ...
+    def numerator (self, *args, **kwargs)-> None :
+      '''the numerator of a rational number in lowest terms'''
+    ...
+    def real (self, *args, **kwargs)-> None :
+      '''the real part of a complex number'''
+    ...
+    def to_bytes (self, /, length, byteorder, *, signed=False) :
+      '''Return an array of bytes representing an integer.
+
+  length
+    Length of bytes object to use.  An OverflowError is raised if the
+    integer is not representable with the given number of bytes.
+  byteorder
+    The byte order used to represent the integer.  If byteorder is 'big',
+    the most significant byte is at the beginning of the byte array.  If
+    byteorder is 'little', the most significant byte is at the end of the
+    byte array.  To request the native byte order of the host system, use
+    `sys.byteorder' as the byte order value.
+  signed
+    Determines whether two's complement is used to represent the integer.
+    If signed is False and a negative integer is given, an OverflowError
+    is raised.'''
+    ...
+    def values (self, *args, **kwargs)-> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via :
+      '''dict() -> new empty dictionary
+dict(mapping) -> new dictionary initialized from a mapping object's
+    (key, value) pairs
+dict(iterable) -> new dictionary initialized as if via:
+    d = {}
+    for k, v in iterable:
+        d[k] = v
+dict(**kwargs) -> new dictionary initialized with the name=value pairs
+    in the keyword argument list.  For example:  dict(one=1, two=2)'''
+    ...
+
 class DiametricDimension:
     def __init__ (self, *args, **kwargs)-> None :
       '''__init__( (object)arg1) -> None :
@@ -29321,8 +29470,8 @@ __init__( (object)arg1) -> None :
     C++ signature :
         bool compute(class PyDbIdMapping {lvalue},class PyIdPair {lvalue})'''
     ...
-    def deepCloneContext (self, *args, **kwargs)-> object :
-      '''deepCloneContext( (IdMapping)arg1) -> object :
+    def deepCloneContext (self, *args, **kwargs)-> PyDb.DeepCloneType :
+      '''deepCloneContext( (IdMapping)arg1) -> DeepCloneType :
 
     C++ signature :
         enum AcDb::DeepCloneType deepCloneContext(class PyDbIdMapping {lvalue})'''
