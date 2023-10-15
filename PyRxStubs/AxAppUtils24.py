198c8f10314f30dca07c4beec8baa5fe60eb4eb9
Daniel <dwmcabinets@yahoo.com>
2023-10-15 17:29:47 +0800

    added utility function to transform the matrix from axDoc.Utility.GetSubEntity to a Matrix3d

--- PyRxStubs/AxAppUtils24.py
+++ PyRxStubs/AxAppUtils24.py
@@ -20,3 +20,13 @@ def vec3d(val: Ge.Vector3d):
 
 def matrix3d(val: Ge.Matrix3d):
     return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, val.toList())
+
+# not sure if this is correct
+def nestedXformToMatrix3d(xmat:[float])->Ge.Matrix3d:
+        mat = Ge.Matrix3d()
+        o = Ge.Point3d(xmat[3][0],xmat[3][1],xmat[3][2])
+        x = Ge.Vector3d(xmat[0][0],xmat[0][1],xmat[0][2])
+        y = Ge.Vector3d(xmat[1][0],xmat[1][1],xmat[1][2])
+        z = Ge.Vector3d(xmat[2][0],xmat[2][1],xmat[2][2])
+        mat.setCoordSystem(o,x,y,z)
+        return mat
