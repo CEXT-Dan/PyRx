984fc13a3211288fea6ec64c5f50be2d8d9b6501
Daniel <dwmcabinets@yahoo.com>
2023-10-18 07:30:00 +0800

    updated sample

--- PySamples/ActiveX/testacomoutofproccess.py
+++ PySamples/ActiveX/testacomoutofproccess.py
@@ -1,31 +1,15 @@
 
-import pythoncom
-import win32com.client
 from timeit import default_timer as timer
 
-import AxApp24
+import AxApp24 as Ax
 
-def comMatrix3d(mat):
-        return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, mat)
-    
-def getApp() -> AxApp24.IAcadApplication:
-    id = AxApp24.AcadApplication.CLSID
-    app: AxApp24.IAcadApplication = win32com.client.Dispatch(id)
-    return app
-
-def getDbx() -> AxApp24.IAxDbDocument:
-    id = AxApp24.AxDbDocument.CLSID
-    dbx: AxApp24.IAxDbDocument = win32com.client.Dispatch(id)
-    return dbx
-    
 start = timer()
-comMat = comMatrix3d(
-    [[1.0, 0.0, 0.0, 100.0],
-     [0.0, 1.0, 0.0, 100.0], 
-     [0.0, 0.0, 1.0, 0.0], 
-     [0.0, 0.0, 0.0, 1.0]])
+comMat = [[1.0, 0.0, 0.0, 100.0],
+          [0.0, 1.0, 0.0, 100.0],
+          [0.0, 0.0, 1.0, 0.0],
+          [0.0, 0.0, 0.0, 1.0]]
 
-acad = getApp()
+acad = Ax.getApp()
 model = acad.ActiveDocument.ModelSpace
 
 for ent in model:
@@ -34,4 +18,3 @@ for ent in model:
         
 end = timer()
 print(end - start)
-
