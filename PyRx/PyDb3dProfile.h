14ed0317406986b033ebee980a0ad10322c1e27c
Daniel <dwmcabinets@yahoo.com>
2023-10-29 09:37:43 +0800

    progress on PyDb3dProfile

--- PyRx/PyDb3dProfile.h
+++ PyRx/PyDb3dProfile.h
@@ -15,7 +15,15 @@ public:
     PyDb3dProfile(const AcDb3dProfile& src);
     PyDb3dProfile(AcDb3dProfile* ptr, bool autoDelete);
     virtual ~PyDb3dProfile() override = default;
- 
+
+    PyDbEntity  entity() const;
+    bool        isClosed() const;
+    bool        isPlanar() const;
+    bool        isSubent() const;
+    bool        isFace() const;
+    bool        isEdge() const;
+    bool        isValid() const;
+
     static PyRxClass    desc();
     static std::string  className();
 
