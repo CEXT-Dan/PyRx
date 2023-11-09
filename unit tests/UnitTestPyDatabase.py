90de5f2a9a2f02729a5f859e6d413f768aab82c1
Daniel <dwmcabinets@yahoo.com>
2023-11-09 08:15:08 +0800

    version to 1.1.145 update test due to source changes update stubs

--- unit tests/UnitTestPyDatabase.py
+++ unit tests/UnitTestPyDatabase.py
@@ -186,11 +186,11 @@ class TestDatabase(unittest.TestCase):
         
     def test_getallobjectidsfromdwg(self):
         ids = self.db06457.objectIds()
-        self.assertEqual(len(ids), 23176)
+        self.assertEqual(len(ids), 26674)
         
     def test_getallobjectidsfromdwgdesc(self):
         ids = self.db06457.objectIds(Db.BlockReference.desc())
-        self.assertEqual(len(ids), 1825 )
+        self.assertEqual(len(ids), 1826 )
 
 
 def PyRxCmd_pydbtest():
