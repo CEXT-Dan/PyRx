6c7bf5a39eb29aeffa63a9d1dd9239aaa0217b57
Daniel <dwmcabinets@yahoo.com>
2023-11-18 08:58:38 +0800

    work on PyGeCurveCurveInt3d

--- PyRx/PyGeCurveCurveInt3d.h
+++ PyRx/PyGeCurveCurveInt3d.h
@@ -2,6 +2,8 @@
 #include "PyGeEntity3d.h"
 
 class PyGeInterval;
+class PyGeCurve3d;
+class PyGePointOnCurve3d;
 
 //-----------------------------------------------------------------------------------------
 //PyGeCurveCurveInt3d wrapper
@@ -11,7 +13,35 @@ class PyGeCurveCurveInt3d : public PyGeEntity3d
 {
 public:
     PyGeCurveCurveInt3d();
+    PyGeCurveCurveInt3d(const AcGeCurveCurveInt3d& src);
+    PyGeCurveCurveInt3d(const PyGeCurve3d& curve1, const PyGeCurve3d& curve2);
+    PyGeCurveCurveInt3d(const PyGeCurve3d& curve1, const PyGeCurve3d& curve2, const AcGeVector3d& planeNormal,const AcGeTol& tol);
+    PyGeCurveCurveInt3d(const PyGeCurve3d& curve1, const PyGeCurve3d& curve2, const PyGeInterval& range1, const PyGeInterval& range2);
+    PyGeCurveCurveInt3d(const PyGeCurve3d& curve1, const PyGeCurve3d& curve2, const PyGeInterval& range1, const PyGeInterval& range2,const AcGeVector3d& planeNormal, const AcGeTol& tol);
     PyGeCurveCurveInt3d(AcGeEntity3d* pEnt);
+
+    PyGeCurve3d             curve1() const;
+    PyGeCurve3d             curve2() const;
+    boost::python::tuple    getIntRanges();
+    AcGeVector3d	        planeNormal() const;
+    AcGeTol                 tolerance() const;
+    int                     numIntPoints() const;
+    AcGePoint3d             intPoint(int intNum) const;
+    boost::python::tuple    getIntParams(int intNum) const;
+    PyGePointOnCurve3d      getPointOnCurve1(int intNum) const;
+    PyGePointOnCurve3d      getPointOnCurve2(int intNum) const;
+
+    boost::python::tuple    getIntConfigs(int intNum) const;
+    Adesk::Boolean          isTangential(int intNum) const;
+    Adesk::Boolean          isTransversal(int intNum) const;
+    double                  intPointTol(int intNum) const;
+    int                     overlapCount() const;
+    Adesk::Boolean	        overlapDirection() const;
+    boost::python::tuple    getOverlapRanges(int overlapNum) const;
+    void                    changeCurveOrder();
+    PyGeCurveCurveInt3d     orderWrt1();
+    PyGeCurveCurveInt3d     orderWrt2();
+
     static PyGeCurveCurveInt3d cast(const PyGeEntity3d& src);
     static PyGeCurveCurveInt3d copycast(const PyGeEntity3d& src);
     static std::string className();
