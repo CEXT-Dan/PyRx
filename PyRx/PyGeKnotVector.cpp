4caca6b52b77bf1bb0f1906f6a1456bc4e84e822
Daniel <dwmcabinets@yahoo.com>
2023-11-20 08:18:41 +0800

    work on PyGeKnotVector

--- PyRx/PyGeKnotVector.cpp
+++ PyRx/PyGeKnotVector.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "PyGeKnotVector.h"
+#include "PyGeInterval.h"
 
 using namespace boost::python;
 
@@ -16,9 +17,70 @@ PyGeKnotVector::PyGeKnotVector()
 {
 }
 
+PyGeKnotVector::PyGeKnotVector(int size, int growSize)
+    : m_imp(size, growSize)
+{
+}
+
 PyGeKnotVector::PyGeKnotVector(const AcGeKnotVector& src)
     : m_imp(src)
 {
+}
+
+PyGeKnotVector::PyGeKnotVector(const boost::python::list& dbls)
+    : m_imp(PyListToDoubleArray(dbls))
+{
+}
+
+PyGeKnotVector::PyGeKnotVector(int plusMult, const PyGeKnotVector& src)
+    : m_imp(plusMult, src.m_imp)
+{
+}
+
+Adesk::Boolean PyGeKnotVector::isEqualTo(const PyGeKnotVector& other) const
+{
+    return m_imp.isEqualTo(other.m_imp);
+}
+
+double PyGeKnotVector::startParam() const
+{
+    return m_imp.startParam();
+}
+
+double PyGeKnotVector::endParam() const
+{
+    return m_imp.endParam();
+}
+
+int PyGeKnotVector::multiplicityAt(int i) const
+{
+    return m_imp.multiplicityAt(i);
+}
+
+int PyGeKnotVector::multiplicityAtParam(double param) const
+{
+    return m_imp.multiplicityAt(param);
+}
+
+int PyGeKnotVector::numIntervals() const
+{
+    return m_imp.numIntervals();
+}
+
+boost::python::tuple PyGeKnotVector::getInterval(int ord, double par) const
+{
+    PyAutoLockGIL lock;
+    PyGeInterval interval;
+    auto res = m_imp.getInterval(ord, par, interval.imp);
+    return boost::python::make_tuple(res, interval);
+}
+
+boost::python::list PyGeKnotVector::getDistinctKnots() const
+{
+    PyAutoLockGIL lock;
+    AcGeDoubleArray knots;
+    m_imp.getDistinctKnots(knots);
+    return DoubleArrayToPyList(knots);
 }
 
 int PyGeKnotVector::append(double val)
--- PyRx/PyGeKnotVector.h
+++ PyRx/PyGeKnotVector.h
@@ -8,8 +8,23 @@ class PyGeKnotVector
 {
 public:
     PyGeKnotVector();
+    PyGeKnotVector(int size, int growSize);
     PyGeKnotVector(const AcGeKnotVector& src);
+    PyGeKnotVector(const boost::python::list& dbls);
+    PyGeKnotVector(int plusMult, const PyGeKnotVector& src);
     ~PyGeKnotVector() = default;
+
+    Adesk::Boolean          isEqualTo(const PyGeKnotVector& other) const;
+    double                  startParam() const;
+    double                  endParam() const;
+    int                     multiplicityAt(int i) const;
+    int                     multiplicityAtParam(double param) const;
+    int                     numIntervals() const;
+    boost::python::tuple    getInterval(int ord, double par) const;
+    boost::python::list     getDistinctKnots() const;
+
+
+
     int                 append(double val);
 
     static std::string className();
