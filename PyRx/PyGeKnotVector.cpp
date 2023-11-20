757a153d930d0ff72f588645b1cc63e3fd8d6082
Daniel <dwmcabinets@yahoo.com>
2023-11-20 08:50:06 +0800

    PyGeKnotVector and stubs

--- PyRx/PyGeKnotVector.cpp
+++ PyRx/PyGeKnotVector.cpp
@@ -7,7 +7,41 @@ using namespace boost::python;
 void makePyGeKnotVectorWrapper()
 {
     class_<PyGeKnotVector>("KnotVector")
-        .def("append", &PyGeKnotVector::append)
+        .def(init<>())
+        .def(init<int, int>())
+        .def(init<const boost::python::list&>())
+        .def(init<int, const PyGeKnotVector&>())
+        .def("isEqualTo", &PyGeKnotVector::isEqualTo)
+        .def("startParam", &PyGeKnotVector::startParam)
+        .def("endParam", &PyGeKnotVector::endParam)
+        .def("multiplicityAt", &PyGeKnotVector::multiplicityAt)
+        .def("multiplicityAtParam", &PyGeKnotVector::multiplicityAtParam)
+        .def("numIntervals", &PyGeKnotVector::numIntervals)
+        .def("getInterval", &PyGeKnotVector::getInterval)
+        .def("getDistinctKnots", &PyGeKnotVector::getDistinctKnots)
+        .def("contains", &PyGeKnotVector::contains)
+        .def("isOn", &PyGeKnotVector::isOn)
+        .def("reverse", &PyGeKnotVector::reverse)
+        .def("removeAt", &PyGeKnotVector::removeAt)
+        .def("removeSubVector", &PyGeKnotVector::removeSubVector)
+        .def("insertAt", &PyGeKnotVector::insertAt1)
+        .def("insertAt", &PyGeKnotVector::insertAt2)
+        .def("insert", &PyGeKnotVector::insert)
+        .def("append", &PyGeKnotVector::append1)
+        .def("append", &PyGeKnotVector::append2)
+        .def("append", &PyGeKnotVector::append3)
+        .def("split", &PyGeKnotVector::split)
+        .def("setRange", &PyGeKnotVector::setRange)
+        .def("tolerance", &PyGeKnotVector::tolerance)
+        .def("setTolerance", &PyGeKnotVector::setTolerance)
+        .def("length", &PyGeKnotVector::length)
+        .def("isEmpty", &PyGeKnotVector::isEmpty)
+        .def("logicalLength", &PyGeKnotVector::logicalLength)
+        .def("setLogicalLength", &PyGeKnotVector::setLogicalLength)
+        .def("physicalLength", &PyGeKnotVector::physicalLength)
+        .def("setPhysicalLength", &PyGeKnotVector::setPhysicalLength)
+        .def("growLength", &PyGeKnotVector::growLength)
+        .def("setGrowLength", &PyGeKnotVector::setGrowLength)
         .def("className", &PyGeKnotVector::className).staticmethod("className")
         ;
 }
@@ -83,9 +117,123 @@ boost::python::list PyGeKnotVector::getDistinctKnots() const
     return DoubleArrayToPyList(knots);
 }
 
-int PyGeKnotVector::append(double val)
+Adesk::Boolean PyGeKnotVector::contains(double param) const
+{
+    return m_imp.contains(param);
+}
+
+Adesk::Boolean PyGeKnotVector::isOn(double knot) const
+{
+    return m_imp.isOn(knot);
+}
+
+void PyGeKnotVector::reverse()
+{
+    m_imp.reverse();
+}
+
+void PyGeKnotVector::removeAt(int index)
+{
+    m_imp.removeAt(index);
+}
+
+void PyGeKnotVector::removeSubVector(int startIndex, int endIndex)
+{
+    m_imp.removeSubVector(startIndex, endIndex);
+}
+
+void PyGeKnotVector::insertAt1(int indx, double u)
+{
+    m_imp.insertAt(indx, u);
+}
+
+void PyGeKnotVector::insertAt2(int indx, double u, int multiplicity)
+{
+    m_imp.insertAt(indx, u, multiplicity);
+}
+
+void PyGeKnotVector::insert(double u)
+{
+    m_imp.insert(u);
+}
+
+int PyGeKnotVector::append1(double val)
 {
     return m_imp.append(val);
+}
+
+void PyGeKnotVector::append2(PyGeKnotVector& tail)
+{
+    m_imp.append(tail.m_imp);
+}
+
+void PyGeKnotVector::append3(PyGeKnotVector& tail, double knotRatio)
+{
+    m_imp.append(tail.m_imp, knotRatio);
+}
+
+boost::python::tuple PyGeKnotVector::split(double par, int multLast, int multFirst) const
+{
+    PyAutoLockGIL lock;
+    PyGeKnotVector pKnot1;
+    PyGeKnotVector pKnot2;
+    m_imp.split(par, &pKnot1.m_imp, multLast, &pKnot2.m_imp, multFirst);
+    return boost::python::make_tuple(pKnot1, pKnot2);
+}
+
+void PyGeKnotVector::setRange(double lower, double upper)
+{
+    m_imp.setRange(lower, upper);
+}
+
+double PyGeKnotVector::tolerance() const
+{
+    return m_imp.tolerance();
+}
+
+void PyGeKnotVector::setTolerance(double tol)
+{
+    m_imp.setTolerance(tol);
+}
+
+int PyGeKnotVector::length() const
+{
+    return m_imp.length();
+}
+
+Adesk::Boolean PyGeKnotVector::isEmpty() const
+{
+    return m_imp.isEmpty();
+}
+
+int PyGeKnotVector::logicalLength() const
+{
+    return m_imp.logicalLength();
+}
+
+void PyGeKnotVector::setLogicalLength(int len)
+{
+    m_imp.setLogicalLength(len);
+}
+
+int PyGeKnotVector::physicalLength() const
+{
+    return m_imp.physicalLength();
+}
+
+void PyGeKnotVector::setPhysicalLength(int len)
+{
+    m_imp.setPhysicalLength(len);
+}
+
+int PyGeKnotVector::growLength() const
+{
+    return m_imp.growLength();
+}
+
+void PyGeKnotVector::setGrowLength(int len)
+{
+    m_imp.setGrowLength(len);
 }
 
 std::string PyGeKnotVector::className()
--- PyRx/PyGeKnotVector.h
+++ PyRx/PyGeKnotVector.h
@@ -13,7 +13,6 @@ public:
     PyGeKnotVector(const boost::python::list& dbls);
     PyGeKnotVector(int plusMult, const PyGeKnotVector& src);
     ~PyGeKnotVector() = default;
-
     Adesk::Boolean          isEqualTo(const PyGeKnotVector& other) const;
     double                  startParam() const;
     double                  endParam() const;
@@ -22,12 +21,30 @@ public:
     int                     numIntervals() const;
     boost::python::tuple    getInterval(int ord, double par) const;
     boost::python::list     getDistinctKnots() const;
-
-
-
-    int                 append(double val);
-
-    static std::string className();
+    Adesk::Boolean          contains(double param) const;
+    Adesk::Boolean          isOn(double knot) const;
+    void                    reverse();
+    void                    removeAt(int index);
+    void                    removeSubVector(int startIndex, int endIndex);
+    void                    insertAt1(int indx, double u);
+    void                    insertAt2(int indx, double u, int multiplicity);
+    void                    insert(double u);
+    int                     append1(double val);
+    void                    append2(PyGeKnotVector& tail);
+    void                    append3(PyGeKnotVector& tail, double knotRatio);
+    boost::python::tuple    split(double par, int multLast, int multFirst) const;
+    void                    setRange(double lower, double upper);
+    double                  tolerance() const;
+    void                    setTolerance(double tol);
+    int                     length() const;
+    Adesk::Boolean          isEmpty() const;
+    int                     logicalLength() const;
+    void                    setLogicalLength(int len);
+    int                     physicalLength() const;
+    void                    setPhysicalLength(int len);
+    int                     growLength() const;
+    void                    setGrowLength(int len);
+    static std::string      className();
 public:
     AcGeKnotVector m_imp;
 };
