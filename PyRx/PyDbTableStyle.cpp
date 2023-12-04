6aeee354384d40ad81706c90fcfffb2f91431274
Daniel <dwmcabinets@yahoo.com>
2023-12-04 09:21:38 +0800

    BRX port

--- PyRx/PyDbTableStyle.cpp
+++ PyRx/PyDbTableStyle.cpp
@@ -131,14 +131,20 @@ PyDbTableStyle::PyDbTableStyle(const PyDbObjectId& id, AcDb::OpenMode mode)
 PyDbTableStyle::PyDbTableStyle(const PyDbObjectId& id, AcDb::OpenMode mode, bool erased)
     : PyDbObject(openAcDbObject<AcDbTableStyle>(id, mode, erased), false)
 {
-
 }
 
 std::string PyDbTableStyle::getName()
 {
+#if defined(_BRXTARGET) && _BRXTARGET <= 240
+    RxAutoOutStr name;
+    PyThrowBadEs(impObj()->getName(name.buf));
+    return wstr_to_utf8(name.buf);
+#else
     AcString name;
     PyThrowBadEs(impObj()->getName(name));
     return wstr_to_utf8(name);
+#endif
+
 }
 
 void PyDbTableStyle::setName(const std::string& pszName)
