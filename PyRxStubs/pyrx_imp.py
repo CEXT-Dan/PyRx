b0c7c52aa87e139b9fdff7febe13ac04e55e0296
Daniel <dwmcabinets@yahoo.com>
2024-02-26 08:49:06 +0800

    possible solution for Issue #3

---
+++ PyRxStubs/pyrx_imp.py
@@ -0,0 +1,10 @@
+import PyRx as Rx
+import PyGe as Ge
+import PyGi as Gi
+import PyDb as Db
+import PyAp as Ap
+import PyEd as Ed
+import PyPl as Pl
+import PyGs as Gs
+# this module is to hide Pylance 'module not found' warnings
+# Pylance cant reat C++
