0d412057324e3f1962d9f1bd10abea35f9805733
Daniel <dwmcabinets@yahoo.com>
2024-03-02 11:13:44 +0800

    work on Gi/Gs

--- PyRx/PyGiGraphicsKernel.h
+++ PyRx/PyGiGraphicsKernel.h
@@ -1,6 +1,35 @@
 #pragma once
+
+
+//-----------------------------------------------------------------------------------------
+//PyGiKernelDescriptor
+void makePyGiKernelDescriptorWrapper();
+
+class PyGiKernelDescriptor
+{
+public:
+    PyGiKernelDescriptor(const AcGiKernelDescriptor& kernel);
+    bool supports(const PyGiKernelDescriptor& desc) const;
+
+    static std::string className();
+
+public:
+    AcGiKernelDescriptor impl;
+};
+
+//-----------------------------------------------------------------------------------------
+//PyGiGraphicsKernel
+void makePyGiGraphicsKernelWrapper();
+
 class PyGiGraphicsKernel
 {
+public:
+    PyGiGraphicsKernel(const AcGiGraphicsKernel& Kernel);
+    const PyGiKernelDescriptor getDescriptor(void);
+    static std::string className();
+
+public:
+    AcGiGraphicsKernel impl;
 };
 
 class PyGiContext
