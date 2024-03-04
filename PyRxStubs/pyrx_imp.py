293574aa8d8db9ba16d79b7d6f6c519336852029
Daniel <dwmcabinets@yahoo.com>
2024-03-04 17:02:34 +0800

    added conditional import for BrxCv

--- PyRxStubs/pyrx_imp.py
+++ PyRxStubs/pyrx_imp.py
@@ -6,5 +6,10 @@ import PyAp as Ap
 import PyEd as Ed
 import PyPl as Pl
 import PyGs as Gs
+
+import importlib.util
+if importlib.util.find_spec("PyBrxCv") is not None:
+    import PyBrxCv as Cv
+
 # this module is to hide Pylance 'module not found' warnings
 # Pylance cant read Python embedded in C++
