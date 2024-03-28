ac60756083f0a6516a87cf3692ab6d5a6071f06b
Daniel <dwmcabinets@yahoo.com>
2024-03-28 21:34:25 +0800

    improve test

--- PySamples/_perfTest.py
+++ PySamples/_perfTest.py
@@ -5,12 +5,13 @@ from pyrx_imp import Db
 from pyrx_imp import Ap
 from pyrx_imp import Ed
 from pyrx_imp import Gs
+from pyrx_impx import Ax
 import timeit
 
 #run this before release!!!
 #PySamples\dwg\TestPoints.dwg
 
-#import AxApp24 as Ax
+theApp = Ax.getApp()
 
 # calls Db.Point.desc() every loop
 def move_points_old():
@@ -56,24 +57,32 @@ def move_points_new():
         ent = Db.Entity(id, Db.OpenMode.ForWrite)
         ent.transformBy(mat)
 
+#model is iterable
+def move_points_iter():
+    mat = Ge.Matrix3d()
+    mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
+    db = Db.curDb()
+    model = Db.BlockTableRecord(db.modelSpaceId())
 
-# theApp = Ax.getApp()
+    for id in model:
+        ent = Db.Entity(id, Db.OpenMode.ForWrite)
+        ent.transformBy(mat)
+        
+def move_points_com():
+        mat = Ge.Matrix3d()
+        mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
+        comMat = mat.toList()
 
-# def move_points_com():
-    
-#         mat = Ge.Matrix3d()
-#         mat.setToTranslation(Ge.Point3d(100, 100, 0).asVector())
-#         comMat = mat.toList()
-
-#         for ent in theApp.ActiveDocument.ModelSpace:
-#             if ent.ObjectName == "AcDbPoint":
-#                 ent.TransformBy(comMat)
+        for ent in theApp.ActiveDocument.ModelSpace:
+            if ent.ObjectName == "AcDbPoint":
+                ent.TransformBy(comMat)
 
 
 print("OldTest:..")
 print("test move_points_new....     time = 3.5414436000000933")
 print("test move_points........     time = 3.7101225000005797")
 print("test move_points_old....     time = 4.271034999999756")
+print("test move_points_iter....    time = 4.271034999999756")
 print("test move_points_com....     time = 18.48414749999938")
 
 print("\nrun command pyperftest")
@@ -83,7 +92,8 @@ def PyRxCmd_pyperftest():
         print("test move_points_new....\t time = {}".format(timeit.timeit(move_points_new, number=20)))
         print("test move_points........\t time = {}".format(timeit.timeit(move_points, number=20)))
         print("test move_points_old....\t time = {}".format(timeit.timeit(move_points_old, number=20)))
-        #print("test move_points_com....\t time = {}".format(timeit.timeit(move_points_com, number=20)))
+        print("test move_points_iter...\t time = {}".format(timeit.timeit(move_points_iter, number=20)))
+        print("test move_points_com....\t time = {}".format(timeit.timeit(move_points_com, number=20)))
     except Exception as err:
         print(err)
 
