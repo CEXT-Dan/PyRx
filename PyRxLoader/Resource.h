182a4bac29c4065db79f64af473f220db30b9ce7
Daniel <dwmcabinets@yahoo.com>
2024-03-29 17:26:06 +0800

    start loader module

---
+++ PyRxLoader/Resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ArxBrxTest.rc
+//
+#define IDS_PROJNAME 100
+#define MENUITEM_ID_1 10000
+#define MENUITEM_ID_2 10001
+#define MENUITEM_ID_3 10002
+#define MENUITEM_ID_4 10003
+#define MENUITEM_ID_5 10004
+#define MENUITEM_ID_6 10005
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         100
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
---
+++ PyRxLoader/StdAfx.h
@@ -0,0 +1,297 @@
+// (C) Copyright 2002-2007 by Autodesk, Inc. 
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted, 
+// provided that the above copyright notice appears in all copies and 
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting 
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC. 
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+//
+// Use, duplication, or disclosure by the U.S. Government is subject to 
+// restrictions set forth in FAR 52.227-19 (Commercial Computer
+// Software - Restricted Rights) and DFAR 252.227-7013(c)(1)(ii)
+// (Rights in Technical Data and Computer Software), as applicable.
+//
+
+//-----------------------------------------------------------------------------
+//- StdAfx.h : include file for standard system include files,
+//-      or project specific include files that are used frequently,
+//-      but are changed infrequently
+//-----------------------------------------------------------------------------
+#pragma once
+
+#pragma pack (push, 8)
+#pragma warning(disable: 4786 4996 26812)
+
+//#pragma warning(disable: 4098)
+
+//-----------------------------------------------------------------------------
+#define STRICT
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN			//- Exclude rarely-used stuff from Windows headers
+#endif
+
+//- Modify the following defines if you have to target a platform prior to the ones specified below.
+//- Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER					//- Allow use of features specific to Windows 95 and Windows NT 4 or later.
+#define WINVER _WIN32_WINNT_WIN10			//- Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#endif
+
+#ifndef _WIN32_WINNT			//- Allow use of features specific to Windows NT 4 or later.
+#define _WIN32_WINNT _WIN32_WINNT_WIN10		//- Change this to the appropriate value to target Windows 2000 or later.
+#endif						
+
+#ifndef _WIN32_WINDOWS			//- Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS _WIN32_WINNT_WIN10	//- Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE				//- Allow use of features specific to IE 4.0 or later.
+#define _WIN32_IE _WIN32_WINNT_WIN10		//- Change this to the appropriate value to target IE 5.0 or later.
+#endif
+
+
+//- ObjectARX and OMF headers needs this
+#include <map>
+#include <vector>
+#include <array>
+#include <functional>
+#include <memory>
+#include <variant>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <filesystem>
+//-----------------------------------------------------------------------------
+#include <afxwin.h>				//- MFC core and standard components
+#include <afxext.h>				//- MFC extensions
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxole.h>				//- MFC OLE classes
+#include <afxodlgs.h>			//- MFC OLE dialog classes
+#include <afxdisp.h>			//- MFC Automation classes
+#endif // _AFX_NO_OLE_SUPPORT
+
+#ifndef _AFX_NO_DB_SUPPORT
+#include <afxdb.h>				//- MFC ODBC database classes
+#endif // _AFX_NO_DB_SUPPORT
+
+#ifndef _AFX_NO_DAO_SUPPORT
+#include <afxdao.h>				//- MFC DAO database classes
+#endif // _AFX_NO_DAO_SUPPORT
+
+#include <afxdtctl.h>			//- MFC support for Internet Explorer 4 Common Controls
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>				//- MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+//-----------------------------------------------------------------------------
+//- Include ObjectDBX/ObjectARX headers
+//- Uncomment one of the following lines to bring a given library in your project.
+//#define _BREP_SUPPORT_			//- Support for the BRep API
+//#define _HLR_SUPPORT_				//- Support for the Hidden Line Removal API
+//#define _AMODELER_SUPPORT_		//- Support for the AModeler API
+//#define _ASE_SUPPORT_				//- Support for the ASI/ASE API
+//#define _RENDER_SUPPORT_			//- Support for the AutoCAD Render API
+//#define _ARX_CUSTOM_DRAG_N_DROP_	//- Support for the ObjectARX Drag'n Drop API
+//#define _INC_LEAGACY_HEADERS_		//- Include legacy headers in this project
+
+
+//** I added this because doing a static analysis picks up errors we have no control over 
+#pragma warning (push, 0)
+#include "arxHeaders.h"
+#pragma warning (pop)
+
+#pragma comment( lib , "AcPal.lib" )
+
+//-----------------------------------------------------------------------------
+#include "DocData.h" //- Your document specific data class holder
+
+//- Declare it as an extern here so that it becomes available in all modules
+extern AcApDataManager<CDocData> DocVars;
+
+constexpr inline std::wstring& tolower(std::wstring& s) noexcept {
+    std::transform(s.begin(), s.end(), s.begin(),
+        [](wchar_t c) { return std::tolower(c); });
+    return s;
+}
+
+inline std::wstring tolower(const std::wstring& s) noexcept {
+    std::wstring buffer{ s };
+    std::transform(buffer.begin(), buffer.end(), buffer.begin(),
+        [](wchar_t c) { return std::tolower(c); });
+    return buffer;
+}
+
+inline std::filesystem::path tolower(const std::filesystem::path& s) noexcept {
+    std::wstring buffer{ s };
+    return std::filesystem::path{ tolower(buffer) };
+}
+
+constexpr inline void ltrim(std::string& s, char chr) noexcept {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [&](char ch) {
+        return chr != ch;
+        }));
+}
+
+constexpr inline void rtrim(std::string& s, char chr) noexcept {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [&](char ch) {
+        return chr != ch;
+        }).base(), s.end());
+}
+
+constexpr inline void trim(std::string& s, char chr) noexcept {
+    ltrim(s, chr);
+    rtrim(s, chr);
+}
+
+constexpr inline std::string ltrim_copy(std::string s, char chr) noexcept {
+    ltrim(s, chr);
+    return s;
+}
+
+constexpr inline std::string rtrim_copy(std::string s, char chr) noexcept {
+    rtrim(s, chr);
+    return s;
+}
+
+// trim from both ends (copying)
+constexpr inline std::string trim_copy(std::string s, char chr) noexcept {
+    trim(s, chr);
+    return s;
+}
+
+constexpr inline void ltrim(std::wstring& s, wchar_t chr) noexcept {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [&](wchar_t ch) {
+        return chr != ch;
+        }));
+}
+
+constexpr inline void rtrim(std::wstring& s, wchar_t chr) noexcept {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [&](wchar_t ch) {
+        return chr != ch;
+        }).base(), s.end());
+}
+
+constexpr inline void trim(std::wstring& s, wchar_t chr) noexcept {
+    ltrim(s, chr);
+    rtrim(s, chr);
+}
+
+constexpr inline std::wstring ltrim_copy(std::wstring s, wchar_t chr) noexcept {
+    ltrim(s, chr);
+    return s;
+}
+
+constexpr inline std::wstring rtrim_copy(std::wstring s, wchar_t chr) noexcept {
+    rtrim(s, chr);
+    return s;
+}
+
+// trim from both ends (copying)
+constexpr inline std::wstring trim_copy(std::wstring s, wchar_t chr) noexcept {
+    trim(s, chr);
+    return s;
+}
+
+constexpr inline bool iCompare(const std::string& a, const std::string& b) noexcept
+{
+    return std::equal(a.begin(), a.end(),
+        b.begin(), b.end(),
+        [](char a, char b) {
+            return tolower(a) == tolower(b);
+        });
+}
+
+constexpr inline bool iCompare(const std::wstring& a, const std::wstring& b) noexcept
+{
+    return std::equal(a.begin(), a.end(),
+        b.begin(), b.end(),
+        [](wchar_t a, wchar_t b) {
+            return tolower(a) == tolower(b);
+        });
+}
+
+inline [[nodiscard]] std::wstring utf8_to_wstr(const char* str8) noexcept {
+    if (str8 == nullptr)
+        return std::wstring{};
+    const std::string str{ str8 };
+    const int count = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), NULL, 0);
+    std::wstring wstr(count, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), &wstr[0], count);
+    return wstr;
+}
+
+inline [[nodiscard]] std::wstring utf8_to_wstr(const std::string& str) noexcept {
+    const int count = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), NULL, 0);
+    std::wstring wstr(count, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), &wstr[0], count);
+    return wstr;
+}
+
+inline [[nodiscard]] std::string wstr_to_utf8(const std::wstring& wstr) noexcept {
+    const int count = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.length(), NULL, 0, NULL, NULL);
+    std::string str(count, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], count, NULL, NULL);
+    return str;
+}
+
+inline [[nodiscard]] std::string wstr_to_utf8(const wchar_t* utf16wc) noexcept {
+    if (utf16wc == nullptr)
+        return std::string{};
+    const std::wstring wstr{ utf16wc };
+    const int count = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.length(), NULL, 0, NULL, NULL);
+    std::string str(count, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], count, NULL, NULL);
+    return str;
+}
+
+template <typename Out>
+constexpr inline void splitA(const std::string& s, char delim, Out result) noexcept {
+    std::istringstream iss(s);
+    std::string item;
+    while (std::getline(iss, item, delim)) {
+        *result++ = item;
+    }
+}
+
+constexpr inline void splitA(const std::string& s, char delim, std::vector<std::string>& elems) noexcept {
+    splitA(s, delim, std::back_inserter(elems));
+}
+
+template <typename Out>
+constexpr inline void splitW(const std::wstring& s, wchar_t delim, Out result) noexcept {
+    std::wistringstream iss(s);
+    std::wstring item;
+    while (std::getline(iss, item, delim)) {
+        *result++ = item;
+    }
+}
+
+constexpr inline void splitW(const std::wstring& s, wchar_t delim, std::vector<std::wstring>& elems) noexcept {
+    splitW(s, delim, std::back_inserter(elems));
+}
+
+constexpr inline bool wstrReplace(std::wstring& str, const std::wstring& from, const std::wstring& to) noexcept {
+    size_t start_pos = str.find(from);
+    if (start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+constexpr inline void wEraseSubStr(std::wstring& mainStr, const std::wstring& toErase) noexcept
+{
+    size_t pos = mainStr.find(toErase);
+    if (pos != std::wstring::npos)
+        mainStr.erase(pos, toErase.length());
+}
+
+#pragma pack (pop)
+
