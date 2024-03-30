21e292a358e0ed69c266b37290412e91b034f3bc
Daniel <dwmcabinets@yahoo.com>
2024-03-30 08:59:15 +0800

    update projects loader module works yay

--- PyRx/PyRxApp.cpp
+++ PyRx/PyRxApp.cpp
@@ -92,7 +92,6 @@ bool WxRxApp::Init_wxPython()
     return true;
 }
 
-
 //------------------------------------------------------------------------------------------------
 // helper function to initWxApp
 
@@ -129,6 +128,21 @@ std::filesystem::path PyRxApp::modulePath()
     return path;
 }
 
+void PyRxApp::appendINISettings()
+{
+    auto settingsPath = modulePath() / _T("PyRx.INI");
+    std::wstring stubPath(MAX_PATH, 0);
+    int res = GetPrivateProfileStringW(_T("PYRXSETTINGS"), _T("PYRXSTUBPATH"), _T(""), stubPath.data(), stubPath.size(), settingsPath.c_str());
+    if (res != 0)
+    {
+        appendSearchPath(stubPath.c_str());
+    }
+    else
+    {
+        acutPrintf(_T("Failed to read setting %ls: "), _T("PYRXSTUBPATH"));
+    }
+}
+
 PyRxApp& PyRxApp::instance()
 {
     static PyRxApp mthis;
@@ -151,6 +165,8 @@ bool PyRxApp::init()
         initPyBrxCvModule(); 
 #endif
         initWxApp();
+        appendINISettings();
+
 
         if (Py_IsInitialized() && setPyConfig())
         {
