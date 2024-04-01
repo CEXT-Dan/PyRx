35a82c89f82e3172b7ff5a3a2e581c4ed367ce68
Daniel <dwmcabinets@yahoo.com>
2024-04-01 15:02:37 +0800

    update sample

--- PySamples/PyDb/readDwg.py
+++ PySamples/PyDb/readDwg.py
@@ -13,20 +13,29 @@ def PyRxCmd_pydebug() -> None:
     import PyRxDebug
     PyRxDebug.startListener()
 
+# Python disposes object in order of creation
+# so we'll define new a new function as a scope 
+# to make sure modelspace is closed before the database
+
+def iterateDatabase(db: Db.Database, entities):
+    model = Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.ForRead)
+    for id in model.objectIds():
+        classname = id.objectClass().name()
+        if classname in entities:
+            entities[classname] = entities[classname]+1
+        else:
+            entities[classname] = 1
+    
 def PyRxCmd_pyreaddwg():
     try:
         db = Db.Database(False, True)
         db.readDwgFile("..\\dwg\\Floor Plan Sample.dwg")
         db.closeInput(True)
         entities = {}
-        model = Db.BlockTableRecord(db.modelSpaceId(), Db.OpenMode.ForRead)
-
-        for id in model.objectIds():
-            classname = id.objectClass().name()
-            if classname in entities:
-                entities[classname] = entities[classname]+1
-            else:
-                entities[classname] = 1
+        
+        #scope
+        iterateDatabase(db, entities)
+       
         print(entities)
     except Exception as err:
         print(err)
