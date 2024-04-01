fd06517914a81dbe94fadcfa5304336285ace467
Daniel <dwmcabinets@yahoo.com>
2024-04-01 21:22:09 +0800

    rename folder

--- unitTests/dbc.py
+++ unitTests/dbc.py
@@ -1,19 +1,16 @@
 import os
 import PyDb as Db
 
-mediapath = os.getcwd() + "\\testmedia\\"
-print(mediapath)
-
 dbs = {}
 
 def loaddbs():
+    mediapath = os.getcwd() + "\\testmedia\\"
     file = Db.HostApplicationServices().findFile(mediapath + "06457.dwg")
     print(file)
     dbs["06457"] = Db.Database(False, True)
     dbs["06457"].readDwgFile(file)
     dbs["06457"].closeInput(True)
-    print("06457 is open")
-    
+ 
 def cleardbs():
     dbs = None
     
