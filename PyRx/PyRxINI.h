5093c8b9849aee6d81be7005a497e29e800e79ad
Daniel <dwmcabinets@yahoo.com>
2024-04-04 18:18:21 +0800

    work on .ini stuff issue #1

--- PyRx/PyRxINI.h
+++ PyRx/PyRxINI.h
@@ -1,5 +1,10 @@
 #pragma once
 class PyRxINI
 {
+public:
+
+    static std::filesystem::path iniPath();
+    static std::tuple<bool, bool> pythonIsolated();
+    static std::tuple<bool, std::wstring> pythonvenv_path();
 };
 
