b66652c0ca9a19ffdf7baba6acfdb9b1bfd9f1a9
Daniel <dwmcabinets@yahoo.com>
2024-04-05 20:40:52 +0800

    update testcfg.py

--- unitTests/testcfg.py
+++ unitTests/testcfg.py
@@ -1,14 +1,14 @@
 from datetime import date
-import PyRx  
-import PyGe  
-import PyGi 
+import PyRx as Rx
+import PyGe as Ge 
+import PyGi as Gi
 import PyDb as Db 
 import PyAp as Ap
-import PyEd  
+import PyEd as Ed 
 
 host = Ap.Application.hostAPI()
 
-showSkipped = False
+#settings
 testVerbosity = 2
 logToFile = True
 logFileName = "tout-{}-{}.{}".format(host, date.today(), 'txt')
