9bf3434f722c7f55fb0816a4e0aa94964d5505d3
Daniel <dwmcabinets@yahoo.com>
2024-04-07 08:11:15 +0800

    added initget sample

---
+++ PySamples/PyEd/InitGet.py
@@ -0,0 +1,51 @@
+from pyrx_imp import Rx
+from pyrx_imp import Ge
+from pyrx_imp import Gi
+from pyrx_imp import Db
+from pyrx_imp import Ap
+from pyrx_imp import Ed
+import traceback
+
+def PyRxCmd_doprompt1():
+    try:
+        
+        Ed.Editor.initGet(1, "Merge Create")
+        res = Ed.Editor.getKword("\nEnter an option [Merge/Create]: ")
+        if res[0] != Ed.PromptStatus.eOk:
+           raise Exception(res)
+       
+        match res[1]:
+            case 'Merge': 
+                print("Merge was pressed")
+            case 'Create': 
+                print("Create was pressed")
+            case _  : 
+                print("Set a default value")
+
+    except Exception as err:
+        traceback.print_exception(err)
+
+def PyRxCmd_doprompt2():
+    try:
+        
+        Ed.Editor.initGet(0 ,"Merge Create")
+        mgs = "\nEnter an option [Merge/Create] <{}>: ".format('create')
+        res = Ed.Editor.getKword(mgs)
+        
+        if res[0] != Ed.PromptStatus.eKeyword:
+            print("Create was pressed")
+            return
+
+        match res[1]:
+            case 'Merge': 
+                print("Merge was pressed")
+            case 'Create': 
+                print("Create was pressed")
+            case _  : 
+                print("Set a default value")
+
+    except Exception as err:
+        traceback.print_exception(err)
+
+
+
