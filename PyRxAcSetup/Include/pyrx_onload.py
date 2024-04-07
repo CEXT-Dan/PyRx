ddb4a57795bc7e29326a2ab783970238fba3917d
Daniel <dwmcabinets@yahoo.com>
2024-04-07 22:11:25 +0800

    update notes in .ini

--- PyRxAcSetup/Include/pyrx_onload.py
+++ PyRxAcSetup/Include/pyrx_onload.py
@@ -1,4 +1,37 @@
-# import wx
+# # import
+# from pyrx_imp import Rx
+# from pyrx_imp import Ge
+# from pyrx_imp import Gi
+# from pyrx_imp import Db
 # from pyrx_imp import Ap
-# wx.MessageBox("py_onload, hi from py_onload")
-# Ap.Application.loadPythonModule("M:\\Dev\\Projects\\PyRxGit\\PySamples\\_doit")
+# from pyrx_imp import Ed
+# from pyrx_imp import Gs
+# import traceback
+# import wx
+
+# def OnPyInitApp() -> None:
+#     print("\nOnPyInitApp")
+
+# def OnPyUnloadApp() -> None:
+#     print("\nOnPyUnloadApp")
+    
+# def OnPyReload() -> None:
+#     print("\nOnPyReload")
+
+# def OnPyLoadDwg() -> None:
+#     print("\nOnPyLoadDwg")
+
+# def OnPyUnloadDwg() -> None:
+#     print("\nOnPyUnloadDwg")
+    
+# def PyRxCmd_mycommand(CmdFlags=Ap.CmdFlags.TRANSPARENT) -> None:
+#     try:
+#         print("Hello world!")
+#     except Exception as err:
+#         traceback.print_exception(err)
+
+# def PyRxLisp_mylisp(args):
+#     try:
+#         return args
+#     except Exception as err:
+#         print(err)
