a21bfe6988a50c8f759000bab8bf62b24e0bb59b
Daniel <dwmcabinets@yahoo.com>
2024-04-13 19:44:21 +0800

    LayerStateManager stubs

--- PyRx/PyDbDbLayerStateManager.cpp
+++ PyRx/PyDbDbLayerStateManager.cpp
@@ -8,35 +8,35 @@ using namespace boost::python;
 //PyDbDbLayerStateManager
 void makePyDbDbLayerStateManagerWrapper()
 {
-    PyDocString DS("PyDb.Dictionary");
+    PyDocString DS("LayerStateManager");
     class_<PyDbDbLayerStateManager, bases<PyRxObject>>("LayerStateManager")
         .def(init<>())
-        .def(init<const PyDbDatabase&>())
+        .def(init<const PyDbDatabase&>(DS.ARGS({ "db: PyDb.Database=None" })))
         .def("layerStatesDictionaryId", &PyDbDbLayerStateManager::layerStatesDictionaryId1)
-        .def("layerStatesDictionaryId", &PyDbDbLayerStateManager::layerStatesDictionaryId2)
-        .def("hasLayerState", &PyDbDbLayerStateManager::hasLayerState)
+        .def("layerStatesDictionaryId", &PyDbDbLayerStateManager::layerStatesDictionaryId2, DS.ARGS({ "bCreateIfNotPresent: bool=False" }))
+        .def("hasLayerState", &PyDbDbLayerStateManager::hasLayerState, DS.ARGS({ "sName: str" }))
         .def("saveLayerState", &PyDbDbLayerStateManager::saveLayerState1)
-        .def("saveLayerState", &PyDbDbLayerStateManager::saveLayerState2)
+        .def("saveLayerState", &PyDbDbLayerStateManager::saveLayerState2, DS.ARGS({ "sName: str", "mask: PyDb.LayerStateMask","id: PyDb.ObjectId=None" }))
         .def("restoreLayerState", &PyDbDbLayerStateManager::restoreLayerState1)
-        .def("restoreLayerState", &PyDbDbLayerStateManager::restoreLayerState2)
-        .def("setLayerStateMask", &PyDbDbLayerStateManager::setLayerStateMask)
-        .def("getLayerStateMask", &PyDbDbLayerStateManager::getLayerStateMask)
-        .def("deleteLayerState", &PyDbDbLayerStateManager::deleteLayerState)
-        .def("renameLayerState", &PyDbDbLayerStateManager::renameLayerState)
-        .def("importLayerState", &PyDbDbLayerStateManager::importLayerState)
-        .def("exportLayerState", &PyDbDbLayerStateManager::exportLayerState)
-        .def("setLayerStateDescription", &PyDbDbLayerStateManager::setLayerStateDescription)
-        .def("getLayerStateDescription", &PyDbDbLayerStateManager::getLayerStateDescription)
-        .def("layerStateHasViewportData", &PyDbDbLayerStateManager::layerStateHasViewportData)
-        .def("importLayerStateFromDb", &PyDbDbLayerStateManager::importLayerStateFromDb)
-        .def("getLayerStateNames", &PyDbDbLayerStateManager::getLayerStateNames)
-        .def("getLastRestoredLayerState", &PyDbDbLayerStateManager::getLastRestoredLayerState)
-        .def("getLayerStateLayers", &PyDbDbLayerStateManager::getLayerStateLayers)
-        .def("compareLayerStateToDb", &PyDbDbLayerStateManager::compareLayerStateToDb)
-        .def("addLayerStateLayers", &PyDbDbLayerStateManager::addLayerStateLayers)
-        .def("removeLayerStateLayers", &PyDbDbLayerStateManager::removeLayerStateLayers)
-        .def("isDependentLayerState", &PyDbDbLayerStateManager::isDependentLayerState)
-        .def("getDatabase", &PyDbDbLayerStateManager::getDatabase)
+        .def("restoreLayerState", &PyDbDbLayerStateManager::restoreLayerState2, DS.ARGS({ "sName: str", "vpId: PyDb.ObjectId", "flags: int = 0", "mask: PyDb.LayerStateMask=None" }))
+        .def("setLayerStateMask", &PyDbDbLayerStateManager::setLayerStateMask, DS.ARGS({ "sName: str", "mask: PyDb.LayerStateMask" }))
+        .def("getLayerStateMask", &PyDbDbLayerStateManager::getLayerStateMask, DS.ARGS({ "sName: str" }))
+        .def("deleteLayerState", &PyDbDbLayerStateManager::deleteLayerState, DS.ARGS({ "sName: str" }))
+        .def("renameLayerState", &PyDbDbLayerStateManager::renameLayerState, DS.ARGS({ "sName: str","newsName: str" }))
+        .def("importLayerState", &PyDbDbLayerStateManager::importLayerState, DS.ARGS({ "fName: str" }))
+        .def("exportLayerState", &PyDbDbLayerStateManager::exportLayerState, DS.ARGS({ "sName: str","fName: str" }))
+        .def("setLayerStateDescription", &PyDbDbLayerStateManager::setLayerStateDescription, DS.ARGS({ "sName: str","desc: str" }))
+        .def("getLayerStateDescription", &PyDbDbLayerStateManager::getLayerStateDescription, DS.ARGS({ "sName: str" }))
+        .def("layerStateHasViewportData", &PyDbDbLayerStateManager::layerStateHasViewportData, DS.ARGS({ "sName: str" }))
+        .def("importLayerStateFromDb", &PyDbDbLayerStateManager::importLayerStateFromDb, DS.ARGS({ "sName: str","db: PyDb.Database" }))
+        .def("getLayerStateNames", &PyDbDbLayerStateManager::getLayerStateNames, DS.ARGS({ "bIncludeHidden: bool","bIncludeXref: bool" }))
+        .def("getLastRestoredLayerState", &PyDbDbLayerStateManager::getLastRestoredLayerState, DS.ARGS())
+        .def("getLayerStateLayers", &PyDbDbLayerStateManager::getLayerStateLayers, DS.ARGS({ "sName: str","invert: bool" }))
+        .def("compareLayerStateToDb", &PyDbDbLayerStateManager::compareLayerStateToDb, DS.ARGS({ "sName: str","vpId: PyDb.ObjectId" }))
+        .def("addLayerStateLayers", &PyDbDbLayerStateManager::addLayerStateLayers, DS.ARGS({ "sName: str","layerIds: list[PyDb.ObjectId]" }))
+        .def("removeLayerStateLayers", &PyDbDbLayerStateManager::removeLayerStateLayers, DS.ARGS({ "sName: str","layerNames: list[str]" }))
+        .def("isDependentLayerState", &PyDbDbLayerStateManager::isDependentLayerState, DS.ARGS({ "sName: str" }))
+        .def("getDatabase", &PyDbDbLayerStateManager::getDatabase, DS.ARGS())
         .def("desc", &PyDbDbLayerStateManager::desc, DS.SARGS(15560)).staticmethod("desc")
         .def("className", &PyDbDbLayerStateManager::className, DS.SARGS()).staticmethod("className")
         ;
@@ -168,7 +168,7 @@ boost::python::list PyDbDbLayerStateManager::getLayerStateNames(bool bIncludeHid
 
 boost::python::tuple PyDbDbLayerStateManager::getLastRestoredLayerState()
 {
-    AcString sName; 
+    AcString sName;
     PyDbObjectId restoredLSId;
     PyThrowBadEs(impObj()->getLastRestoredLayerState(sName, restoredLSId.m_id));
     return boost::python::make_tuple(wstr_to_utf8(sName), restoredLSId);
@@ -177,7 +177,7 @@ boost::python::tuple PyDbDbLayerStateManager::getLastRestoredLayerState()
 boost::python::list PyDbDbLayerStateManager::getLayerStateLayers(const std::string& sName, bool bInvert)
 {
     AcStringArray layerArray;
-    PyThrowBadEs(impObj()->getLayerStateLayers(layerArray,utf8_to_wstr(sName).c_str(), bInvert));
+    PyThrowBadEs(impObj()->getLayerStateLayers(layerArray, utf8_to_wstr(sName).c_str(), bInvert));
     return AcStringArrayToPyList(layerArray);
 }
 
@@ -188,7 +188,7 @@ bool PyDbDbLayerStateManager::compareLayerStateToDb(const std::string& sName, co
 
 void PyDbDbLayerStateManager::addLayerStateLayers(const std::string& sName, const boost::python::list& layerIds)
 {
-    PyThrowBadEs(impObj()->addLayerStateLayers(utf8_to_wstr(sName).c_str(),PyListToObjectIdArray(layerIds)));
+    PyThrowBadEs(impObj()->addLayerStateLayers(utf8_to_wstr(sName).c_str(), PyListToObjectIdArray(layerIds)));
 }
 
 void PyDbDbLayerStateManager::removeLayerStateLayers(const std::string& sName, const boost::python::list& layerNames)
