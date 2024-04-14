2b2765e21ab43ed5ae9cece55a2df97d79e6b29a
Daniel <dwmcabinets@yahoo.com>
2024-04-14 17:48:25 +0800

    update sample

--- PySamples/helloworld.py
+++ PySamples/helloworld.py
@@ -1,6 +1,8 @@
 import traceback
 from pyrx_imp import Rx, Ge, Gi, Db, Ap, Ed
 
+print("Added new command helloworld, type helloworld at the prompt: ")
+
 def PyRxCmd_helloworld() -> None:
     try:
         print("whats up!: ")
