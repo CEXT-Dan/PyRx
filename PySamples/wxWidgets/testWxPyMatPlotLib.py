ad8a111cedce62ba7359b21254894d3874a118fc
Daniel <dwmcabinets@yahoo.com>
2024-04-18 21:34:06 +0800

    improved showplotlib sample

--- PySamples/wxWidgets/testWxPyMatPlotLib.py
+++ PySamples/wxWidgets/testWxPyMatPlotLib.py
@@ -1,11 +1,3 @@
-
-from numpy import arange, sin, pi
-
-import matplotlib
-matplotlib.use('WXAgg')
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-from matplotlib.figure import Figure
-
 from pyrx_imp import Rx
 from pyrx_imp import Ge
 from pyrx_imp import Gi
@@ -15,76 +7,98 @@ from pyrx_imp import Ed
 from pyrx_imp import Gs
 import wx
 
+# imports
 import numpy as np
+import matplotlib
+
+# tell matplotlib to use wxPython
+matplotlib.use("WXAgg")
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 print("Added command = showplotlib")
 
+
 def PyRxCmd_showplotlib():
-    try: 
+    try:
         res = Ap.ResourceOverride()
-        dlg = TestDialog(None, -1, "Plot",wx.Size(500,300))
+        dlg = TestDialog(None, -1, "Plot", wx.Size(500, 400))
         if dlg.ShowModal() == wx.ID_OK:
             print("Yay!")
     except Exception as err:
         print(err)
 
+
+# matplotlib panel as a control, with a custom draw
 class CanvasPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        self.figure = Figure(figsize=(1,1))
+        self.figure = Figure(figsize=(1, 1))
         self.axes = self.figure.add_subplot(111)
         self.canvas = FigureCanvas(self, -1, self.figure)
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
         self.SetSizerAndFit(self.sizer)
 
+    # https://matplotlib.org/stable/gallery/images_contours_and_fields/contourf_hatching.html#contourf-hatching
     def draw(self):
         x = np.linspace(-3, 5, 150).reshape(1, -1)
         y = np.linspace(-3, 5, 120).reshape(-1, 1)
         z = np.cos(x) + np.sin(y)
         x, y = x.flatten(), y.flatten()
-        cs = self.axes.contourf(x, y, z, hatches=['-', '/', '\\', '//'],
-                cmap='gray', extend='both', alpha=0.5)
+        cs = self.axes.contourf(
+            x,
+            y,
+            z,
+            hatches=["-", "/", "\\", "//"],
+            cmap="gray",
+            extend="both",
+            alpha=0.5,
+        )
         self.figure.colorbar(cs)
-        
+
+
 class TestDialog(wx.Dialog):
     def __init__(
-            self, parent, id, title, size, pos=wx.DefaultPosition,
-            style=wx.DEFAULT_DIALOG_STYLE):
+        self,
+        parent,
+        id,
+        title,
+        size,
+        pos=wx.DefaultPosition,
+        style=wx.DEFAULT_DIALOG_STYLE,
+    ):
 
+        # init
         wx.Dialog.__init__(self)
         self.Create(parent, id, title, pos, size, style)
-        self.Bind(wx.EVT_INIT_DIALOG, self.OnInitDialog)
-        
-
         self.pltpanel = CanvasPanel(self)
-        
+        self.Bind(wx.EVT_INIT_DIALOG, self.OnInitDialog)
+
+        # create a sizer and append the controls
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.SetMinSize(size)
         sizer.Add(self.pltpanel, 1, wx.LEFT | wx.TOP | wx.GROW)
-        
-        btnsizer = wx.StdDialogButtonSizer()
 
+        btnsizer = wx.StdDialogButtonSizer()
         if wx.Platform != "__WXMSW__":
             btn = wx.ContextHelpButton(self)
             btnsizer.AddButton(btn)
 
+        # ok button
         btn = wx.Button(self, wx.ID_OK)
         btn.SetDefault()
         btnsizer.AddButton(btn)
-
+        # cancel button
         btn = wx.Button(self, wx.ID_CANCEL)
         btnsizer.AddButton(btn)
         btnsizer.Realize()
-
-        sizer.Add(btnsizer, 0, wx.RIGHT | wx.BOTTOM| wx.GROW, 5)
+        # add and resize
+        sizer.Add(btnsizer, 0, wx.RIGHT | wx.BOTTOM | wx.GROW, 5)
         self.SetSizerAndFit(sizer)
 
-
+    # show the world
     def OnInitDialog(self, event):
         Ap.Application.setTitleThemeDark(self.GetHandle())
         Ap.Application.applyHostIcon(self.GetHandle())
         self.pltpanel.draw()
-   
-
-        
