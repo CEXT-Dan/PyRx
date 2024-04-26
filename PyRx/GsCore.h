e54bcb31cc7598d905759423c2d4768f5a59d6a0
Daniel <dwmcabinets@yahoo.com>
2024-04-26 09:10:38 +0800

    added GsCore::getViewParameters

--- PyRx/GsCore.h
+++ PyRx/GsCore.h
@@ -11,6 +11,7 @@ class GsCore
 public:
    static PyGsView getCurrentAcGsView(int vpNum);
    static PyGsView getCurrent3DAcGsView(int vpNum);
+   static bool getViewParameters(int vpNum, PyGsView& view);
    static bool setViewParameters1(int viewportNumber, const PyGsView& obj, bool bRegenRequired, bool bRescaleRequired);
    static bool setViewParameters2(int viewportNumber, const PyGsView& obj, bool bRegenRequired, bool bRescaleRequired, bool bSyncRequired);
 };
