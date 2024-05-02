95484c1a42a18e7f666949f8a2b85a86820bfbc9
Daniel <dwmcabinets@yahoo.com>
2024-05-03 07:59:30 +0800

    GsCore::getBlockImage added background color

--- PyRx/GsCore.h
+++ PyRx/GsCore.h
@@ -15,6 +15,6 @@ public:
    static bool getViewParameters(int vpNum, PyGsView& view);
    static bool setViewParameters1(int viewportNumber, const PyGsView& obj, bool bRegenRequired, bool bRescaleRequired);
    static bool setViewParameters2(int viewportNumber, const PyGsView& obj, bool bRegenRequired, bool bRescaleRequired, bool bSyncRequired);
-   static PyObject* getBlockImage(const PyDbObjectId& blkid, int x, int y);
+   static PyObject* getBlockImage(const PyDbObjectId& blkid, int x, int y, boost::python::object& rgb);
 };
 
