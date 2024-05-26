6720f9472ec2f7140fec1b61ef28d96c85f99bf3
Daniel <dwmcabinets@yahoo.com>
2024-05-26 21:58:08 +0800

    progress on PyDbSection

--- PyRx/PyDbSection.cpp
+++ PyRx/PyDbSection.cpp
@@ -24,10 +24,76 @@ void makePyDbSectionWrapper()
         .def(init<const PyDbObjectId&>())
         .def(init<const PyDbObjectId&, AcDb::OpenMode>())
         .def(init<const PyDbObjectId&, AcDb::OpenMode, bool>(DS.CTOR(ctords)))
+        .def("state", &PyDbSection::state)
+        .def("setState", &PyDbSection::setState)
+        .def("getName", &PyDbSection::getName)
+        .def("setName", &PyDbSection::setName)
+        .def("viewingDirection", &PyDbSection::viewingDirection)
+        .def("setViewingDirection", &PyDbSection::setViewingDirection)
+        .def("verticalDirection", &PyDbSection::verticalDirection)
+        .def("setVerticalDirection", &PyDbSection::setVerticalDirection)
+        .def("normal", &PyDbSection::normal)
+        .def("plane", &PyDbSection::plane)
+        .def("indicatorTransparency", &PyDbSection::indicatorTransparency)
+        .def("setIndicatorTransparency", &PyDbSection::setIndicatorTransparency)
+        .def("indicatorFillColor", &PyDbSection::indicatorFillColor)
+        .def("setIndicatorFillColor", &PyDbSection::setIndicatorFillColor)
+        .def("numVertices", &PyDbSection::numVertices)
+        .def("getVertex", &PyDbSection::getVertex)
+        .def("setVertex", &PyDbSection::setVertex)
+        .def("addVertex", &PyDbSection::addVertex)
+        .def("removeVertex", &PyDbSection::removeVertex)
+        .def("getVertices", &PyDbSection::getVertices)
+        .def("setVertices", &PyDbSection::setVertices)
+        .def("height", &PyDbSection::height)
+        .def("setHeight", &PyDbSection::setHeight)
+        .def("hitTest", &PyDbSection::hitTest)
+        .def("createJog", &PyDbSection::createJog)
+        .def("getSettings", &PyDbSection::getSettings)
+        .def("isLiveSectionEnabled", &PyDbSection::isLiveSectionEnabled)
+        .def("enableLiveSection", &PyDbSection::enableLiveSection)
+        .def("generateSectionGeometry", &PyDbSection::generateSectionGeometry)
+        .def("elevation", &PyDbSection::elevation)
+        .def("setElevation", &PyDbSection::setElevation)
+        .def("topPlane", &PyDbSection::topPlane)
+        .def("setTopPlane", &PyDbSection::setTopPlane)
+        .def("bottomPlane", &PyDbSection::bottomPlane)
+        .def("setBottomPlane", &PyDbSection::setBottomPlane)
+        .def("isSlice", &PyDbSection::isSlice)
+        .def("setIsSlice", &PyDbSection::setIsSlice)
+        .def("thicknessDepth", &PyDbSection::thicknessDepth)
+        .def("setThicknessDepth", &PyDbSection::setThicknessDepth)
+        .def("hasJogs", &PyDbSection::hasJogs)
+        .def("sectionPlaneOffset", &PyDbSection::sectionPlaneOffset)
+        .def("setSectionPlaneOffset", &PyDbSection::setSectionPlaneOffset)
         .def("className", &PyDbSection::className, DS.SARGS()).staticmethod("className")
         .def("desc", &PyDbSection::desc, DS.SARGS(15560)).staticmethod("desc")
         .def("cloneFrom", &PyDbSection::cloneFrom, DS.SARGS({ "otherObject: PyRx.RxObject" })).staticmethod("cloneFrom")
         .def("cast", &PyDbSection::cast, DS.SARGS({ "otherObject: PyRx.RxObject" })).staticmethod("cast")
+        ;
+
+    enum_<AcDbSection::State>("SectionState")
+        .value("kPlane", AcDbSection::State::kPlane)
+        .value("kBoundary", AcDbSection::State::kBoundary)
+        .value("kVolume", AcDbSection::State::kVolume)
+        .export_values()
+        ;
+    enum_<AcDbSection::SubItem>("SectionSubItem")
+        .value("kNone", AcDbSection::SubItem::kNone)
+        .value("kSectionLine", AcDbSection::SubItem::kSectionLine)
+        .value("kSectionLineTop", AcDbSection::SubItem::kSectionLineTop)
+        .value("kSectionLineBottom", AcDbSection::SubItem::kSectionLineBottom)
+        .value("kBackLine", AcDbSection::SubItem::kBackLine)
+        .value("kBackLineTop", AcDbSection::SubItem::kBackLineTop)
+        .value("kBackLineBottom", AcDbSection::SubItem::kBackLineBottom)
+        .value("kVerticalLineTop", AcDbSection::SubItem::kVerticalLineTop)
+        .value("kVerticalLineBottom", AcDbSection::SubItem::kVerticalLineBottom)
+        .export_values()
+        ;
+    enum_<AcDbSection::Height>("SectionHeight")
+        .value("kHeightAboveSectionLine", AcDbSection::Height::kHeightAboveSectionLine)
+        .value("kHeightBelowSectionLine", AcDbSection::Height::kHeightBelowSectionLine)
+        .export_values()
         ;
 }
 
@@ -64,6 +130,246 @@ PyDbSection::PyDbSection(const PyDbObjectId& id, AcDb::OpenMode mode, bool erase
 PyDbSection::PyDbSection(AcDbSection* ptr, bool autoDelete)
     : PyDbEntity(ptr, autoDelete)
 {
+}
+
+AcDbSection::State PyDbSection::state(void) const
+{
+    return impObj()->state();
+}
+
+void PyDbSection::setState(AcDbSection::State nState)
+{
+    PyThrowBadEs(impObj()->setState(nState));
+}
+
+std::string PyDbSection::getName()
+{
+    return wstr_to_utf8(impObj()->getName());
+}
+
+void PyDbSection::setName(const std::string& name)
+{
+    PyThrowBadEs(impObj()->setName(utf8_to_wstr(name).c_str()));
+}
+
+AcGeVector3d PyDbSection::viewingDirection(void) const
+{
+    return impObj()->viewingDirection();
+}
+
+void PyDbSection::setViewingDirection(const AcGeVector3d& dir)
+{
+    PyThrowBadEs(impObj()->setViewingDirection(dir));
+}
+
+AcGeVector3d PyDbSection::verticalDirection(void) const
+{
+    return impObj()->verticalDirection();
+}
+
+void PyDbSection::setVerticalDirection(const AcGeVector3d& dir)
+{
+    PyThrowBadEs(impObj()->setVerticalDirection(dir));
+}
+
+AcGeVector3d PyDbSection::normal(void) const
+{
+    return impObj()->normal();
+}
+
+boost::python::tuple PyDbSection::plane() const
+{
+    PyAutoLockGIL lock;
+    AcGeVector3d uAxis;
+    AcGeVector3d vAxis;
+    PyThrowBadEs(impObj()->plane(uAxis, vAxis));
+    return boost::python::make_tuple(uAxis, vAxis);
+}
+
+int PyDbSection::indicatorTransparency(void) const
+{
+    return impObj()->indicatorTransparency();
+}
+
+void PyDbSection::setIndicatorTransparency(int nTransparency)
+{
+    PyThrowBadEs(impObj()->setIndicatorTransparency(nTransparency));
+}
+
+AcCmColor PyDbSection::indicatorFillColor(void) const
+{
+    return impObj()->indicatorFillColor();
+}
+
+void PyDbSection::setIndicatorFillColor(const AcCmColor& color)
+{
+    PyThrowBadEs(impObj()->setIndicatorFillColor(color));
+}
+
+int PyDbSection::numVertices(void) const
+{
+    return impObj()->numVertices();
+}
+
+AcGePoint3d PyDbSection::getVertex(int nIndex)
+{
+    AcGePoint3d pt;
+    PyThrowBadEs(impObj()->getVertex(nIndex, pt));
+    return pt;
+}
+
+void PyDbSection::setVertex(int nIndex, const AcGePoint3d& pt)
+{
+    PyThrowBadEs(impObj()->setVertex(nIndex, pt));
+}
+
+void PyDbSection::addVertex(int nInsertAt, const AcGePoint3d& pt)
+{
+    PyThrowBadEs(impObj()->addVertex(nInsertAt, pt));
+}
+
+void PyDbSection::removeVertex(int nIndex)
+{
+    PyThrowBadEs(impObj()->removeVertex(nIndex));
+}
+
+boost::python::list PyDbSection::getVertices() const
+{
+    PyAutoLockGIL lock;
+    AcGePoint3dArray pts;
+    PyThrowBadEs(impObj()->getVertices(pts));
+    return Point3dArrayToPyList(pts);
+}
+
+void PyDbSection::setVertices(const boost::python::list& pts)
+{
+    PyThrowBadEs(impObj()->setVertices(PyListToPoint3dArray(pts)));
+}
+
+double PyDbSection::height(AcDbSection::Height nHeightType) const
+{
+    return impObj()->height(nHeightType);
+}
+
+void PyDbSection::setHeight(AcDbSection::Height nHeightType, double fHeight)
+{
+    PyThrowBadEs(impObj()->setHeight(nHeightType, fHeight));
+}
+
+boost::python::tuple PyDbSection::hitTest(const AcGePoint3d& ptHit)
+{
+#if defined(_BRXTARGET) && _BRXTARGET <= 240
+    throw PyNotimplementedByHost();
+#else
+    PyAutoLockGIL lock;
+    int pSegmentIndex = -1;
+    AcGePoint3d ptOnSegment;
+    AcDbSection::SubItem pSubItem = AcDbSection::SubItem::kNone;
+    bool res = impObj()->hitTest(ptHit, &pSegmentIndex, &ptOnSegment, &pSubItem);
+    return boost::python::make_tuple(res, pSegmentIndex, ptOnSegment, pSubItem);
+#endif
+}
+
+void PyDbSection::createJog(const AcGePoint3d& ptOnSection)
+{
+    PyThrowBadEs(impObj()->createJog(ptOnSection));
+}
+
+PyDbObjectId PyDbSection::getSettings(void) const
+{
+    return PyDbObjectId(impObj()->getSettings());
+}
+
+bool PyDbSection::isLiveSectionEnabled(void) const
+{
+    return impObj()->isLiveSectionEnabled();
+}
+
+void PyDbSection::enableLiveSection(bool bEnable)
+{
+    PyThrowBadEs(impObj()->enableLiveSection(bEnable));
+}
+
+boost::python::tuple PyDbSection::generateSectionGeometry(const PyDbEntity& pEnt)
+{
+    PyAutoLockGIL lock;
+    AcArray<AcDbEntity*> intBoundaryEnts;
+    AcArray<AcDbEntity*> intFillEnts;
+    AcArray<AcDbEntity*> backgroundEnts;
+    AcArray<AcDbEntity*> foregroundEnts;
+    AcArray<AcDbEntity*> curveTangencyEnts;
+    PyThrowBadEs(impObj()->generateSectionGeometry(pEnt.impObj(), intBoundaryEnts, intFillEnts, backgroundEnts, foregroundEnts, curveTangencyEnts));
+    return boost::python::make_tuple(
+        AcDbEntityArrayToPyList(intBoundaryEnts),
+        AcDbEntityArrayToPyList(intFillEnts),
+        AcDbEntityArrayToPyList(backgroundEnts),
+        AcDbEntityArrayToPyList(foregroundEnts),
+        AcDbEntityArrayToPyList(curveTangencyEnts));
+}
+
+double PyDbSection::elevation() const
+{
+    return impObj()->elevation();
+}
+
+void PyDbSection::setElevation(double elev)
+{
+    PyThrowBadEs(impObj()->setElevation(elev));
+}
+
+double PyDbSection::topPlane() const
+{
+    return impObj()->topPlane();
+}
+
+void PyDbSection::setTopPlane(double val)
+{
+    PyThrowBadEs(impObj()->setTopPlane(val));
+}
+
+double PyDbSection::bottomPlane() const
+{
+    return impObj()->bottomPlane();
+}
+
+void PyDbSection::setBottomPlane(double val)
+{
+    PyThrowBadEs(impObj()->setBottomPlane(val));
+}
+
+bool PyDbSection::isSlice() const
+{
+    return impObj()->isSlice();
+}
+
+void PyDbSection::setIsSlice(bool value)
+{
+    PyThrowBadEs(impObj()->setIsSlice(value));
+}
+
+double PyDbSection::thicknessDepth() const
+{
+    return impObj()->thicknessDepth();
+}
+
+void PyDbSection::setThicknessDepth(double fThickness)
+{
+    PyThrowBadEs(impObj()->setThicknessDepth(fThickness));
+}
+
+bool PyDbSection::hasJogs() const
+{
+    return impObj()->hasJogs();
+}
+
+double PyDbSection::sectionPlaneOffset() const
+{
+    return impObj()->sectionPlaneOffset();
+}
+
+void PyDbSection::setSectionPlaneOffset(double offset)
+{
+    PyThrowBadEs(impObj()->setSectionPlaneOffset(offset));
 }
 
 std::string PyDbSection::className()
--- PyRx/PyDbSection.h
+++ PyRx/PyDbSection.h
@@ -2,6 +2,11 @@
 #include "PyDbEntity.h"
 #include "DbSection.h"
 
+class PyDbObjectId;
+
+// AcDbSectionManager
+// AcDbSectionSettings
+
 //-----------------------------------------------------------------------------------
 //PyDbSection
 void makePyDbSectionWrapper();
@@ -15,8 +20,50 @@ public:
     PyDbSection(const PyDbObjectId& id, AcDb::OpenMode mode);
     PyDbSection(const PyDbObjectId& id, AcDb::OpenMode mode, bool erased);
     PyDbSection(AcDbSection* ptr, bool autoDelete);
-
     virtual ~PyDbSection() override = default;
+
+    AcDbSection::State  state(void) const;
+    void                setState(AcDbSection::State nState);
+    std::string         getName();
+    void                setName(const std::string& name);
+    AcGeVector3d        viewingDirection(void) const;
+    void                setViewingDirection(const AcGeVector3d& dir);
+    AcGeVector3d        verticalDirection(void) const;
+    void                setVerticalDirection(const AcGeVector3d& dir);
+    AcGeVector3d        normal(void) const;
+    boost::python::tuple plane() const;
+    int                 indicatorTransparency(void) const;
+    void                setIndicatorTransparency(int nTransparency);
+    AcCmColor           indicatorFillColor(void) const;
+    void                setIndicatorFillColor(const AcCmColor& color);
+    int                 numVertices(void) const;
+    AcGePoint3d         getVertex(int nIndex);
+    void                setVertex(int nIndex, const AcGePoint3d& pt);
+    void                addVertex(int nInsertAt, const AcGePoint3d& pt);
+    void                removeVertex(int nInsertAt);
+    boost::python::list getVertices() const;
+    void                setVertices(const boost::python::list& pts);
+    double              height(AcDbSection::Height nHeightType) const;
+    void                setHeight(AcDbSection::Height nHeightType,double fHeight);
+    boost::python::tuple hitTest(const AcGePoint3d& ptHit);
+    void                createJog(const AcGePoint3d& ptOnSection);
+    PyDbObjectId        getSettings(void) const;
+    bool                isLiveSectionEnabled(void) const;
+    void                enableLiveSection(bool bEnable);
+    boost::python::tuple generateSectionGeometry(const PyDbEntity& pEnt);
+    double              elevation() const;
+    void                setElevation(double elev);
+    double              topPlane() const;
+    void                setTopPlane(double val);
+    double              bottomPlane() const;
+    void                setBottomPlane(double);
+    bool                isSlice() const;
+    void                setIsSlice(bool value);
+    double              thicknessDepth() const;
+    void                setThicknessDepth(double fThickness);
+    bool                hasJogs() const;
+    double              sectionPlaneOffset() const;
+    void                setSectionPlaneOffset(double offset);
 
     static std::string  className();
     static PyRxClass    desc();
