3a7b39bb378cef1ae2f4ace83f8c31577dd4cbe2
Daniel <dwmcabinets@yahoo.com>
2024-06-03 21:43:04 +0800

    test

--- unitTests/UnitTestResbuf.py
+++ unitTests/UnitTestResbuf.py
@@ -9,26 +9,29 @@ import PyDb as Db
 import PyAp as Ap
 import PyEd as Ed
 
-
 host = Ap.Application.hostAPI()
 
 
 class TestResbuf(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestResbuf, self).__init__(*args, **kwargs)
-        
+
     def test_invoke_list(self):
-        args = [(Rx.LispType.kText, "C:ADDNUM"),(Rx.LispType.kInt16,10),(Rx.LispType.kNone,0) ]
-        expected = [(5005, 'C:ADDNUM'), (5003, 10)]
+        args = [
+            (Rx.LispType.kText, "C:ADDNUM"),
+            (Rx.LispType.kInt16, 10),
+            (Rx.LispType.kNone, 0),
+        ]
+        expected = [(5005, "C:ADDNUM"), (5003, 10)]
         result = Db.Core.resbufTest(args)
-        self.assertEqual(expected,result)
-        
+        self.assertEqual(expected, result)
+
     def test_invoke_rtdxf0(self):
-        args = [(0 , "LEADER")]
-        expected = [(0 , "LEADER")]
+        args = [(0, "LEADER")]
+        expected = [(0, "LEADER")]
         result = Db.Core.resbufTest(args)
-        self.assertEqual(expected,result)
-        
+        self.assertEqual(expected, result)
+
 def resbuftester():
     try:
         suite = unittest.TestLoader().loadTestsFromTestCase(TestResbuf)
@@ -38,7 +41,7 @@ def resbuftester():
                 runner = unittest.TextTestRunner(f, verbosity=testcfg.testVerbosity)
                 runner.run(suite)
         else:
-            print('TestResbuf')
+            print("TestResbuf")
             print(unittest.TextTestRunner(verbosity=testcfg.testVerbosity).run(suite))
     except Exception as err:
         print(err)
