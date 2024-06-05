02ffdb66c6d6ef78637488919771acc357ac7dc7
Daniel <dwmcabinets@yahoo.com>
2024-06-05 20:44:38 +0800

    update samples

--- PySamples/testPyDbOsnapOverrule.py
+++ PySamples/testPyDbOsnapOverrule.py
@@ -1,10 +1,6 @@
-from pyrx_imp import Rx
-from pyrx_imp import Ge
-from pyrx_imp import Gi
-from pyrx_imp import Db
-from pyrx_imp import Ap
-from pyrx_imp import Ed
-from pyrx_imp import Gs
+import traceback
+from pyrx_imp import Rx, Ge, Gs, Gi, Db, Ap, Ed
+
 
 def OnPyInitApp() -> None:
     print("\ncommand = pyosnapoverrule")
