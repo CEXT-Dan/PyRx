b939814395164f020d887d283d353b42d390a65e
Daniel <dwmcabinets@yahoo.com>
2024-06-09 11:40:09 +0800

    improve working onload

--- PyRxAcSetup/Include/internal_onload.py
+++ PyRxAcSetup/Include/internal_onload.py
@@ -6,8 +6,10 @@ loadedOnce = False
 def OnPyLoadDwg() -> None:
     global loadedOnce
     if loadedOnce == False:
-        Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/unitTests/UnitTestRunner.py")
-        Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/PyRxStubs/utility/genDoc.py")
+        try:
+            Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/unitTests/UnitTestRunner.py")
+            Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/PyRxStubs/utility/genDoc.py")
+        except Exception as err:
+            traceback.print_exception(err)
         loadedOnce = True
     
-
