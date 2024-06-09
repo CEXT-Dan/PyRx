57e72088fad6c3865481dc425b2eb6468a206f8c
Daniel <dwmcabinets@yahoo.com>
2024-06-09 17:25:11 +0800

    Create internal_onload.py

---
+++ PyRxAcSetup/Include/internal_onload.py
@@ -0,0 +1,14 @@
+import traceback
+from pyrx_imp import Rx, Ge, Gi, Db, Ap, Ed
+
+loadedOnce = False
+
+def OnPyLoadDwg() -> None:
+    global loadedOnce
+    if loadedOnce == False:
+        try:
+            Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/unitTests/UnitTestRunner.py")
+            Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/PyRxStubs/utility/genDoc.py")
+        except Exception as err:
+            traceback.print_exception(err)
+        loadedOnce = True
