68b71baca7f4973a82bc9b695925ad2ba650b5ef
Daniel <dwmcabinets@yahoo.com>
2024-06-20 09:58:22 +0800

    update loader and test

--- PyRxAcSetup/Include/internal_onload.py
+++ PyRxAcSetup/Include/internal_onload.py
@@ -1,14 +1,32 @@
 import traceback
 from pyrx_imp import Rx, Ge, Gi, Db, Ap, Ed
 
-loadedOnce = False
+def OnPyInitApp() -> None:
+    print("\nOnPyInitApp _onpyload")
+
+def OnPyUnloadApp() -> None:
+    print("\nOnPyUnloadApp _onpyload")
+    
+def OnPyReload() -> None:
+    print("\nOnPyReload _onpyload")
 
 def OnPyLoadDwg() -> None:
-    global loadedOnce
-    if loadedOnce == False:
-        try:
-            Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/unitTests/UnitTestRunner.py")
-            Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/PyRxStubs/utility/genDoc.py")
-        except Exception as err:
-            traceback.print_exception(err)
-        loadedOnce = True
+    print("\nOnPyLoadDwg _onpyload")
+
+def OnPyUnloadDwg() -> None:
+    print("\nOnPyUnloadDwg _onpyload")
+    
+def PyRxCmd_mycommand(CmdFlags=Ap.CmdFlags.TRANSPARENT) -> None:
+    try:
+        print("Hello world!")
+    except Exception as err:
+        traceback.print_exception(err)
+
+def PyRxLisp_mylisp(args):
+    try:
+        return args
+    except Exception as err:
+        print(err)
+
+Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/unitTests/UnitTestRunner.py")
+Ap.Application.loadPythonModule("M:/Dev/Projects/PyRxGit/PyRxStubs/utility/genDoc.py")
