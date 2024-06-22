ed0c259f23bab7507682bf76f8c1ffb27716f982
Daniel <dwmcabinets@yahoo.com>
2024-06-22 08:19:24 +0800

    update sample ACAD 2022 + 2025: initget Sample #88

--- PySamples/PyEd/InitGet.py
+++ PySamples/PyEd/InitGet.py
@@ -6,46 +6,46 @@ from pyrx_imp import Ap
 from pyrx_imp import Ed
 import traceback
 
+
 def PyRxCmd_doprompt1():
     try:
-        
+
         Ed.Editor.initGet(1, "Merge Create")
         res = Ed.Editor.getKword("\nEnter an option [Merge/Create]: ")
         if res[0] != Ed.PromptStatus.eOk:
-           raise Exception(res)
-       
+            raise Exception(res)
+
         match res[1]:
-            case 'Merge': 
+            case "Merge":
                 print("Merge was pressed")
-            case 'Create': 
+            case "Create":
                 print("Create was pressed")
-            case _  : 
+            case _:
                 print("Set a default value")
 
     except Exception as err:
         traceback.print_exception(err)
 
+
 def PyRxCmd_doprompt2():
     try:
-        
-        Ed.Editor.initGet(0 ,"Merge Create")
-        mgs = "\nEnter an option [Merge/Create] <{}>: ".format('create')
+
+        Ed.Editor.initGet(0, "Merge Create")
+        mgs = "\nEnter an option [Merge/Create] <{}>: ".format("Create")
         res = Ed.Editor.getKword(mgs)
-        
-        if res[0] != Ed.PromptStatus.eKeyword:
+        if res[0] == Ed.PromptStatus.eError or res[0] == Ed.PromptStatus.eCancel:
+            return
+        elif res[0] == Ed.PromptStatus.eNone:
             print("Create was pressed")
             return
 
         match res[1]:
-            case 'Merge': 
+            case "Merge":
                 print("Merge was pressed")
-            case 'Create': 
+            case "Create":
                 print("Create was pressed")
-            case _  : 
+            case _:
                 print("Set a default value")
 
     except Exception as err:
         traceback.print_exception(err)
-
-
-
