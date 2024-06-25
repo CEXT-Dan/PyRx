bc98f662d59c9599f59706413c0b0bc5a8572dd6
Daniel <dwmcabinets@yahoo.com>
2024-06-25 08:43:51 +0800

    progress on sheetsets

--- PyRxCore/PySmSheetSetMgr.cpp
+++ PyRxCore/PySmSheetSetMgr.cpp
@@ -3,6 +3,7 @@
 
 #if defined(_ARXTARGET) || defined(_BRXTARGET) 
 #include "PySmSheetSetMgrImpl.h"
+#include "PyDbObject.h"
 
 using namespace boost::python;
 
@@ -20,7 +21,7 @@ PySmPersist::PySmPersist(PySmPersistImpl* ptr)
 {
 }
 
-PySmPersist::PySmPersist(PySmPersistImpl& other)
+PySmPersist::PySmPersist(const PySmPersistImpl& other)
     : m_pyImp(new PySmPersistImpl(other))
 {
 }
@@ -49,6 +50,10 @@ void makePySmComponentWrapper()
 {
     PyDocString DS("SmComponent");
     class_<PySmComponent,bases<PySmPersist>>("SmComponent", boost::python::no_init)
+        .def("getName", &PySmComponent::getName)
+        .def("setName", &PySmComponent::setName)
+        .def("getDesc", &PySmComponent::getDesc)
+        .def("setDesc", &PySmComponent::setDesc)
         .def("className", &PySmComponent::className, DS.SARGS()).staticmethod("className")
         ;
 }
@@ -58,9 +63,29 @@ PySmComponent::PySmComponent(PySmComponentImpl* ptr)
 {
 }
 
-PySmComponent::PySmComponent(PySmComponentImpl& other)
+PySmComponent::PySmComponent(const PySmComponentImpl& other)
     : PySmPersist(other)
 {
+}
+
+std::string PySmComponent::getName() const
+{
+    return wstr_to_utf8(impObj()->GetName());
+}
+
+void PySmComponent::setName(const std::string& csName)
+{
+    impObj()->SetName(utf8_to_wstr(csName).c_str());
+}
+
+std::string PySmComponent::getDesc() const
+{
+    return wstr_to_utf8(impObj()->GetDesc());
+}
+
+void PySmComponent::setDesc(const std::string& csDesc)
+{
+    impObj()->SetDesc(utf8_to_wstr(csDesc).c_str());
 }
 
 std::string PySmComponent::className()
@@ -82,6 +107,7 @@ void makePySmDatabaseWrapper()
 {
     PyDocString DS("PySmDatabase");
     class_<PySmDatabase, bases<PySmComponent>>("PySmDatabase", boost::python::no_init)
+        .def("smObjects", &PySmDatabase::smObjects)
         .def("className", &PySmDatabase::className, DS.SARGS()).staticmethod("className")
         ;
 }
@@ -91,9 +117,19 @@ PySmDatabase::PySmDatabase(PySmDatabaseImpl* ptr)
 {
 }
 
-PySmDatabase::PySmDatabase(PySmDatabaseImpl& other)
+PySmDatabase::PySmDatabase(const PySmDatabaseImpl& other)
     : PySmComponent(other)
 {
+}
+
+boost::python::list PySmDatabase::smObjects()
+{
+    PyAutoLockGIL lock;
+    boost::python::list pylist;
+    const auto& v = impObj()->GetEnumerator();
+    for (const auto& i : v)
+        pylist.append(PySmPersist(i));
+    return pylist;
 }
 
 std::string PySmDatabase::className()
@@ -116,6 +152,16 @@ void makePySmSheetSetMgrWrapper()
 {
     PyDocString DS("SmSheetSetMgr");
     class_<PySmSheetSetMgr>("SmSheetSetMgr")
+
+        .def("createDatabase", &PySmSheetSetMgr::createDatabase1)
+        .def("createDatabase", &PySmSheetSetMgr::createDatabase2)
+        .def("openDatabase", &PySmSheetSetMgr::openDatabase)
+        .def("findOpenDatabase", &PySmSheetSetMgr::findOpenDatabase)
+        .def("closeAll", &PySmSheetSetMgr::closeAll)
+        .def("close", &PySmSheetSetMgr::close)
+        .def("getParentSheetSet", &PySmSheetSetMgr::getParentSheetSet)
+        .def("getSheetFromLayout", &PySmSheetSetMgr::getSheetFromLayout)
+        .def("databases", &PySmSheetSetMgr::databases)
         .def("className", &PySmSheetSetMgr::className, DS.SARGS()).staticmethod("className")
         ;
 }
@@ -125,13 +171,59 @@ PySmSheetSetMgr::PySmSheetSetMgr()
 {
 }
 
+PySmDatabase PySmSheetSetMgr::createDatabase1(const std::string& filename)
+{
+    return PySmDatabase(impObj()->CreateDatabase(utf8_to_wstr(filename).c_str()));
+}
+
+PySmDatabase PySmSheetSetMgr::createDatabase2(const std::string& filename, const std::string& templatefilename, bool bAlwaysCreate)
+{
+    return PySmDatabase(impObj()->CreateDatabase(utf8_to_wstr(filename).c_str(), utf8_to_wstr(templatefilename).c_str(), bAlwaysCreate));
+}
+
+PySmDatabase PySmSheetSetMgr::openDatabase(const std::string& filename)
+{
+    return PySmDatabase(impObj()->OpenDatabase(utf8_to_wstr(filename).c_str()));
+}
+
+PySmDatabase PySmSheetSetMgr::findOpenDatabase(const std::string& filename)
+{
+    return PySmDatabase(impObj()->FindOpenDatabase(utf8_to_wstr(filename).c_str()));
+}
+
+void PySmSheetSetMgr::closeAll()
+{
+    impObj()->CloseAll();
+}
+
+void PySmSheetSetMgr::close(PySmDatabase& db)
+{
+    impObj()->Close(*db.impObj());
+}
+
+boost::python::tuple PySmSheetSetMgr::getParentSheetSet(const std::string& dwg, const std::string& layout)
+{
+    PyAutoLockGIL lock;
+    const auto& val = impObj()->GetParentSheetSet(utf8_to_wstr(dwg).c_str(), utf8_to_wstr(layout).c_str());
+    //
+    return boost::python::make_tuple();
+}
+
+boost::python::tuple PySmSheetSetMgr::getSheetFromLayout(PyDbObject& pAcDbLayout)
+{
+    PyAutoLockGIL lock;
+    const auto& val = impObj()->GetSheetFromLayout(pAcDbLayout.impObj());
+    //
+    return boost::python::make_tuple();
+}
+
 boost::python::list PySmSheetSetMgr::databases() const
 {
     PyAutoLockGIL lock;
     boost::python::list pylist;
     const auto& v = impObj()->GetDatabaseEnumerator();
-    //for (const auto i : v)
-        //pylist.append(i);
+    for (const auto& i : v)
+        pylist.append(PySmDatabase(i));
     return pylist;
 }
 
