cfa59359d7cd24e91f5dd6a14d5c7545d052e115
Daniel <dwmcabinets@yahoo.com>
2024-07-16 17:17:44 +0800

    add Ice.lib

--- PyRxCore/PyBrxIFC.h
+++ PyRxCore/PyBrxIFC.h
@@ -6,10 +6,18 @@
 //---------------------------------------------------------------------------------------- -
 //PyIFCGuid
 void makePyIFCGuidrapper();
-class PyPyIFCGuid
+class PyIFCGuid
 {
 public:
+    PyIFCGuid();
+    PyIFCGuid(Ice::IfcApi::Guid* pObject, bool autoDelete);
+    ~PyIFCGuid() = default;
 
+    static std::string      className();
+public:
+    Ice::IfcApi::Guid*      impObj(const std::source_location& src = std::source_location::current()) const;
+public:
+    std::shared_ptr<Ice::IfcApi::Guid> m_pyImp;
 };
 
 #endif
