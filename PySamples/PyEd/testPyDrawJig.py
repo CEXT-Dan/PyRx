915e05f30768a0475b7e20bb6e27a37c382150ac
Daniel <dwmcabinets@yahoo.com>
2024-09-13 15:12:08 +0800

    update samples

--- PySamples/PyEd/testPyDrawJig.py
+++ PySamples/PyEd/testPyDrawJig.py
@@ -1,49 +1,38 @@
 import traceback
 from pyrx_imp import Ap, Db, Ed, Ge, Gi, Gs, Rx
 
-
 print("command = pydrawjig")
 
-# just like in ARX, ent must not be null
 class MyDrawJig(Ed.DrawJig):
     def __init__(self, basepoint):
-            Ed.DrawJig.__init__(self)
-            self.curpoint = basepoint
-            self.basepoint = basepoint
-            self.lastpoint = basepoint
-        
-            self.line = Db.Line(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 100, 0))
-            self.line.setDatabaseDefaults()
+        Ed.DrawJig.__init__(self)
+        self.basepoint = basepoint
+        self.curpoint = basepoint
+        self.tri = [
+            Ge.Point3d(-1, 0, 0),
+            Ge.Point3d(1, 0, 0),
+            Ge.Point3d(0, 1, 0),
+            Ge.Point3d(-1, 0, 0),
+        ]
 
-            self.circle = Db.Circle(Ge.Point3d(50, 50, 0), Ge.Vector3d.kZAxis, 10)
-            self.circle.setDatabaseDefaults()
-
-
+    #get the mouse point, use base point or not
     def sampler(self):
-            self.setUserInputControls(Ed.UserInputControls.kAccept3dCoordinates)
-            point_result_tuple = self.acquirePoint()
-            self.curpoint = point_result_tuple[1]
-            
-            if point_result_tuple[0] == Ed.DragStatus.kNormal:
-                return Ed.DragStatus.kNoChange
-            return point_result_tuple[0]
+        self.setUserInputControls(Ed.UserInputControls.kAccept3dCoordinates)
+        ds, self.curpoint = self.acquirePoint(self.basepoint)
+        return ds
 
-    # C++ update returns False is not overridden
     def update(self):
-        try:
-            self.lastpoint = self.curpoint
-            return True
-        except Exception as err:
-            print(err)
+        return True
 
-    #worldDraw
-    def worldDraw(self, wd : Gi.WorldDraw):
+    # draw anything you like, even your block, you can also scale or apply
+    # color
+    def worldDraw(self, wd: Gi.WorldDraw):
         try:
-            mat = Ge.Matrix3d.translation(self.curpoint-self.basepoint)
+            mat = Ge.Matrix3d.translation(self.curpoint - self.basepoint)
             geo = wd.geometry()
             geo.pushModelTransform(mat)
-            geo.draw(self.line)
-            geo.draw(self.circle)
+            #draw tri as polyline
+            geo.polyline(self.tri,Ge.Vector3d.kZAxis)
             geo.popModelTransform()
             return True
         except Exception as err:
@@ -52,7 +41,7 @@ class MyDrawJig(Ed.DrawJig):
 def PyRxCmd_pydrawjig():
     try:
         jig = MyDrawJig(Ge.Point3d(0, 0, 0))
-        jig.setDispPrompt("\nPick endPoint:\n")
+        jig.setDispPrompt("\nPick point:\n")
         res = jig.drag()
         print("done", res)
     except Exception as err:
