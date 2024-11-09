901fdf18699fd2e8ef1e8452b060b07f64b96d61
Daniel <dwmcabinets@yahoo.com>
2024-11-09 08:33:27 +0800

    progress on PyBrEntity

--- PyRxCore/PyBrEntity.cpp
+++ PyRxCore/PyBrEntity.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "PyBrEntity.h"
 #include "PyGeBoundBlock3d.h"
+#include "PyGeLinearEnt3d.h"
 
 void makePyBrEntityWrapper()
 {
@@ -62,6 +63,39 @@ boost::python::tuple PyBrEntity::getPointContainment(const AcGePoint3d& point)
     AcBrEntity* container = nullptr;
     PyThrowBadBr(impObj()->getPointContainment(point, containment, container));
     return boost::python::make_tuple(containment, PyBrEntity{ container , true });
+}
+
+boost::python::tuple PyBrEntity::getLineContainment(const PyGeLinearEnt3d& line, const Adesk::UInt32 numHitsWanted)
+{
+    PyAutoLockGIL lock;
+    Adesk::UInt32 numHitsFound = 0;
+    AcBrHit* hits = nullptr;
+    PyThrowBadBr(impObj()->getLineContainment(*line.impObj(), numHitsWanted, numHitsFound, hits));
+
+    //TODO:
+    PyThrowBadEs(eNotImplementedYet); //AcBrHitArray not PyBrEntity
+    return boost::python::make_tuple(numHitsFound, PyBrEntity{ hits , true });
+}
+
+void PyBrEntity::setValidationLevel(const AcBr::ValidationLevel level)
+{
+    PyThrowBadBr(impObj()->setValidationLevel(level));
+}
+
+AcBr::ValidationLevel PyBrEntity::getValidationLevel() const
+{
+    AcBr::ValidationLevel level;
+    PyThrowBadBr(impObj()->getValidationLevel(level));
+    return level;
+}
+
+Adesk::Boolean PyBrEntity::brepChanged() const
+{
+#if defined(_BRXTARGET250)
+    throw PyNotimplementedByHost();
+#else
+    return impObj()->brepChanged();
+#endif
 }
 
 PyRxClass PyBrEntity::desc()
