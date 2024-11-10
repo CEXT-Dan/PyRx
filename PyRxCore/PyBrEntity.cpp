f4b1fc51698daabc557269bd4d530112ef7e3257
Daniel <dwmcabinets@yahoo.com>
2024-11-10 10:34:56 +0800

    brep outline classes

--- PyRxCore/PyBrEntity.cpp
+++ PyRxCore/PyBrEntity.cpp
@@ -3,6 +3,8 @@
 #include "PyGeBoundBlock3d.h"
 #include "PyGeLinearEnt3d.h"
 
+//-----------------------------------------------------------------------------------------
+// PyBrEntity
 void makePyBrEntityWrapper()
 {
 
@@ -100,12 +102,12 @@ Adesk::Boolean PyBrEntity::brepChanged() const
 
 PyRxClass PyBrEntity::desc()
 {
-    return PyRxClass(AcRxClass::desc(), false);
+    return PyRxClass(AcBrEntity::desc(), false);
 }
 
 std::string  PyBrEntity::className()
 {
-    return std::string{ "AcRxClass" };
+    return std::string{ "AcBrEntity" };
 }
 
 AcBrEntity* PyBrEntity::impObj(const std::source_location& src /*= std::source_location::current()*/) const
@@ -114,4 +116,249 @@ AcBrEntity* PyBrEntity::impObj(const std::source_location& src /*= std::source_l
         throw PyNullObject(src);
     }
     return static_cast<AcBrEntity*>(m_pyImp.get());
+}
+
+//-----------------------------------------------------------------------------------------
+// PyBrBrep
+void makePyBrBrepWrapper()
+{
+
+}
+
+PyBrBrep::PyBrBrep(const AcRxObject* ptr)
+    : PyBrEntity(ptr)
+{
+}
+
+PyBrBrep::PyBrBrep(AcRxObject* ptr, bool autoDelete)
+    : PyBrEntity(ptr, autoDelete)
+{
+}
+
+PyRxClass PyBrBrep::desc()
+{
+    return PyRxClass(AcBrBrep::desc(), false);
+}
+
+std::string PyBrBrep::className()
+{
+    return std::string{ "AcBrBrep" };
+}
+
+AcBrBrep* PyBrBrep::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<AcBrBrep*>(m_pyImp.get());
+}
+
+//-----------------------------------------------------------------------------------------
+// PyBrComplex
+void makePyBrComplexWrapper()
+{
+
+}
+
+PyBrComplex::PyBrComplex(const AcRxObject* ptr)
+    : PyBrEntity(ptr)
+{
+}
+
+PyBrComplex::PyBrComplex(AcRxObject* ptr, bool autoDelete)
+    : PyBrEntity(ptr, autoDelete)
+{
+}
+
+PyRxClass PyBrComplex::desc()
+{
+    return PyRxClass(AcBrComplex::desc(), false);
+}
+
+std::string PyBrComplex::className()
+{
+    return std::string{ "AcBrComplex" };
+}
+
+AcBrComplex* PyBrComplex::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<AcBrComplex*>(m_pyImp.get());
+}
+
+//-----------------------------------------------------------------------------------------
+// PyBrEdge
+void makePyBrEdgeWrapper()
+{
+
+}
+
+PyBrEdge::PyBrEdge(const AcRxObject* ptr)
+    : PyBrEntity(ptr)
+{
+}
+
+PyBrEdge::PyBrEdge(AcRxObject* ptr, bool autoDelete)
+    : PyBrEntity(ptr, autoDelete)
+{
+}
+
+PyRxClass PyBrEdge::desc()
+{
+    return PyRxClass(AcBrEdge::desc(), false);
+}
+
+std::string PyBrEdge::className()
+{
+    return std::string{ "AcBrEdge" };
+}
+
+AcBrEdge* PyBrEdge::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<AcBrEdge*>(m_pyImp.get());
+}
+
+//-----------------------------------------------------------------------------------------
+// PyBrFace
+void makePyBrFaceWrapper()
+{
+
+}
+
+PyBrFace::PyBrFace(const AcRxObject* ptr)
+    : PyBrEntity(ptr)
+{
+}
+
+PyBrFace::PyBrFace(AcRxObject* ptr, bool autoDelete)
+    : PyBrEntity(ptr, autoDelete)
+{
+}
+
+PyRxClass PyBrFace::desc()
+{
+    return PyRxClass(AcBrFace::desc(), false);
+}
+
+std::string PyBrFace::className()
+{
+    return std::string{ "AcBrFace" };
+}
+
+AcBrFace* PyBrFace::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<AcBrFace*>(m_pyImp.get());
+}
+
+//-----------------------------------------------------------------------------------------
+// PyBrLoop
+void makePyBrLoopWrapper()
+{
+
+}
+
+PyBrLoop::PyBrLoop(const AcRxObject* ptr)
+    : PyBrEntity(ptr)
+{
+}
+
+PyBrLoop::PyBrLoop(AcRxObject* ptr, bool autoDelete)
+    : PyBrEntity(ptr, autoDelete)
+{
+}
+
+PyRxClass PyBrLoop::desc()
+{
+    return PyRxClass(AcBrLoop::desc(), false);
+}
+
+std::string PyBrLoop::className()
+{
+    return std::string{ "AcBrLoop" };
+}
+
+AcBrLoop* PyBrLoop::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<AcBrLoop*>(m_pyImp.get());
+}
+
+//-----------------------------------------------------------------------------------------
+// PyBrShell
+void makePyBrShellWrapper()
+{
+
+}
+
+PyBrShell::PyBrShell(const AcRxObject* ptr)
+    : PyBrEntity(ptr)
+{
+}
+
+PyBrShell::PyBrShell(AcRxObject* ptr, bool autoDelete)
+    : PyBrEntity(ptr, autoDelete)
+{
+}
+
+PyRxClass PyBrShell::desc()
+{
+    return PyRxClass(AcBrShell::desc(), false);
+}
+
+std::string PyBrShell::className()
+{
+    return std::string{ "AcBrShell" };
+}
+
+AcBrShell* PyBrShell::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<AcBrShell*>(m_pyImp.get());
+}
+
+//-----------------------------------------------------------------------------------------
+// PyBrVertex
+void makePyBrVertexWrapper()
+{
+
+}
+
+PyBrVertex::PyBrVertex(const AcRxObject* ptr)
+    : PyBrEntity(ptr)
+{
+}
+
+PyBrVertex::PyBrVertex(AcRxObject* ptr, bool autoDelete)
+    : PyBrEntity(ptr, autoDelete)
+{
+}
+
+PyRxClass PyBrVertex::desc()
+{
+    return PyRxClass(AcBrVertex::desc(), false);
+}
+
+std::string PyBrVertex::className()
+{
+    return std::string{ "AcBrVertex" };
+}
+
+AcBrVertex* PyBrVertex::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<AcBrVertex*>(m_pyImp.get());
 }
