111ea84491a763861b96dd98e45fa586d2f8ce37
Daniel <dwmcabinets@yahoo.com>
2024-11-22 19:18:13 +0800

    missed static keyword

--- PyRxCore/PyBrxDbProperties.h
+++ PyRxCore/PyBrxDbProperties.h
@@ -16,7 +16,7 @@ public:
     static boost::python::list  listAll(const PyDbObjectId& id);
     static boost::python::tuple isValid(const PyDbObjectId& id, const std::string& propertyName);
     static boost::python::tuple isReadOnly(const PyDbObjectId& id, const std::string& propertyName);
-    PyDbAcValue                 getValue(const PyDbObjectId& id, const std::string& propertyName);
+    static PyDbAcValue          getValue(const PyDbObjectId& id, const std::string& propertyName);
     static void                 setValue(const PyDbObjectId& id, const std::string& propertyName, const PyDbAcValue& value);
 };
 
