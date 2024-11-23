83f342bb7d8f800db227936e1dbd78f61bcadb8f
Daniel <dwmcabinets@yahoo.com>
2024-11-24 06:06:19 +0800

    added Brx::Core::getOpenObjects() useful for testing garbage collection

--- PyRxCore/PyBrxCore.h
+++ PyRxCore/PyBrxCore.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #ifdef BRXAPP
+
+class PyRxObject;
+
 void makePyBrxCoreWrapper();
 
 class PyBrxCore
 {
 public:
     static bool isLicenseAvailable(BricsCAD::LicensedFeature feature);
+    static boost::python::list getOpenObjects();
 };
 
 
