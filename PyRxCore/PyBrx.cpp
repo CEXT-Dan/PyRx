974a641322c13de6b00d406fe098ef34b547c166
Daniel <dwmcabinets@yahoo.com>
2024-11-23 12:36:34 +0800

    added Brx.Core for free Brx functions added Brx.Core.isLicenseAvailable and corresponding enum Brx.LicensedFeature

--- PyRxCore/PyBrx.cpp
+++ PyRxCore/PyBrx.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "PyBrx.h"
 #include "PyBrxDbProperties.h"
+#include "PyBrxCore.h"
 
 #ifdef BRXAPP
 
@@ -10,9 +11,17 @@ BOOST_PYTHON_MODULE(PyBrx)
 {
     docstring_options local_docstring_options(py_show_user_defined, py_show_py_signatures, py_show_cpp_signatures);
 
+    makePyBrxCoreWrapper();
 #ifndef _BRXTARGET240
     makePyBrxDbPropertiesWrapper();
 #endif
+    enum_<BricsCAD::LicensedFeature>("LicensedFeature")
+        .value("eCore", BricsCAD::LicensedFeature::eCore)
+        .value("eCommunicator", BricsCAD::LicensedFeature::eCommunicator)
+        .value("eBim", BricsCAD::LicensedFeature::eBim)
+        .value("eMechanical", BricsCAD::LicensedFeature::eMechanical)
+        .export_values()
+        ;
 }
 
 void initPyBrxModule()
