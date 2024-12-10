3074342ca1e510cf0def95f1e532a399f45d4a53
Daniel <dwmcabinets@yahoo.com>
2024-12-10 15:16:20 +0800

    improve test_equals_hash

--- PyRxCore/acrxEntryPoint.cpp
+++ PyRxCore/acrxEntryPoint.cpp
@@ -474,7 +474,13 @@ public:
 
     static void AcRxPyApp_idoit(void)
     {
+        AcDbLine* pLine = new AcDbLine();
+        AcDbLine* pline2 = AcDbLine::cast(pLine);
 
+        std::size_t h1 = std::hash<AcDbLine*>{}(pLine);
+        std::size_t h2 = std::hash<AcDbLine*>{}(pline2);
+
+        acutPrintf(_T("\nEQ = %ls"), h1 == h2 ? _T("True") : _T("False"));
     }
 #endif
 };
