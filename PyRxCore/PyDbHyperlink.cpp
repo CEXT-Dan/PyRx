b3dcd236ac40f93010753af2cc10c2da5e69f6ab
Daniel <dwmcabinets@yahoo.com>
2024-12-16 13:11:12 +0800

    [#143] progress on AcDbHyperlink

--- PyRxCore/PyDbHyperlink.cpp
+++ PyRxCore/PyDbHyperlink.cpp
@@ -1,2 +1,179 @@
 #include "stdafx.h"
 #include "PyDbHyperlink.h"
+#include "PyDbObject.h"
+
+//-----------------------------------------------------------------------------------------
+//PyDbHyperlink
+void makePyDbHyperlinkWrapper()
+{
+
+}
+
+PyDbHyperlink::PyDbHyperlink(AcDbHyperlink* ptr, bool autoDelete)
+    : m_pyImp(ptr, PySharedObjectDeleter<AcDbHyperlink>(autoDelete))
+{
+}
+
+std::string PyDbHyperlink::name() const
+{
+    return wstr_to_utf8(impObj()->name());
+}
+
+void PyDbHyperlink::setName(const std::string& cName)
+{
+    impObj()->setName(utf8_to_wstr(cName).c_str());
+}
+
+std::string PyDbHyperlink::description() const
+{
+    return wstr_to_utf8(impObj()->description());
+}
+
+void PyDbHyperlink::setDescription(const std::string& cDescription)
+{
+    impObj()->setDescription(utf8_to_wstr(cDescription).c_str());
+}
+
+std::string PyDbHyperlink::subLocation() const
+{
+    return wstr_to_utf8(impObj()->subLocation());
+}
+
+void PyDbHyperlink::setSubLocation(const std::string& cSubLocation)
+{
+    impObj()->setSubLocation(utf8_to_wstr(cSubLocation).c_str());
+}
+
+std::string PyDbHyperlink::getDisplayString() const
+{
+    return wstr_to_utf8(impObj()->getDisplayString());
+}
+
+bool PyDbHyperlink::isOutermostContainer() const
+{
+    return impObj()->isOutermostContainer();
+}
+
+int PyDbHyperlink::getNestedLevel() const
+{
+    return impObj()->getNestedLevel();
+}
+
+Adesk::Int32 PyDbHyperlink::flags() const
+{
+    return impObj()->flags();
+}
+
+void PyDbHyperlink::setFlags(const Adesk::Int32 lFlags)
+{
+    impObj()->setFlags(lFlags);
+}
+
+std::string PyDbHyperlink::className()
+{
+    return "AcDbHyperlink";
+}
+
+AcDbHyperlink* PyDbHyperlink::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//-----------------------------------------------------------------------------------------
+//PyDbHyperlinkCollection
+void makePyDbHyperlinkCollectionWrapper()
+{
+
+}
+
+PyDbHyperlinkCollection::PyDbHyperlinkCollection(AcDbHyperlinkCollection* ptr, bool autoDelete)
+    : m_pyImp(ptr, PySharedObjectDeleter<AcDbHyperlinkCollection>(autoDelete))
+{
+}
+
+void PyDbHyperlinkCollection::addHead1(const std::string& sName, const std::string& sDescription)
+{
+    impObj()->addHead(utf8_to_wstr(sName).c_str(), utf8_to_wstr(sDescription).c_str());
+}
+
+void PyDbHyperlinkCollection::addHead2(const std::string& sName, const std::string& sDescription, const std::string& sSubLocation)
+{
+    impObj()->addHead(utf8_to_wstr(sName).c_str(), utf8_to_wstr(sDescription).c_str(), utf8_to_wstr(sSubLocation).c_str());
+}
+
+void PyDbHyperlinkCollection::addTail1(const std::string& sName, const std::string& sDescription)
+{
+    impObj()->addTail(utf8_to_wstr(sName).c_str(), utf8_to_wstr(sDescription).c_str());
+}
+
+void PyDbHyperlinkCollection::addTail2(const std::string& sName, const std::string& sDescription, const std::string& sSubLocation)
+{
+    impObj()->addTail(utf8_to_wstr(sName).c_str(), utf8_to_wstr(sDescription).c_str(), utf8_to_wstr(sSubLocation).c_str());
+}
+
+void PyDbHyperlinkCollection::addAt1(int nIndex, const std::string& sName, const std::string& sDescription)
+{
+    impObj()->addAt(nIndex, utf8_to_wstr(sName).c_str(), utf8_to_wstr(sDescription).c_str());
+}
+
+void PyDbHyperlinkCollection::addAt2(int nIndex, const std::string& sName, const std::string& sDescription, const std::string& sSubLocation)
+{
+    impObj()->addAt(nIndex,utf8_to_wstr(sName).c_str(), utf8_to_wstr(sDescription).c_str(), utf8_to_wstr(sSubLocation).c_str());
+}
+
+void PyDbHyperlinkCollection::addAt3(int index, const PyDbHyperlink& pHLink)
+{
+#if defined(_BRXTARGET250)
+    throw PyNotimplementedByHost();
+#else
+    impObj()->addAt(index, pHLink.impObj());
+#endif
+}
+
+void PyDbHyperlinkCollection::removeHead()
+{
+    impObj()->removeHead();
+}
+
+void PyDbHyperlinkCollection::removeTail()
+{
+    impObj()->removeTail();
+}
+
+void PyDbHyperlinkCollection::removeAt(int nIndex)
+{
+    impObj()->removeAt(nIndex);
+}
+
+int PyDbHyperlinkCollection::count() const
+{
+    return impObj()->count();
+}
+
+PyDbHyperlink PyDbHyperlinkCollection::item(int nIndex) const
+{
+    return PyDbHyperlink{ impObj()->item(nIndex), false };
+}
+
+std::string PyDbHyperlinkCollection::className()
+{
+    return "AcDbHyperlinkCollection";
+}
+
+AcDbHyperlinkCollection* PyDbHyperlinkCollection::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//-----------------------------------------------------------------------------------------
+//PyDbEntityHyperlinkPE
+void makePyDbEntityHyperlinkPEWrapper()
+{
+
+}
--- PyRxCore/PyDbHyperlink.h
+++ PyRxCore/PyDbHyperlink.h
@@ -1,5 +1,82 @@
 #pragma once
+
+#include "PyRxObject.h"
+#include "achapi.h"
+
+#pragma pack (push, 8)
+class PyDbObject;
+
+//-----------------------------------------------------------------------------------------
+//PyDbHyperlink
+void makePyDbHyperlinkWrapper();
+
 class PyDbHyperlink
 {
+public:
+    PyDbHyperlink(AcDbHyperlink* ptr, bool autoDelete);
+    virtual ~PyDbHyperlink() = default;
+    std::string     name() const;
+    void            setName(const std::string& cName);
+    std::string     description() const;
+    void            setDescription(const std::string& cDescription);
+    std::string     subLocation() const;
+    void            setSubLocation(const std::string& cSubLocation);
+    std::string     getDisplayString() const;
+    bool            isOutermostContainer() const;
+    int             getNestedLevel() const;
+    Adesk::Int32    flags() const;
+    void            setFlags(const Adesk::Int32 lFlags);
+    static std::string  className();
+public:
+    AcDbHyperlink* impObj(const std::source_location& src = std::source_location::current()) const;
+
+public:
+    std::shared_ptr<AcDbHyperlink> m_pyImp;
 };
 
+//-----------------------------------------------------------------------------------------
+//PyDbHyperlinkCollection
+void makePyDbHyperlinkCollectionWrapper();
+
+class PyDbHyperlinkCollection
+{
+public:
+    PyDbHyperlinkCollection(AcDbHyperlinkCollection* ptr, bool autoDelete);
+    virtual ~PyDbHyperlinkCollection() = default;
+    void            addHead1(const std::string& sName, const std::string& sDescription);
+    void            addHead2(const std::string& sName, const std::string& sDescription, const std::string& sSubLocation);
+    void            addTail1(const std::string& sName, const std::string& sDescription);
+    void            addTail2(const std::string& sName, const std::string& sDescription, const std::string& sSubLocation);
+    void            addAt1(int nIndex, const std::string& sName, const std::string& sDescription);
+    void            addAt2(int nIndex, const std::string& sName, const std::string& sDescription, const std::string& sSubLocation);
+    void            addAt3(int index, const PyDbHyperlink& pHLink);
+    void            removeHead();
+    void            removeTail();
+    void            removeAt(int nIndex);
+    int             count() const;
+    PyDbHyperlink   item(int nIndex) const;
+    static std::string  className();
+public:
+    AcDbHyperlinkCollection* impObj(const std::source_location& src = std::source_location::current()) const;
+public:
+    std::shared_ptr<AcDbHyperlinkCollection> m_pyImp;
+};
+
+//-----------------------------------------------------------------------------------------
+//PyDbEntityHyperlinkPE
+void makePyDbEntityHyperlinkPEWrapper();
+
+class PyDbEntityHyperlinkPE : public PyRxObject
+{
+public:
+    boost::python::tuple getHyperlinkCollection1(const PyDbObject& obj);
+    boost::python::tuple getHyperlinkCollection2(bool bOneOnly, bool bIgnoreBlockDefinition);
+
+    static PyRxClass     desc();
+    static std::string   className();
+
+public:
+    AcDbEntityHyperlinkPE* impObj(const std::source_location& src = std::source_location::current()) const;
+};
+
+#pragma pack (pop)
