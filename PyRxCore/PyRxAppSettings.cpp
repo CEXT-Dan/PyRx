a033200058f183f0072e66c88d053c1a7245aeed
Daniel <dwmcabinets@yahoo.com>
2024-12-23 17:09:02 +0800

    [#145][#148] remove PyConfig_InitIsolatedConfig

--- PyRxCore/PyRxAppSettings.cpp
+++ PyRxCore/PyRxAppSettings.cpp
@@ -9,19 +9,6 @@ const std::filesystem::path& PyRxAppSettings::iniPath()
     return spath;
 }
 
-const std::tuple<bool, bool> PyRxAppSettings::pythonIsolated()
-{
-    std::array<wchar_t, 8> buffer = { 0 };
-    if (acedGetEnv(_T("PYRX_PYTHONISOLATED"), buffer.data(), buffer.size()) == RTNORM)
-    {
-        if (_wtoi(buffer.data()) == 1)
-            return std::make_tuple(true, true);
-    }
-    int nDefault = 0;
-    int result = GetPrivateProfileInt(_T("PYRXSETTINGS"), _T("PYTHONISOLATED"), nDefault, iniPath().c_str());
-    return std::make_tuple(true, result);
-}
-
 const std::tuple<bool, std::wstring> PyRxAppSettings::pythonvenv_path()
 {
     std::error_code ec;
@@ -32,12 +19,6 @@ const std::tuple<bool, std::wstring> PyRxAppSettings::pythonvenv_path()
             if (std::filesystem::exists(exepath.c_str(), ec))
                 return std::make_tuple(true, exepath.c_str());
         }
-    }
-    {
-        std::wstring exepath(MAX_PATH, 0);
-        GetPrivateProfileStringW(_T("PYRXSETTINGS"), _T("PYTHONEXECUTABLE"), _T(""), exepath.data(), exepath.size(), iniPath().c_str());
-        if (std::filesystem::exists(exepath.c_str(), ec))
-            return std::make_tuple(true, exepath.c_str());
     }
     return std::make_tuple(false, L"");
 }
--- PyRxCore/PyRxAppSettings.h
+++ PyRxCore/PyRxAppSettings.h
@@ -3,7 +3,6 @@ class PyRxAppSettings
 {
 public:
     static const std::filesystem::path& iniPath();
-    static const std::tuple<bool, bool> pythonIsolated();
     static const std::tuple<bool, std::wstring> pythonvenv_path();
 };
 
