b1ffcfae375c4f9a25d679bc54acfd796de1b498
Daniel <dwmcabinets@yahoo.com>
2024-12-25 04:06:40 +0800

    improve load_pyrx_onload

--- PyRxCore/PyRxAppSettings.cpp
+++ PyRxCore/PyRxAppSettings.cpp
@@ -22,3 +22,32 @@ const std::tuple<bool, std::wstring> PyRxAppSettings::pythonvenv_path()
     }
     return std::make_tuple(false, L"");
 }
+
+const std::tuple<bool, std::wstring> PyRxAppSettings::pyonload_path()
+{
+    std::error_code ec;
+    std::wstring buffer(5, 0);
+    if (GetEnvironmentVariable(_T("PYRX_DISABLE_ONLOAD"), buffer.data(), buffer.size()))
+    {
+        if (std::stoi(buffer) == 1)
+            return std::make_tuple(false, std::wstring());
+    }
+
+    std::wstring path(MAX_PATH, 0);
+    if (acedGetEnv(_T("PYRX_ONLOAD_PATH"), path.data(), path.size()) == RTNORM)
+    {
+        if (AcString foundPath; acdbHostApplicationServices()->findFile(foundPath, path.c_str()) == eOk && foundPath.length() != 0)
+        {
+            if (std::filesystem::exists((const wchar_t*)foundPath, ec))
+                return std::make_tuple(true, std::wstring((const wchar_t*)foundPath));
+        }
+    }
+
+    const auto pyrx_onloadPath = PyRxApp::modulePath() / _T("pyrx_onload.py");
+    if (AcString foundPath; acdbHostApplicationServices()->findFile(foundPath, pyrx_onloadPath.c_str()) == eOk && foundPath.length() != 0)
+    {
+        if (std::filesystem::exists((const wchar_t*)foundPath, ec))
+            return std::make_tuple(true, std::wstring((const wchar_t*)foundPath));
+    }
+    return std::make_tuple(false, std::wstring());
+}
