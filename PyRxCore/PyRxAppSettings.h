5d50a1799ddfa02221ee889c22d77e5da916a810
Daniel <dwmcabinets@yahoo.com>
2024-12-25 04:06:40 +0800

    improve load_pyrx_onload

--- PyRxCore/PyRxAppSettings.h
+++ PyRxCore/PyRxAppSettings.h
@@ -4,5 +4,6 @@ class PyRxAppSettings
 public:
     static const std::filesystem::path& iniPath();
     static const std::tuple<bool, std::wstring> pythonvenv_path();
+    static const std::tuple<bool, std::wstring> pyonload_path();
 };
 
