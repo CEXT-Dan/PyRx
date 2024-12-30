7cc36c48149bf23ac325c896bd910315ea2bc562
Daniel <dwmcabinets@yahoo.com>
2024-12-31 05:06:36 +0800

    PyBrxPanelImpl on size

--- PyRxCore/PyBrxPanel.h
+++ PyRxCore/PyBrxPanel.h
@@ -22,6 +22,7 @@ public:
     virtual BOOL CreateControlBar(LPCREATESTRUCT lpCreateStruct) override;
 
     DECLARE_MESSAGE_MAP();
+    afx_msg void OnSize(UINT nType, int cx, int cy);
 
 private:
     std::unique_ptr<PyBrxChild> m_child;
@@ -46,6 +47,7 @@ public:
 
     DECLARE_MESSAGE_MAP();
     afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+    afx_msg void OnSize(UINT nType, int cx, int cy);
 private:
     wxPanel* m_panel = nullptr;
     wxWindow* m_thiswin = nullptr;
