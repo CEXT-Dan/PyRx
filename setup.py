1112b261976e2b444c70cf5d997309d4a4096dbe
Daniel <dwmcabinets@yahoo.com>
2024-12-31 17:20:56 +0800

    move long_description.md

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@ from pathlib import Path
 
 from setuptools import setup
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "GitResources/long_description.md").read_text()
-
 setup(
-    long_description=long_description,
+    long_description="long_description.md",
     long_description_content_type="text/markdown",
 )
