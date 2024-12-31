3b50169a0c5741f33dc49b7c5251e9195534af96
Daniel <dwmcabinets@yahoo.com>
2024-12-31 13:56:35 +0800

    [#159] added PyRxAppSettings::getCommandLineArgs()

--- PyRxCore/PyRxAppSettings.h
+++ PyRxCore/PyRxAppSettings.h
@@ -5,5 +5,6 @@ public:
     static const std::filesystem::path& iniPath();
     static const std::tuple<bool, std::wstring> pythonvenv_path();
     static const std::tuple<bool, std::wstring> pyonload_path();
+    static std::vector<std::wstring>& getCommandLineArgs();
 };
 
