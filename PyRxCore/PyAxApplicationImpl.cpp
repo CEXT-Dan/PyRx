bcb7edf789aa9bcbb073e7a022141cc37acca236
Daniel <dwmcabinets@yahoo.com>
2025-01-02 11:50:54 +0800

    [#165] import interfaces

--- PyRxCore/PyAxApplicationImpl.cpp
+++ PyRxCore/PyAxApplicationImpl.cpp
@@ -1,2 +1,17 @@
 #include "stdafx.h"
 #include "PyAxApplicationImpl.h"
+
+
+#ifdef PYRXDEBUG
+
+#if defined(_ARXTARGET)
+#include "axboiler.h"
+#endif
+
+PyAxApplicationImpl::PyAxApplicationImpl()
+ : m_pyimp(acedGetIDispatch(FALSE))
+{
+}
+
+
+#endif
--- PyRxCore/PyAxApplicationImpl.h
+++ PyRxCore/PyAxApplicationImpl.h
@@ -1,5 +1,48 @@
 #pragma once
+
+#ifdef PYRXDEBUG
+
+#if defined(_BRXTARGET) && (_BRXTARGET <= 250)
+#import "axbricscaddb1.tlb" raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#import "axbricscadapp1.tlb" raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#endif
+
+#if defined(_ARXTARGET) && (_ARXTARGET <= 243)
+#import "acax24ENU.tlb"  raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#endif
+
+#if defined(_ARXTARGET) && (_ARXTARGET == 250)
+#import "acax25ENU.tlb"  raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#endif
+
+#if defined(_ZRXTARGET) && (_ZRXTARGET == 240)
+#import "zwcad21.tlb"  raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#endif
+
+#if defined(_ZRXTARGET) && (_ZRXTARGET == 250)
+#import "zwcad25.tlb"  raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#endif
+
+#if defined(_GRXTARGET) && (_GRXTARGET == 240)
+#import "acax.tlb"  raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#endif
+
+#if defined(_GRXTARGET) && (_GRXTARGET == 250)
+#import "acax.tlb"  raw_interfaces_only, raw_native_types, no_namespace, named_guids
+#endif
+
+#if defined(_ZRXTARGET)
+#define IAcadApplicationPtr IZcadApplicationPtr
+#endif
+
 class PyAxApplicationImpl
 {
+public:
+    PyAxApplicationImpl();
+    ~PyAxApplicationImpl() = default;
+
+public:
+    IAcadApplicationPtr m_pyimp;
 };
 
+#endif
