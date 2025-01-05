9fad59b9d1d4b2d9e7405b3e6437b03605b275fa
Daniel <dwmcabinets@yahoo.com>
2025-01-05 08:04:58 +0800

    improve test_ap_lisp_decorator

--- unitTests/UnitTestMisc.py
+++ unitTests/UnitTestMisc.py
@@ -19,33 +19,31 @@ host = Ap.Application.hostAPI()
 class TestMisc(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestMisc, self).__init__(*args, **kwargs)
-    
+
     @unittest.skipIf(*testcfg.makeSkip(testcfg.ETFlags.eGRX))
     def test_gs_getBlockImage(self):
         objHnd = Db.Handle("36f")
         objId = dbc.dbs["dynblock"].getObjectId(False, objHnd)
         img: wx.Image = Gs.Core.getBlockImage(objId, 32, 32, 1.0, [0, 0, 0])
         self.assertTrue(img.IsOk, True)
-        self.assertEqual(img.GetHeight(),32)
-        self.assertEqual(img.GetWidth(),32)
-        
+        self.assertEqual(img.GetHeight(), 32)
+        self.assertEqual(img.GetWidth(), 32)
 
     def test_ap_command_decorator(self):
         commands = Ed.Core.getCommands()
         subcmds = commands["PY_UNITTESTRUNNER"]
         names = [tp[0] for tp in subcmds]
-        self.assertTrue('FAR' in names)
-        self.assertTrue('FOO' in names)
-        self.assertTrue('FOOBAR' in names)
-        self.assertTrue('FOOFAR' in names)
-        
+        self.assertTrue("FAR" in names)
+        self.assertTrue("FOO" in names)
+        self.assertTrue("FOOBAR" in names)
+        self.assertTrue("FOOFAR" in names)
+
     def test_ap_lisp_decorator(self):
-        commands = Ed.Core.getCommands()
-        args = [(Rx.LispType.kText, "C:LPF1"),(Rx.LispType.kNone,0) ]
+        args = [(Rx.LispType.kText, "C:LPF1"), (Rx.LispType.kNone, 0)]
         rbout = Ed.Core.invoke(args)
-        self.assertEqual(rbout,[(Rx.LispType.kInt16,2)])
-        
-        
+        self.assertEqual(rbout, [(Rx.LispType.kInt16, 2)])
+
+
 def MiscTester():
     try:
         suite = unittest.TestLoader().loadTestsFromTestCase(TestMisc)
