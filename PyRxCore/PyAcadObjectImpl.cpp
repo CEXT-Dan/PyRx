fd201720a627e940c582029bd7a15f67c7bcd117
Daniel <dwmcabinets@yahoo.com>
2025-01-07 18:47:07 +0800

    organize ActiveX classes

---
+++ PyRxCore/PyAcadObjectImpl.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "PyAcadObjectImpl.h"
+
+#ifdef PYRXDEBUG
+//------------------------------------------------------------------------------------
+//PyIAcadObjectImpl
+PyIAcadObjectImpl::PyIAcadObjectImpl(IAcadObject* ptr)
+    : m_pimpl(ptr)
+{
+}
+
+IAcadObject* PyIAcadObjectImpl::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pimpl == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<IAcadObject*>(m_pimpl.GetInterfacePtr());
+}
+
+#endif
