1079a802de711d03ba2eeff4ddcc55b652815c07
Daniel <dwmcabinets@yahoo.com>
2025-01-08 05:57:11 +0800

    com wrappers

--- PyRxCore/PyAcadObjectImpl.cpp
+++ PyRxCore/PyAcadObjectImpl.cpp
@@ -77,6 +77,79 @@ IAcadGroup* PyIAcadGroupImpl::impObj(const std::source_location& src /*= std::so
     return static_cast<IAcadGroup*>(m_pimpl.GetInterfacePtr());
 }
 
+//------------------------------------------------------------------------------------
+//PyIAcadGroupsImpl
+PyIAcadGroupsImpl::PyIAcadGroupsImpl(IAcadGroups* ptr)
+    : PyIAcadObjectImpl(ptr)
+{
+}
+
+IAcadGroups* PyIAcadGroupsImpl::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pimpl == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<IAcadGroups*>(m_pimpl.GetInterfacePtr());
+}
+
+//------------------------------------------------------------------------------------
+//PyIAcadDimStyleImpl
+PyIAcadDimStyleImpl::PyIAcadDimStyleImpl(IAcadDimStyle* ptr)
+    : PyIAcadObjectImpl(ptr)
+{
+}
+
+IAcadDimStyle* PyIAcadDimStyleImpl::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pimpl == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<IAcadDimStyle*>(m_pimpl.GetInterfacePtr());
+}
+
+//------------------------------------------------------------------------------------
+//PyIAcadDimStylesImpl
+PyIAcadDimStylesImpl::PyIAcadDimStylesImpl(IAcadDimStyles* ptr)
+    : PyIAcadObjectImpl(ptr)
+{
+}
+
+IAcadDimStyles* PyIAcadDimStylesImpl::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pimpl == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<IAcadDimStyles*>(m_pimpl.GetInterfacePtr());
+}
+
+//------------------------------------------------------------------------------------
+//PyIAcadLayerImpl
+PyIAcadLayerImpl::PyIAcadLayerImpl(IAcadLayer* ptr)
+    : PyIAcadObjectImpl(ptr)
+{
+}
+
+IAcadLayer* PyIAcadLayerImpl::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pimpl == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<IAcadLayer*>(m_pimpl.GetInterfacePtr());
+}
+
+//------------------------------------------------------------------------------------
+//PyIAcadLayersImpl
+PyIAcadLayersImpl::PyIAcadLayersImpl(IAcadLayers* ptr)
+    : PyIAcadObjectImpl(ptr)
+{
+}
+
+IAcadLayers* PyIAcadLayersImpl::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pimpl == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return static_cast<IAcadLayers*>(m_pimpl.GetInterfacePtr());
+}
+
 #endif
-
-
