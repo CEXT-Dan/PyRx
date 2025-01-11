21e4cd75ecb5aad21ced0d027cfafff3e234a587
Daniel <dwmcabinets@yahoo.com>
2025-01-11 13:05:36 +0800

    revert Ax

--- PyRxCore/PyAcAx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef PYRX_IN_PROGRESS_PYAX
-
-#pragma pack (push, 8)
-void initPyAxModule();
-#pragma pack (pop)
-
-#endif
--- PyRxCore/PyAcadDatabase.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef PYRX_IN_PROGRESS_PYAX
-
-class PyAcadDatabase
-{
-};
-
-#endif
