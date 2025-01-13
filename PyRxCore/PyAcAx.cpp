4d72b17ad8e5c4361acc95d0adaf4128ab67d651
Daniel <dwmcabinets@yahoo.com>
2025-01-13 16:10:50 +0800

    Ax enums AcWindowState AcColorMethod AcColor AcZoomScaleType

--- PyRxCore/PyAcAx.cpp
+++ PyRxCore/PyAcAx.cpp
@@ -15,12 +15,51 @@ BOOST_PYTHON_MODULE(PyAx)
     makePyAcadDocumentsWrapper();
     makePyAcadApplicationWrapper();
 
+    enum_<PyAcSectionGeneration>("AcSectionGeneration")
+        .value("acSectionGenerationSourceAllObjects", PyAcSectionGeneration::pyacSectionGenerationSourceAllObjects)
+        .value("acSectionGenerationSourceSelectedObjects", PyAcSectionGeneration::pyacSectionGenerationSourceSelectedObjects)
+        .value("acSectionGenerationDestinationNewBlock", PyAcSectionGeneration::pyacSectionGenerationDestinationNewBlock)
+        .value("acSectionGenerationDestinationReplaceBlock", PyAcSectionGeneration::pyacSectionGenerationDestinationReplaceBlock)
+        .value("acSectionGenerationDestinationFile", PyAcSectionGeneration::pyacSectionGenerationDestinationFile)
+        .export_values()
+        ;
 
-    //PyAcSectionGeneration
-    //PyAcWindowState
-    //PyAcColorMethod
-    //PyAcColor
-    //AcZoomScaleType
+    enum_<PyAcWindowState>("AcWindowState")
+        .value("acNorm", PyAcWindowState::pyacNorm)
+        .value("acMin", PyAcWindowState::pyacMin)
+        .value("acMax", PyAcWindowState::pyacMax)
+        .export_values()
+        ;
+
+    enum_<PyAcColorMethod>("AcColorMethod")
+        .value("acColorMethodByLayer", PyAcColorMethod::pyacColorMethodByLayer)
+        .value("acColorMethodByBlock", PyAcColorMethod::pyacColorMethodByBlock)
+        .value("acColorMethodByRGB", PyAcColorMethod::pyacColorMethodByRGB)
+        .value("acColorMethodByACI", PyAcColorMethod::pyacColorMethodByACI)
+        .value("acColorMethodForeground", PyAcColorMethod::pyacColorMethodForeground)
+        .export_values()
+        ;
+
+    enum_<PyAcColor>("AcColor")
+        .value("acByBlock", PyAcColor::pyacByBlock)
+        .value("acRed", PyAcColor::pyacRed)
+        .value("acYellow", PyAcColor::pyacYellow)
+        .value("acGreen", PyAcColor::pyacGreen)
+        .value("acCyan", PyAcColor::pyacCyan)
+        .value("acBlue", PyAcColor::pyacBlue)
+        .value("acMagenta", PyAcColor::pyacMagenta)
+        .value("acWhite", PyAcColor::pyacWhite)
+        .value("acByLayer", PyAcColor::pyacByLayer)
+        .export_values()
+        ;
+
+    enum_<PyAcZoomScaleType>("AcZoomScaleType")
+        .value("acZoomScaledAbsolute", PyAcZoomScaleType::pyacZoomScaledAbsolute)
+        .value("acZoomScaledRelative", PyAcZoomScaleType::pyacZoomScaledRelative)
+        .value("acZoomScaledRelativePSpace", PyAcZoomScaleType::pyacZoomScaledRelativePSpace)
+        .export_values()
+        ;
+
 }
 void initPyAxModule()
 {
