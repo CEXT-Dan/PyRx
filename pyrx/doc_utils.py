6aa09b4cae73078f846a1063dc23d5a02e527799
Grzegorz Świtek <grzegorz.switek@fort.pl>
2025-01-17 22:32:52 +0100

    [docstring] add get_overloads

--- pyrx/doc_utils.py
+++ pyrx/doc_utils.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import re
 
 _BASE_SIGNATURE_PATT = re.compile(r"!\[\((.*?)\)\]!")
+_OVERLOADS_PATT = re.compile(r"<\[\(Overloads:\n(.*)\)\]\>", re.IGNORECASE | re.DOTALL)
 
 
 def _get_docstring_section_from_pattern(docstring: str, pattern: re.Pattern) -> str | None:
@@ -14,3 +15,7 @@ def _get_docstring_section_from_pattern(docstring: str, pattern: re.Pattern) -> 
 
 def get_base_signature(docstring: str) -> str | None:
     return _get_docstring_section_from_pattern(docstring, _BASE_SIGNATURE_PATT)
+
+
+def get_overloads(docstring: str) -> str | None:
+    return _get_docstring_section_from_pattern(docstring, _OVERLOADS_PATT)
--- tests/test_doc_utils/test_parse_dostring.py
+++ tests/test_doc_utils/test_parse_dostring.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from pyrx.doc_utils import get_base_signature
+from pyrx.doc_utils import get_base_signature, get_overloads
 
 BASE_DIR = Path(__file__).parent
 RESOURCES = BASE_DIR / "resources"
@@ -42,6 +42,40 @@ def test_get_base_signature(docstring_file, expected, read_dostring):
     docstring = read_dostring(docstring_file)
     res = get_base_signature(docstring)
     assert res == expected
+
+
+@pytest.mark.parametrize(
+    "docstring_file, expected",
+    (
+        pytest.param(
+            "Db.AbstractViewTableRecord.setUcs.txt",
+            """
+    - origin: PyGe.Point3d, xAxis: PyGe.Vector3d, yAxis : PyGe.Vector3d
+    - view: PyDb.OrthographicView
+    - ucsId: PyDb.ObjectId
+""",
+            id="001",
+        ),
+        pytest.param(
+            "Db.Entity.intersectWith.txt",
+            """
+    - entity: PyDb.Entity, intType : PyDb.Intersect
+    - entity: PyDb.Entity, intType : PyDb.Intersect, thisGsMarker : int, otherGsMarker : int
+    - entity: PyDb.Entity, intType : PyDb.Intersect, plane : PyGe.Plane
+    - entity: PyDb.Entity, intType : PyDb.Intersect, plane : PyGe.Plane, thisGsMarker : int, otherGsMarker : int
+""",
+            id="002",
+        ),
+        pytest.param("Db.DbObject.wblockClone.txt", None, id="003"),
+    ),
+)
+def test_get_overloads(docstring_file, expected, read_dostring):
+    docstring = read_dostring(docstring_file)
+    res = get_overloads(docstring)
+    if res is None:
+        assert res is expected
+    else:  # isinstance(res, str)
+        assert res.strip() == expected.strip()
 
 
 if __name__ == "__main__":
