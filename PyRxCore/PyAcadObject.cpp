83fb0970f911a35b3b7b49f7a3b76dbba4029489
Daniel <dwmcabinets@yahoo.com>
2025-01-19 07:19:34 +0800

    class outlines

--- PyRxCore/PyAcadObject.cpp
+++ PyRxCore/PyAcadObject.cpp
@@ -1,2 +1,285 @@
 #include "stdafx.h"
-#include "PyAcadDbObject.h"
+#include "PyAcadObject.h"
+#include "PyAcadObjectImpl.h"
+
+using namespace boost::python;
+
+//----------------------------------------------------------------------------------------
+//PyAcadAcCmColor
+void makePyAcadAcCmColorWrapper()
+{
+    PyDocString DS("AcadAcCmColor");
+    class_<PyAcadAcCmColor>("AcadAcCmColor", boost::python::no_init)
+        .def("className", &PyAcadAcCmColor::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadAcCmColor::PyAcadAcCmColor(PyIAcadAcCmColorImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadAcCmColor::className()
+{
+    return "AcadAcCmColor";
+}
+
+PyIAcadAcCmColorImpl* PyAcadAcCmColor::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadHyperlink
+void makePyAcadHyperlinkWrapper()
+{
+    PyDocString DS("AcadHyperlink");
+    class_<PyAcadHyperlink>("AcadHyperlink", boost::python::no_init)
+        .def("className", &PyAcadHyperlink::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadHyperlink::PyAcadHyperlink(PyIAcadHyperlinkImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadHyperlink::className()
+{
+    return "AcadHyperlink";
+}
+
+PyIAcadHyperlinkImpl* PyAcadHyperlink::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadHyperlinks
+void makePyAcadHyperlinksWrapper()
+{
+    PyDocString DS("AcadHyperlinks");
+    class_<PyAcadHyperlinks>("AcadHyperlinks", boost::python::no_init)
+        .def("className", &PyAcadHyperlinks::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadHyperlinks::PyAcadHyperlinks(PyIAcadHyperlinksImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadHyperlinks::className()
+{
+    return "AcadHyperlinks";
+}
+
+PyIAcadHyperlinksImpl* PyAcadHyperlinks::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadSectionTypeSettings
+void makePyAcadSectionTypeSettingsWrapper()
+{
+    PyDocString DS("AcadSectionTypeSettings");
+    class_<PyAcadSectionTypeSettings>("AcadSectionTypeSettings", boost::python::no_init)
+        .def("className", &PyAcadSectionTypeSettings::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadSectionTypeSettings::PyAcadSectionTypeSettings(PyIAcadSectionTypeSettingsImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadSectionTypeSettings::className()
+{
+    return "AcadHyperlinks";
+}
+
+PyIAcadSectionTypeSettingsImpl* PyAcadSectionTypeSettings::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadDatabasePreferences
+void makePyAcadDatabasePreferencesWrapper()
+{
+    PyDocString DS("AcadDatabasePreferences");
+    class_<PyAcadDatabasePreferences>("AcadDatabasePreferences", boost::python::no_init)
+        .def("className", &PyAcadDatabasePreferences::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadDatabasePreferences::PyAcadDatabasePreferences(PyIAcadDatabasePreferencesImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadDatabasePreferences::className()
+{
+    return "AcadDatabasePreferences";
+}
+
+PyIAcadDatabasePreferencesImpl* PyAcadDatabasePreferences::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadPreferencesFiles
+void makePyAcadPreferencesFilesWrapper()
+{
+    PyDocString DS("AcadPreferencesFiles");
+    class_<PyAcadPreferencesFiles>("AcadPreferencesFiles", boost::python::no_init)
+        .def("className", &PyAcadPreferencesFiles::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadPreferencesFiles::PyAcadPreferencesFiles(PyIAcadPreferencesFilesImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadPreferencesFiles::className()
+{
+    return "AcadPreferencesFiles";
+}
+
+PyIAcadPreferencesFilesImpl* PyAcadPreferencesFiles::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadPreferencesDisplay
+void makePyAcadPreferencesDisplayWrapper()
+{
+    PyDocString DS("AcadPreferencesDisplay");
+    class_<PyAcadPreferencesDisplay>("AcadPreferencesDisplay", boost::python::no_init)
+        .def("className", &PyAcadPreferencesDisplay::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadPreferencesDisplay::PyAcadPreferencesDisplay(PyIAcadPreferencesDisplayImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadPreferencesDisplay::className()
+{
+    return "AcadPreferencesDisplay";
+}
+
+PyIAcadPreferencesDisplayImpl* PyAcadPreferencesDisplay::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadPreferencesOpenSave
+void makePyAcadPreferencesOpenSaveWrapper()
+{
+    PyDocString DS("AcadPreferencesOpenSave");
+    class_<PyAcadPreferencesOpenSave>("AcadPreferencesOpenSave", boost::python::no_init)
+        .def("className", &PyAcadPreferencesOpenSave::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadPreferencesOpenSave::PyAcadPreferencesOpenSave(PyIAcadPreferencesOpenSaveImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadPreferencesOpenSave::className()
+{
+    return "AcadPreferencesOpenSave";
+}
+
+PyIAcadPreferencesOpenSaveImpl* PyAcadPreferencesOpenSave::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadPreferencesOutput
+void makePyAcadPreferencesOutputWrapper()
+{
+    PyDocString DS("AcadPreferencesOutput");
+    class_<PyAcadPreferencesOutput>("AcadPreferencesOutput", boost::python::no_init)
+        .def("className", &PyAcadPreferencesOutput::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadPreferencesOutput::PyAcadPreferencesOutput(PyIAcadPreferencesOutputImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadPreferencesOutput::className()
+{
+    return "AcadPreferencesOutput";
+}
+
+PyIAcadPreferencesOutputImpl* PyAcadPreferencesOutput::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
+
+//----------------------------------------------------------------------------------------
+//PyAcadPreferencesSystem
+void makePyAcadPreferencesSystemWrapper()
+{
+    PyDocString DS("AcadPreferencesSystem");
+    class_<PyAcadPreferencesSystem>("AcadPreferencesSystem", boost::python::no_init)
+        .def("className", &PyAcadPreferencesSystem::className, DS.SARGS()).staticmethod("className")
+        ;
+}
+
+PyAcadPreferencesSystem::PyAcadPreferencesSystem(PyIAcadPreferencesSystemImpl* ptr)
+    : m_pyImp(ptr)
+{
+}
+
+std::string PyAcadPreferencesSystem::className()
+{
+    return "AcadPreferencesSystem";
+}
+
+PyIAcadPreferencesSystemImpl* PyAcadPreferencesSystem::impObj(const std::source_location& src /*= std::source_location::current()*/) const
+{
+    if (m_pyImp == nullptr) [[unlikely]] {
+        throw PyNullObject(src);
+    }
+    return m_pyImp.get();
+}
