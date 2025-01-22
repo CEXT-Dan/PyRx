b40180027b8a3723ed05d9a1a14d61badf4de4e3
Daniel <dwmcabinets@yahoo.com>
2025-01-22 12:24:13 +0800

    improve test

--- tests/test_core/test_PyAx/test_AxPreferences.py
+++ tests/test_core/test_PyAx/test_AxPreferences.py
@@ -19,37 +19,41 @@ class TestAxPreferences:
 
     def test_axSelection(self):
         axSelection = self.axPreferences.selection()
-        assert axSelection.displayGrips() == True
-        
+
         # grips
         flag = axSelection.displayGrips()
         axSelection.setDisplayGrips(not flag)
         assert axSelection.displayGrips() != flag
         axSelection.setDisplayGrips(flag)
+        assert axSelection.displayGrips() == flag
         
         # grips in blocks
         flag = axSelection.displayGripsWithinBlocks()
         axSelection.setDisplayGripsWithinBlocks(not flag)
         assert axSelection.displayGripsWithinBlocks() != flag
         axSelection.setDisplayGripsWithinBlocks(flag)
+        assert axSelection.displayGripsWithinBlocks() == flag
         
         # grip size
         flag = axSelection.gripSize()
         axSelection.setGripSize(flag + 1)
         assert axSelection.gripSize() != flag
         axSelection.setGripSize(flag)
+        assert axSelection.gripSize() == flag
         
         # grip clr
         flag = axSelection.gripColorSelected()
         axSelection.setGripColorSelected(Ax.AcColor.acYellow)
         assert axSelection.gripColorSelected() != flag
         axSelection.setGripColorSelected(flag)
+        assert axSelection.gripColorSelected() == flag
         
         # grip clr
         flag = axSelection.gripColorUnselected()
         axSelection.setGripColorUnselected(Ax.AcColor.acYellow)
         assert axSelection.gripColorUnselected() != flag
         axSelection.setGripColorUnselected(flag)
+        assert axSelection.gripColorUnselected() == flag
         
         
 
