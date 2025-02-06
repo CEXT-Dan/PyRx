5cc3793da0b16d433d6a7a49d68b4d7f856d06dc
Daniel <dwmcabinets@yahoo.com>
2025-02-06 08:14:07 +0800

    add stubs for BRX

--- gen_pyi.py
+++ gen_pyi.py
@@ -6,15 +6,41 @@ from pyrx import Ap, Ax, Br, Db, Ed, Ge, Gi, Gs, Pl, Rx, Sm
 from pyrx.doc_utils.misc import DocstringsManager, ReturnTypesManager
 from pyrx.doc_utils.pyi_gen import gen_pyi
 
+if "BRX" in Ap.Application.hostAPI():
+    from pyrx import Cv
+    from pyrx import Bim
+    from pyrx import Brx
 
-def PyRxCmd_gen_pyi():
+
+def PyRxCmd_gen_pyi_brx():
     try:
-        main()
+        runBRX()
     except Exception:
         traceback.print_exc()
 
 
-def main():
+def PyRxCmd_gen_pyi():
+    try:
+        runARX()
+    except Exception:
+        traceback.print_exc()
+
+
+def runBRX():
+    logging.basicConfig(filename="gen_pyi_brx.log", filemode="w", force=True)
+    PYI_DIR = Path(__file__).parent / "pyrx"
+    for module in (Ap, Br, Db, Ed, Ge, Gi, Gs, Pl, Rx, Sm, Ax, Cv, Bim, Brx):
+        res = gen_pyi(
+            module=module,
+            all_modules=(Ap, Br, Db, Ed, Ge, Gi, Gs, Pl, Rx, Sm, Ax, Cv, Bim, Brx),
+            docstrings=DocstringsManager.from_json(),
+            return_types=ReturnTypesManager.from_json(),
+        ).gen()
+        with open(PYI_DIR / f"{module.__name__}.pyi", "w", encoding="utf-8") as f:
+            f.write(res)
+
+
+def runARX():
     logging.basicConfig(filename="gen_pyi.log", filemode="w", force=True)
     PYI_DIR = Path(__file__).parent / "pyrx"
     for module in (Ap, Ax, Br, Db, Ed, Ge, Gi, Gs, Pl, Rx, Sm):
