55014e6ed36680c5650e00206890da94f5caad48
Daniel <dwmcabinets@yahoo.com>
2025-02-10 20:39:04 +0800

    TestAxEntity.test_intersectWith

--- tests/test_core/test_PyAx/test_AxEntity.py
+++ tests/test_core/test_PyAx/test_AxEntity.py
@@ -3,6 +3,7 @@ import pytest
 from tests import MEDIA_DIR
 from pyrx import Ap, Ge, Ax
 
+
 class TestAxEntity:
 
     def setup_class(self):
@@ -12,11 +13,25 @@ class TestAxEntity:
     def test_trueColore(self):
         axSpace = self.axDoc.modelSpace()
         axColor = Ax.AcadAcCmColor()
-        axColor.setRGB(100,100,100)
-        ent = axSpace.addLine(Ge.Point3d(0, 0, 0),Ge.Point3d(100, 0, 0))
+        axColor.setRGB(100, 100, 100)
+        ent = axSpace.addLine(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 0, 0))
         ent.setTrueColor(axColor)
         axColor2 = ent.trueColor()
         assert axColor.red() == axColor2.red()
         assert axColor.green() == axColor2.green()
         assert axColor.blue() == axColor2.blue()
-     
+
+    def test_intersectWith(self):
+        axSpace = self.axDoc.modelSpace()
+        axLine1 = axSpace.addLine(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 100, 0))
+        axLine2 = axSpace.addLine(Ge.Point3d(100, 0, 0), Ge.Point3d(0, 100, 0))
+        axCircle = axSpace.addCircle(Ge.Point3d(50, 50, 0), 50)
+        inters1 = axLine1.intersectWith(axLine2, Ax.AcExtendOption.acExtendNone)
+        inters2 = axLine1.intersectWith(axCircle, Ax.AcExtendOption.acExtendNone)
+        inters3 = axLine2.intersectWith(axCircle, Ax.AcExtendOption.acExtendNone)
+        assert len(inters1) == 1
+        assert inters1[0] == Ge.Point3d(50, 50, 0)
+        assert len(inters2) == 2
+        assert len(inters3) == 2
+        assert inters2[0].distanceTo(inters2[1]) == 100.0
+        assert inters3[0].distanceTo(inters3[1]) == 100.0
