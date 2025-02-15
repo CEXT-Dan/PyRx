2a6bcab53d3bb071cd8ef020afbd3594c724a652
Daniel <dwmcabinets@yahoo.com>
2025-02-15 10:59:28 +0800

    progress on PyIAcadUtilityImpl

--- tests/test_core/test_PyAx/test_AxDocument.py
+++ tests/test_core/test_PyAx/test_AxDocument.py
@@ -9,4 +9,12 @@ class TestAxDocument:
         axDoc = axApp.activeDocument()
         assert len(axDoc.name()) != 0
         assert axDoc.className() == 'AcadDocument'
+        
+    def test_getVariable(self):
+        axApp = Ap.Application.acadApplication()
+        axDoc = axApp.activeDocument()
+        axDoc.setVariable("osmode", 191)
+        assert axDoc.getVariable("osmode") == 191
+        axDoc.setVariable("osmode", 0)
+        assert axDoc.getVariable("osmode") == 0
 
