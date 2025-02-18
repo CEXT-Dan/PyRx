1b5f2b1c7094028ce510d56186b88bb7c410e58c
Daniel <dwmcabinets@yahoo.com>
2025-02-18 16:40:19 +0800

    improve TestAxSelectionSet

--- tests/test_core/test_PyAx/test_AxSelectionSet.py
+++ tests/test_core/test_PyAx/test_AxSelectionSet.py
@@ -15,29 +15,29 @@ class TestAxSelectionSet:
         for idx1 in range(1,49):
             self.axSpace.addLine(Ge.Point3d(0,idx1,0),Ge.Point3d(0,idx1,0))
             
+    def test_sscount(self):
+        axSets = self.axDoc.selectionSets()
+        sscount = axSets.count()# brx has in not zero?
+        axSet = axSets.add("PYRX1")
+        assert axSets.count() == sscount + 1
+        axSet.delete()
+        assert axSets.count() == sscount
+        
     def test_selectAllFilter(self):
         axSets = self.axDoc.selectionSets()
-        axSet = axSets.add("PYRX")
-        assert axSets.count() == 1
-        
+        axSet = axSets.add("PYRX2")
         axSet.selectAll([(0, "POINT")])
         count = axSet.count()
-        
         cnt1 = 0
         for ent in axSet.entities():
             cnt1 += 1
-
         cnt2 = 0
         for ent in axSet:
             cnt2 += 1
-           
         ent : Ax.AcadEntity 
         for ent in axSet:
             assert ent.objectName() == "AcDbPoint"
- 
         axSet.delete()
-        
         assert count >= 2304
         assert cnt1 >= 2304
         assert cnt2 >= 2304
-        assert axSets.count() == 0
