798ba872e506c57129147a21e4d875ce607a8d83
Daniel <dwmcabinets@yahoo.com>
2025-02-24 10:25:36 +0800

    TestAx3DSolid

---
+++ tests/test_core/test_PyAx/test_Ax3DSolid.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from pyrx import Ap, Ge, Ax
+
+
+class TestAx3DSolid:
+
+    def setup_class(self):
+        self.axApp = Ap.Application.acadApplication()
+        self.axDoc = self.axApp.activeDocument()
+
+    def test_checkInterference(self):
+        axSpace = self.axDoc.modelSpace()
+        ent1 = axSpace.addBox(Ge.Point3d(0, 0, 0), 100, 200, 300)
+        ent2 = axSpace.addBox(Ge.Point3d(10, 10, 10), 100, 200, 300)
+        assert ent1.objectName() == "AcDb3dSolid"
+        assert ent2.objectName() == "AcDb3dSolid"
+        inter, ent3 = ent1.checkInterference(ent2,True)
+        assert ent3.objectName() == "AcDb3dSolid"
+        assert inter == True
