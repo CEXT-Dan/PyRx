d846a9711627c0ae451b50fa2dae54df09a4e34d
Daniel <dwmcabinets@yahoo.com>
2025-03-09 14:00:47 +0800

    [#204] add workaround and test

---
+++ tests/test_core/test_PyEd/test_Core_cmdS.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from pyrx import Ap, Ed, Rx, command
+
+@command()
+def cmdtotest1():
+    Ed.Core.setVar("USERS1", "SUCCESS")
+
+
+@Ap.Command()
+def cmdtotest2():
+    Ed.Core.setVar("USERS1", "SUCCESS")
+
+
+def PyRxCmd_cmdtotest3():
+    Ed.Core.setVar("USERS1", "SUCCESS")
+
+
+class TestAcedCmdS:
+
+    def setup_class(self):
+        Ap.Application.loadPythonModule(str(__file__))
+
+    def test_cmdtotest1(self):
+        Ed.Core.setVar("USERS1", "FAIL")
+        resbuf = [
+            (Rx.LispType.kText, "cmdtotest1"),
+            (Rx.LispType.kNone, 0),
+        ]
+        assert Ed.Core.cmdS(resbuf) == True
+        Ed.Core.getVar("USERS1") == "SUCCESS"
+
+    def test_cmdtotest2(self):
+        Ed.Core.setVar("USERS1", "FAIL")
+        resbuf = [
+            (Rx.LispType.kText, "cmdtotest2"),
+            (Rx.LispType.kNone, 0),
+        ]
+        assert Ed.Core.cmdS(resbuf) == True
+        Ed.Core.getVar("USERS1") == "SUCCESS"
+
+    def test_cmdtotest3(self):
+        Ed.Core.setVar("USERS1", "FAIL")
+        resbuf = [
+            (Rx.LispType.kText, "cmdtotest3"),
+            (Rx.LispType.kNone, 0),
+        ]
+        assert Ed.Core.cmdS(resbuf) == True
+        Ed.Core.getVar("USERS1") == "SUCCESS"
