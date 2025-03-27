08a2ce887cfefe20b3213ba0707a898dbfa315e1
Daniel <dwmcabinets@yahoo.com>
2025-03-28 07:16:18 +0800

    add mleader sample

---
+++ PySamples/PyDb/mleaderAtt.py
@@ -0,0 +1,17 @@
+from pyrx import Rx, Ge, Gi, Db, Ap, Ed, Ax, command
+
+
+@command
+def doit():
+    ps, id, _ = Ed.Editor.entSel("\nSelect: \n")
+
+    ml = Db.MLeader(id, Db.OpenMode.kForWrite)
+    mlBlock = Db.BlockTableRecord(ml.blockContentId())
+    mldefs = mlBlock.objectIds(Db.AttributeDefinition.desc())
+
+    for id in mldefs:
+        mlAttRef = ml.getBlockAttribute(id)  # renamed from getblockattributevalue
+        print(mlAttRef.isA().name(), mlAttRef.textString())
+
+        mlAttRef.setTextString("oof")
+        ml.setBlockAttribute(id, mlAttRef)
