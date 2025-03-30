328aa5c27882773a02697bdcd7e4555b0d1a7712
Daniel <dwmcabinets@yahoo.com>
2025-03-31 06:57:16 +0800

    added sample

---
+++ PySamples/PyAx/axLine.py
@@ -0,0 +1,25 @@
+from pyrx import Rx, Ge, Gi, Db, Ap, Ed, Ax, command
+
+print("added command - axLine")
+
+
+@command
+def axLine():
+
+    # get the application, document, and modelspace
+    axApp = Ap.Application.acadApplication()
+    axDoc = axApp.activeDocument()
+    axModel = axDoc.modelSpace()
+
+    # add a lines to modelspace, then access it properties
+    # use Ge.Point3d class instead of a variant
+    axLine1 = axModel.addLine(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 100, 0))
+    axLine2 = axModel.addLine(Ge.Point3d(0, 100, 0), Ge.Point3d(100, 0, 0))
+
+    interdata = axLine1.intersectWith(axLine2, Ax.AcExtendOption.acExtendNone)
+    if len(interdata) == 0:
+        print("\nDoes not interset:")
+        return
+
+    axCircle = axModel.addCircle(interdata[0], 10)
+    axCircle.setColor(Ax.AcColor.acCyan)
