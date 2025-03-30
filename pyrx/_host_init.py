693378a636aa42b151dc224f1fe04870cbc4194e
Grzegorz Świtek <grzegorz.switek@fort.pl>
2025-03-30 21:21:17 +0200

    load REPLs on demand

--- pyrx/_host_init.py
+++ pyrx/_host_init.py
@@ -1,37 +1,14 @@
 from __future__ import annotations
 
-import threading
+import os
 
 from pyrx import command, reload
 from pyrx.PyRxDebug import startListener
-from pyrx.repl.stdlibrepl import run_stdlib_repl
 
 reload("pyrx")
 
 
 command(startListener, name="PYDEBUG")
 
-
-@command
-def pystdrepl():
-    thread = threading.Thread(target=run_stdlib_repl)
-    thread.start()
-
-
-try:
-    from pyrx.repl.ptrepl import run_ptpython_repl
-except ImportError:
-    import importlib.util
-
-    if importlib.util.find_spec("ptpython") is not None:
-        raise
-
-    @command
-    def pyptrepl():
-        print("\nptpython is not installed. Please run `pip install ptpython`.\n")
-else:
-
-    @command
-    def pyptrepl():
-        thread = threading.Thread(target=run_ptpython_repl)
-        thread.start()
+if os.getenv("PYRX_LOAD_REPL", None) == "1":
+    import pyrx.repl.repl_cmds  # noqa
---
+++ pyrx/repl/repl_cmds.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import threading
+
+from pyrx import command, reload
+from pyrx.repl.stdlibrepl import run_stdlib_repl
+
+reload("pyrx")
+
+
+@command
+def pystdrepl():
+    thread = threading.Thread(target=run_stdlib_repl)
+    thread.start()
+
+
+@command
+def pyptrepl():
+    try:
+        from pyrx.repl.ptrepl import run_ptpython_repl
+    except ImportError:
+        import importlib.util
+
+        if importlib.util.find_spec("ptpython") is not None:
+            raise
+        print("\nptpython is not installed. Please run `pip install ptpython`.\n")
+    else:
+        thread = threading.Thread(target=run_ptpython_repl)
+        thread.start()
