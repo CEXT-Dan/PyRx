589f48d1c09afc814a10e80bf91cc8ed177c445e
Daniel <dwmcabinets@yahoo.com>
2025-04-01 16:15:33 +0800

    add TestSortentsTable

---
+++ tests/test_core/test_PyDb/test_SortentsTable.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+import pytest
+from pyrx import Ap, Ax, Db, Ge
+
+
+class TestSortentsTable:
+    @pytest.mark.known_failure_BRX
+    def test_getinstance(self):
+        db = Db.curDb()
+        axApp = Ap.Application.acadApplication()
+        axDoc = axApp.activeDocument()
+        axModel = axDoc.modelSpace()
+        axEnt1 = axModel.addLine(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 0, 0))
+        axEnt2 = axModel.addLine(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 0, 0))
+        id1 = axEnt1.objectId()
+        id2 = axEnt2.objectId()
+
+        model = Db.BlockTableRecord(db.modelSpaceId())
+        sortEnts = model.getSortentsTable(Db.OpenMode.kForWrite, True)
+
+        sortEnts.moveToTop([id1])
+        sortEnts.moveToBottom([id2])
+        assert sortEnts.firstEntityIsDrawnBeforeSecond(id1, id2) == False
+
+        sortEnts.swapOrder(id1, id2)
+        assert sortEnts.firstEntityIsDrawnBeforeSecond(id1, id2) == True
+
+        sortEnts.moveToTop([id1])
+        sortEnts.moveToBottom([id2])
+        assert sortEnts.firstEntityIsDrawnBeforeSecond(id1, id2) == False
+
+        sortEnts.moveToTop([id2])
+        sortEnts.moveToBottom([id1])
+        assert sortEnts.firstEntityIsDrawnBeforeSecond(id1, id2) == True
