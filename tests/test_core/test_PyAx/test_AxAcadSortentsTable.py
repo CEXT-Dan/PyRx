9009b55162bc179886da81229c326d0881076a8e
Daniel <dwmcabinets@yahoo.com>
2025-04-01 13:11:12 +0800

    added test test_AxAcadSortentsTable.py

---
+++ tests/test_core/test_PyAx/test_AxAcadSortentsTable.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+from pyrx import Ap, Ax, Db, Ge
+
+
+def is_before(objs: list[Ax.AcadEntity], left: Ax.AcadEntity, right: Ax.AcadEntity):
+    for obj in objs:
+        if obj == right:
+            return False
+        elif obj == left:
+            return True
+
+
+class TestAcadSortentsTable:
+    def test_getinstance(self):
+        axApp = Ap.Application.acadApplication()
+        axDoc = axApp.activeDocument()
+        axModel = axDoc.modelSpace()
+
+        axEnt1 = axModel.addLine(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 0, 0))
+        axEnt2 = axModel.addLine(Ge.Point3d(0, 0, 0), Ge.Point3d(100, 0, 0))
+
+        ex = axModel.extensionDictionary()
+        if not "AcDbSortentsTable" in [i.objectName() for i in ex.items()]:
+            ex.addObject("ACAD_SORTENTS", "AcDbSortentsTable")
+
+        axSortEnts = Ax.AcadSortentsTable.cast(ex.object("ACAD_SORTENTS"))
+
+        idmap = {}
+        idmap[axEnt1] = "first"
+        idmap[axEnt2] = "second"
+        axSortEnts.moveToTop([axEnt1])
+        axSortEnts.moveToBottom([axEnt2])
+
+        do = axSortEnts.fullDrawOrder(True)
+        assert is_before(do,axEnt1,axEnt2) == False
+
+        axSortEnts.swapOrder(axEnt1, axEnt2)
+        do = axSortEnts.fullDrawOrder(True)
+        assert is_before(do,axEnt1,axEnt2) == True
+
+        axSortEnts.moveToTop([axEnt1])
+        axSortEnts.moveToBottom([axEnt2])
+        do = axSortEnts.fullDrawOrder(True)
+        assert is_before(do,axEnt1,axEnt2) == False
+
+        axSortEnts.moveToTop([axEnt2])
+        axSortEnts.moveToBottom([axEnt1])
+        do = axSortEnts.fullDrawOrder(True)
+        assert is_before(do,axEnt1,axEnt2) == True
