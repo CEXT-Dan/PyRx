1edface84a391f4299c695d5038b102ac7702af9
rdesparbes <desparbes.romain@hotmail.fr>
2025-04-13 23:30:41 +0200

    Added type ignore for _BoostPythonEnumMeta

--- pyrx/doc_utils/boost_meta.py
+++ pyrx/doc_utils/boost_meta.py
@@ -6,7 +6,7 @@ T = TypeVar("T")
 class _BoostPythonEnumMeta(type):
     # This is not a real class, it is just for better type hints
 
-    def __call__(cls: type[T], value: int) -> T: ...
+    def __call__(cls: type[T], value: int) -> T: ...  # type: ignore[empty-body]
 
 
 class _BoostPythonEnum(int, metaclass=_BoostPythonEnumMeta):
