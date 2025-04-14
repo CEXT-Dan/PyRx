4611126eabbc629d1fc839a9702aaef3318fbe4d
Daniel <dwmcabinets@yahoo.com>
2025-04-14 16:28:21 +0800

    [#266] migrate Ge tests

---
+++ tests/test_core/test_PyGe/test_Curve2d.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+from pyrx import Ge, Db
+import pytest
+import unittest
+
+
+class TestCurve2d:
+    def test_Ge_line2d_static_properties(self):
+        xline = Ge.Line2d.kXAxis
+        yline = Ge.Line2d.kYAxis
+        assert xline.isPerpendicularTo(yline) == True
+
+    def test_ge2dcurve_length(self):
+        seg = Ge.LineSeg2d(Ge.Point2d(0, 0), Ge.Point2d(100, 0))
+        length = seg.length(0, 1)
+        assert length == 100
+
+    @pytest.mark.known_failure_BRX
+    @pytest.mark.known_failure_ZRX
+    def test_CurveCurveInt2d_inter(self, db_06457: Db.Database):
+        objHnd1 = Db.Handle("2c959f")
+        objId1 = db_06457.getObjectId(False, objHnd1)
+        assert objId1.isValid() == True
+        objHnd2 = Db.Handle("2c95a0")
+        objId2 = db_06457.getObjectId(False, objHnd2)
+        assert objId2.isValid() == True
+        dbcurve1 = Db.Polyline(objId1)
+        dbcurve2 = Db.Polyline(objId2)
+        gecurve1 = dbcurve1.getAcGeCurve2d()
+        gecurve2 = dbcurve2.getAcGeCurve2d()
+        inter = Ge.CurveCurveInt2d(gecurve1, gecurve2)
+        assert inter.numIntPoints() == 1
+        tp = Ge.Point2d(-38169.03679118369473, -8356.39529306029726)
+        assert inter.intPoint(0) == tp
+
+    @pytest.mark.known_failure_BRX
+    @pytest.mark.known_failure_ZRX
+    def test_CurveCurveInt2d_overlap(self, db_06457: Db.Database):
+        objHnd1 = Db.Handle("2c94f5")
+        objId1 = db_06457.getObjectId(False, objHnd1)
+        assert objId1.isValid() == True
+        objHnd2 = Db.Handle("2c94f6")
+        objId2 = db_06457.getObjectId(False, objHnd2)
+        assert objId2.isValid() == True
+        dbcurve1 = Db.Polyline(objId1)
+        dbcurve2 = Db.Polyline(objId2)
+        gecurve1 = dbcurve1.getAcGeCurve2d()
+        gecurve2 = dbcurve2.getAcGeCurve2d()
+        inter = Ge.CurveCurveInt2d(gecurve1, gecurve2)
+        assert inter.overlapCount() == 1
+        ranges = inter.getOverlapRanges(0)
+        r1: Ge.Interval = ranges[0]
+        r2: Ge.Interval = ranges[1]
+        assertions = unittest.TestCase("__init__")
+        assertions.assertAlmostEqual(r1.length(), 1421.1194776994016, 4)
+        assertions.assertAlmostEqual(r2.length(), 1421.1194776994016, 4)
--- tests/test_core/test_PyGe/test_Vector3d.py
+++ tests/test_core/test_PyGe/test_Vector3d.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
-
 from pyrx import Ge
+import math
+
 
 class TestVector3d:
     def test_repr(self):
-        val = Ge.Vector3d(1.2,2.3,4.5)
-        assert val.__repr__()  == "PyGe.Vector3d(1.20000000000000,2.30000000000000,4.50000000000000)"
-        
-        
+        val = Ge.Vector3d(1.2, 2.3, 4.5)
+        assert (
+            val.__repr__() == "PyGe.Vector3d(1.20000000000000,2.30000000000000,4.50000000000000)"
+        )
+
+    def test_vector3d_rmul1(self):
+        v = Ge.Vector3d(1, 1, 1)
+        v = 10.0 * v
+        assert v == Ge.Vector3d(10, 10, 10)
+
+    def test_vector3d_rmul2(self):
+        v = Ge.Vector3d(1, 1, 1)
+        m = Ge.Matrix3d()
+        m.setToRotation(math.pi, Ge.Vector3d.kZAxis, Ge.Point3d.kOrigin)
+        v = m * v
+        assert v == Ge.Vector3d(-1.0, -1.0, 1.0)
