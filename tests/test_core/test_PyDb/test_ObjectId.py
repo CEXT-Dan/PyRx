2deb0d1250e7890141177b5783ecbab8731fc773
Daniel <dwmcabinets@yahoo.com>
2025-04-15 12:28:32 +0800

    [#266] migrate test_ObjectId

---
+++ tests/test_core/test_PyDb/test_ObjectId.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from pyrx import Db
+
+class TestDbObjectId:
+    def test_property_ids(self):
+        id = Db.HostApplicationServices().workingDatabase().blockTableId()
+        assert id.isNull() == False
+        assert not id.asOldId() == 0
+        assert id.isResident() == True
+        assert id.isValid() == True
+        assert id.database().className() == "AcDbDatabase"
+        assert id.database() !=  None
+        assert id.objectClass().name() == "AcDbBlockTable"
+        assert id.isDerivedFrom(Db.BlockTable.desc())
+        assert id.acadObject() != None
+                
+    def test_handle(self):
+        db = Db.HostApplicationServices().workingDatabase()
+        id = db.blockTableId()
+        bt = Db.BlockTable(id, Db.OpenMode.ForRead)
+        hn = bt.getHandle()
+        assert hn.isNull() == False
+        id2 = db.getObjectId(False,hn)
+        assert id == id2
+        assert id.isValid() == True
+        assert id2.isValid() == True
+        bt.close()
+                
