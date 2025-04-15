a7237c696f39d164237ec8b466905319f67da54b
Daniel <dwmcabinets@yahoo.com>
2025-04-15 17:44:01 +0800

    [#266] migrate tests

---
+++ tests/test_core/test_PyGs/test_GsCore.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from pyrx import Db, Gs
+import pytest
+import wx
+
+
+class TestGsCore:
+    @pytest.mark.known_failure_GRX
+    def test_gs_getBlockImage(self, db_dynblock: Db.Database):
+        objHnd = Db.Handle("36f")
+        objId = db_dynblock.getObjectId(False, objHnd)
+        img: wx.Image = Gs.Core.getBlockImage(objId, 32, 32, 1.0, [0, 0, 0])
+        assert img.IsOk() == True
+        assert img.GetHeight() == 32
+        assert img.GetWidth() == 32
