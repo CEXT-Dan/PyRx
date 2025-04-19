e5ccb37c2bcf7e77080292bb8fe3f8dedcacde14
Daniel <dwmcabinets@yahoo.com>
2025-04-19 09:25:45 +0800

    update template

--- tests/test_core/test_PyDb/test_BlockTableRecord.py
+++ tests/test_core/test_PyDb/test_BlockTableRecord.py
@@ -6,13 +6,13 @@ import pytest
 
 class TestBlockTableRecord:
     def setup_class(self):
-        self.brxdb = Db.Database(False,True)
+        self.brxdb = Db.Database(False, True)
         self.brxdb.readDwgFile(str(MEDIA_DIR / "parametric.dwg"))
         self.brxdb.closeInput(True)
-        
+
     def teardown_class(self):
         del self.brxdb
-    
+
     def test_effective_name(self, db_dynblock: Db.Database):
         bt = Db.BlockTable(db_dynblock.blockTableId())
         for id in bt.toDict().values():
@@ -20,7 +20,7 @@ class TestBlockTableRecord:
             if btr.isLayout():
                 continue
             assert not "*" in btr.effectiveName()
-            
+
     @pytest.mark.known_failure_ARX
     @pytest.mark.known_failure_GRX
     @pytest.mark.known_failure_ZRX
@@ -32,5 +32,3 @@ class TestBlockTableRecord:
             if btr.isLayout():
                 continue
             assert not "*" in btr.effectiveName()
-            
-        
