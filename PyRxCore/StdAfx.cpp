b9d6ef3dce0d169d7d72cc367498a9219b1954a5
Daniel <dwmcabinets@yahoo.com>
2025-04-24 21:40:21 +0800

    add pyrx_version.h

--- PyRxCore/StdAfx.cpp
+++ PyRxCore/StdAfx.cpp
@@ -41,30 +41,9 @@ const TCHAR* getappname()
     return L"ARX";
 }
 
+#include "../pyrx_version.h"
+
 const AcString getPyRxBuldVersion()
 {
-    constexpr TCHAR MAJOR1 = '2';
-    constexpr TCHAR MINOR1 = '2';
-    constexpr TCHAR REVISION1 = '0', REVISION2 = '0', REVISION3 = '6';
-
-    constexpr unsigned int compileYear = (__DATE__[7] - '0') * 1000 + (__DATE__[8] - '0') * 100 + (__DATE__[9] - '0') * 10 + (__DATE__[10] - '0');
-    constexpr unsigned int compileMonth = (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? 1 : ((__DATE__[2] == 'n') ? 6 : 7))    // Jan, Jun or Jul
-        : (__DATE__[0] == 'F') ? 2                                                              // Feb
-        : (__DATE__[0] == 'M') ? ((__DATE__[2] == 'r') ? 3 : 5)                                 // Mar or May
-        : (__DATE__[0] == 'A') ? ((__DATE__[1] == 'p') ? 4 : 8)                                 // Apr or Aug
-        : (__DATE__[0] == 'S') ? 9                                                              // Sep
-        : (__DATE__[0] == 'O') ? 10                                                             // Oct
-        : (__DATE__[0] == 'N') ? 11                                                             // Nov
-        : (__DATE__[0] == 'D') ? 12                                                             // Dec
-        : 0;
-    constexpr unsigned int compileDay = (__DATE__[4] == ' ') ? (__DATE__[5] - '0') : (__DATE__[4] - '0') * 10 + (__DATE__[5] - '0');
-
-    constexpr TCHAR IsoDate[] =
-    {
-       MAJOR1, '.' , MINOR1 , '.', REVISION1, REVISION2, REVISION3,
-       '.', compileYear / 1000 + '0', (compileYear % 1000) / 100 + '0', (compileYear % 100) / 10 + '0', compileYear % 10 + '0',
-       compileMonth / 10 + '0', compileMonth % 10 + '0',
-       compileDay / 10 + '0', compileDay % 10 + '0', 0
-    };
-    return AcString(IsoDate);
+    return std::format(_T("{}.{}"), VERSION_STRING_WVER, IsoDate).c_str();
 }
---
+++ pyrx_version.h
@@ -0,0 +1,41 @@
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 2
+#define VERSION_BUILD 6
+#define VERSION_REVISION 0
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+//DONT CHANGE
+#define VERSION_STRINGIZE_I(x) #x
+#define VERSION_STRINGIZE(x) VERSION_STRINGIZE_I(x)
+
+#define VERSION_STRING \
+    VERSION_STRINGIZE(VERSION_MAJOR) "." \
+    VERSION_STRINGIZE(VERSION_MINOR) "." \
+    VERSION_STRINGIZE(VERSION_BUILD) "." \
+    VERSION_STRINGIZE(VERSION_REVISION)
+
+#define VERSION_STRING_WVER \
+    VERSION_STRINGIZE(VERSION_MAJOR) L"." \
+    VERSION_STRINGIZE(VERSION_MINOR) L"." \
+    VERSION_STRINGIZE(VERSION_BUILD)
+
+
+constexpr unsigned int compileYear = (__DATE__[7] - '0') * 1000 + (__DATE__[8] - '0') * 100 + (__DATE__[9] - '0') * 10 + (__DATE__[10] - '0');
+constexpr unsigned int compileMonth = (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? 1 : ((__DATE__[2] == 'n') ? 6 : 7))    // Jan, Jun or Jul
+    : (__DATE__[0] == 'F') ? 2                                                              // Feb
+    : (__DATE__[0] == 'M') ? ((__DATE__[2] == 'r') ? 3 : 5)                                 // Mar or May
+    : (__DATE__[0] == 'A') ? ((__DATE__[1] == 'p') ? 4 : 8)                                 // Apr or Aug
+    : (__DATE__[0] == 'S') ? 9                                                              // Sep
+    : (__DATE__[0] == 'O') ? 10                                                             // Oct
+    : (__DATE__[0] == 'N') ? 11                                                             // Nov
+    : (__DATE__[0] == 'D') ? 12                                                             // Dec
+    : 0;
+constexpr unsigned int compileDay = (__DATE__[4] == ' ') ? (__DATE__[5] - '0') : (__DATE__[4] - '0') * 10 + (__DATE__[5] - '0');
+
+constexpr TCHAR IsoDate[] =
+{
+   compileYear / 1000 + '0', (compileYear % 1000) / 100 + '0', (compileYear % 100) / 10 + '0', compileYear % 10 + '0',
+   compileMonth / 10 + '0', compileMonth % 10 + '0',
+   compileDay / 10 + '0', compileDay % 10 + '0', 0
+};
