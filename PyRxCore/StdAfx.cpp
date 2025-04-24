f0e0fed742ab2df3d4ed3be51608e8330f700458
Daniel <dwmcabinets@yahoo.com>
2025-04-24 16:27:14 +0800

    fix const build version

--- PyRxCore/StdAfx.cpp
+++ PyRxCore/StdAfx.cpp
@@ -51,7 +51,7 @@ const AcString getPyRxBuldVersion()
     constexpr unsigned int compileMonth = (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? 1 : ((__DATE__[2] == 'n') ? 6 : 7))    // Jan, Jun or Jul
         : (__DATE__[0] == 'F') ? 2                                                              // Feb
         : (__DATE__[0] == 'M') ? ((__DATE__[2] == 'r') ? 3 : 5)                                 // Mar or May
-        : (__DATE__[0] == 'A') ? ((__DATE__[2] == 'p') ? 4 : 8)                                 // Apr or Aug
+        : (__DATE__[0] == 'A') ? ((__DATE__[1] == 'p') ? 4 : 8)                                 // Apr or Aug
         : (__DATE__[0] == 'S') ? 9                                                              // Sep
         : (__DATE__[0] == 'O') ? 10                                                             // Oct
         : (__DATE__[0] == 'N') ? 11                                                             // Nov
