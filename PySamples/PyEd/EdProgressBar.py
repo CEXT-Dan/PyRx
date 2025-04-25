67d094b7252ee8f643aa5cbf246565c5c1c2c7ff
Daniel <dwmcabinets@yahoo.com>
2025-04-26 07:30:57 +0800

    improve sample

--- PySamples/PyEd/EdProgressBar.py
+++ PySamples/PyEd/EdProgressBar.py
@@ -1,11 +1,12 @@
 from pyrx import Rx, Ge, Gi, Db, Ap, Ed, Ax
 import traceback
-
+from time import sleep
 
 def PyRxCmd_doit():
     try:
         Ed.Core.setStatusBarProgressMeter("Calculating", 0, 100)
         for i in range(100):
+            sleep(0.05)
             Ed.Core.setStatusBarProgressMeterPos(i)
         Ed.Core.restoreStatusBar()
     except Exception as err:
