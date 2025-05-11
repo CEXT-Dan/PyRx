fba25855a189bfd04fa21ae2546ab643f164e1c8
Daniel <dwmcabinets@yahoo.com>
2025-05-11 20:01:52 +0800

    [#226] added test

---
+++ tests/test_core/test_PyDb/test_XrefGraph.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from pyrx import Db, Ed
+
+class TestXrefGraph:
+
+    def test_XrefGraph(self, db_xrefGraph: Db.Database):
+        wdb = Db.AutoWorkingDatabase(db_xrefGraph)
+        Db.Core.resolveCurrentXRefs(Db.workingDb(),True,False)
+        gr = Ed.Core.curDwgXrefGraph()
+        names = []
+        for idx in range(gr.numNodes()):
+            node = gr.xrefNode(idx)
+            names.append(node.name())
+            
+        assert "06457" in names
