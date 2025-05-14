7277c20166fd7f1ffe5e95ed60465e953882a1da
Daniel <dwmcabinets@yahoo.com>
2025-05-14 16:49:42 +0800

    [#314] block CTRL_C

--- PyRxCore/PyAcAp.cpp
+++ PyRxCore/PyAcAp.cpp
@@ -18,7 +18,7 @@ static BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
     switch (fdwCtrlType)
     {
         case CTRL_C_EVENT:
-            return FALSE;
+            return TRUE;
         case CTRL_CLOSE_EVENT:
             return TRUE;
         case CTRL_BREAK_EVENT:
