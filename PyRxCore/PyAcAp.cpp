651c289c1eec868c461c91f5d59a379b5e4bf724
Daniel <dwmcabinets@yahoo.com>
2025-05-14 17:31:42 +0800

    improve CtrlHandler

--- PyRxCore/PyAcAp.cpp
+++ PyRxCore/PyAcAp.cpp
@@ -18,13 +18,9 @@ static BOOL WINAPI CtrlHandler(DWORD fdwCtrlType)
     switch (fdwCtrlType)
     {
         case CTRL_C_EVENT:
-            return TRUE;
         case CTRL_CLOSE_EVENT:
-            return TRUE;
         case CTRL_BREAK_EVENT:
-            return TRUE;
         case CTRL_LOGOFF_EVENT:
-            return TRUE;
         case CTRL_SHUTDOWN_EVENT:
             return TRUE;
         default:
