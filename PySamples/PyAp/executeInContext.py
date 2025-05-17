b591277a42322be971b756403b203cf1f7fc6707
Daniel <dwmcabinets@yahoo.com>
2025-05-17 20:30:15 +0800

    update executeInContext.py

--- PySamples/PyAp/executeInContext.py
+++ PySamples/PyAp/executeInContext.py
@@ -1,8 +1,9 @@
-from pyrx_imp import Rx, Ge, Gs, Gi, Db, Ap, Ed
+from pyrx import Rx, Ge, Gs, Gi, Db, Ap, Ed
 
 print("command = exeappctx")
 print("command = exebappctx")
 print("command = exebcmdctx")
+
 
 class Data:
     def __init__(self, message):
@@ -14,14 +15,16 @@ class Data:
 
     def classWorker(self, args=None):
         print(args.message)
-        
+
     def doit(self):
         print(self.message)
+
 
 def worker(args=None):
     args.doit()
 
-Ap.Command()
+
+@Ap.Command()
 def exeappctx():
     try:
         data = Data("executeInApplicationContext")
@@ -29,8 +32,9 @@ def exeappctx():
         man.executeInApplicationContext(worker, data)
     except Exception as err:
         print(err)
-    
-Ap.Command()    
+
+
+@Ap.Command()
 def exebappctx():
     try:
         data = Data("beginExecuteInApplicationContext")
@@ -39,8 +43,9 @@ def exebappctx():
     except Exception as err:
         print(err)
 
-#note the session context
-Ap.Command("exebcmdctx", Ap.CmdFlags.SESSION)  
+
+# note the session context
+@Ap.Command("exebcmdctx", Ap.CmdFlags.SESSION)
 def exebcmdctx():
     try:
         data = Data("beginExecuteInCommandContext")
