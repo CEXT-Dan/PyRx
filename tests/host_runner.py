b628b8c593130cdf4fcc6ef16685b8a40d5dedd0
Grzegorz Świtek <grzegorz.switek@fort.pl>
2025-05-17 23:01:02 +0200

    [#323] add support for testing in application context

--- tests/host_runner.py
+++ tests/host_runner.py
@@ -17,25 +17,45 @@ def _get_known_failures_markers() -> list[str]:
     return markers
 
 
+def _get_markers_arg(markers: list[str], not_markers: list[str]) -> str:
+    return " and ".join(markers + [f"not {marker}" for marker in not_markers])
+
+
 def run_tests(cfg: TestConfig):
     STANDARD_PYTEST_ARGS = ("-vv", str(TESTS_DIR))
     log_filename = TESTS_OUTPUT_DIR / get_log_filename()
     log_filename.parent.mkdir(parents=True, exist_ok=True)
-    with open(log_filename, "w", encoding="utf-8") as log_file:
-        with redirect_stdout(log_file), redirect_stderr(log_file):
-            pytest_args = [*STANDARD_PYTEST_ARGS]
-            if cfg.pytest_args:
-                pytest_args.extend(cfg.pytest_args)
-            if "-m" not in pytest_args:
-                not_markers = []
-                if cfg.known_failures is False:
-                    not_markers.extend(_get_known_failures_markers())
-                if cfg.slow_tests is False:
-                    not_markers.append("slow")
-                if not_markers:
-                    markers_arg = "not " + " and not ".join(not_markers)
-                    pytest_args.extend(("-m", markers_arg))
-            pytest.main(pytest_args)
+    pytest_args = [*STANDARD_PYTEST_ARGS]
+    if cfg.pytest_args:
+        pytest_args.extend(cfg.pytest_args)
+    if "-m" not in pytest_args:
+        not_markers = []
+        if cfg.known_failures is False:
+            not_markers.extend(_get_known_failures_markers())
+        if cfg.slow_tests is False:
+            not_markers.append("slow")
+        pytest_args_cmdctx = pytest_args.copy()
+        pytest_args_appctx = pytest_args.copy()
+        markers_arg_cmdctx = _get_markers_arg(  # without appctx
+            markers=[],
+            not_markers=(not_markers + ["appctx"]),
+        )
+        markers_arg_appctx = _get_markers_arg(  # only appctx
+            markers=["appctx"],
+            not_markers=not_markers,
+        )
+        pytest_args_cmdctx.extend(("-m", markers_arg_cmdctx))
+        pytest_args_appctx.extend(("-m", markers_arg_appctx))
+
+        with (
+            open(log_filename, "a", encoding="utf-8") as log_file,
+            redirect_stdout(log_file),
+            redirect_stderr(log_file),
+        ):
+            print("Running tests in command context")
+            pytest.main(pytest_args_cmdctx)
+            print("\nRunning tests in application context")
+            Ap.DocManager().executeInApplicationContext(pytest.main, pytest_args_appctx)
 
 
 @Ap.Command("RUN_TESTS", Ap.CmdFlags.NO_UNDO_MARKER | Ap.CmdFlags.NOHISTORY)
