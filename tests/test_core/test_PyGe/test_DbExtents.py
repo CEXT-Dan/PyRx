e076865f094327cc5ea692ccfae4a97c1b8b5979
Daniel <dwmcabinets@yahoo.com>
2025-06-10 07:49:39 +0800

    improve TestDbExtents

--- tests/test_core/test_PyGe/test_DbExtents.py
+++ tests/test_core/test_PyGe/test_DbExtents.py
@@ -24,6 +24,11 @@ class TestDbExtents:
         fence = Ge.LineSeg2d(Ge.Point2d(0,5),Ge.Point2d(100,5) )
         ext1 = Db.Extents2d(Ge.Point2d(0, 0), Ge.Point2d(10, 10))
         assert ext1.intersectsWith(fence) == True
+        
+    def test_dbextents_intersects_seg(self):
+        fence = Ge.LineSeg3d(Ge.Point3d(0,0,0),Ge.Point3d(100,100,100) )
+        ex = Db.Extents(Ge.Point3d(25,25,25),Ge.Point3d(75,75,75))
+        assert ex.intersectsWith(fence) == True
 
 
 
