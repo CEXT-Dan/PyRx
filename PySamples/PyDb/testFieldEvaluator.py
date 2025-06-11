5184ca56c14db76dd4ecae0595935b2036d43cda
Daniel <dwmcabinets@yahoo.com>
2025-06-11 16:44:56 +0800

    update sample

--- PySamples/PyDb/testFieldEvaluator.py
+++ PySamples/PyDb/testFieldEvaluator.py
@@ -1,7 +1,10 @@
 import traceback
 from pyrx import Rx, Ge, Gi, Db, Ap, Ed
+import openpyxl as xl
 
-# %%<\XLSXField M:\\Dev\\Projects\\PyRxGit\\tests\\media\\testdata.xlsx|Show|A1>%%
+#%<\XLSXField M:\\Dev\\Projects\\PyRxGit\\tests\\media\\testdata.xlsx|Show|A1>%
+#%<\XLSXField M:\\Dev\\Projects\\PyRxGit\\tests\\media\\testdata.xlsx|Show|B1>%
+#%<\XLSXField M:\\Dev\\Projects\\PyRxGit\\tests\\media\\testdata.xlsx|strings|A4>%
 
 
 print("added command - addevaluator")
@@ -11,35 +14,41 @@ print("added command - remevaluator")
 class FieldEvaluator(Db.FieldEvaluator):
     def __init__(self, name, evalname):
         Db.FieldEvaluator.__init__(self, name, evalname)
+        self.cache = {}
 
     def evaluate(
         self, fld: Db.Field, ctx: int, db: Db.Database, res: Db.AcValue
     ) -> Db.FieldEvalStatus:
         try:
-            fcode = fld.getFieldCode(Db.FieldCodeFlag.kFieldCode)
-            print("\n-{}-".format(fcode))
-            res.setString("YAY")
+            fcode = fld.getFieldCode(Db.FieldCodeFlag.kFieldCode).strip("\\XLSXField")
+            if fcode in self.cache:
+                res.setString(str(self.cache[fcode]))
+                return Db.FieldEvalStatus.kSuccess
+
+            path, sheet, cell = fcode.split("|")
+            workbook = xl.load_workbook(filename=path.strip(), read_only=True)
+            worksheet = workbook[sheet]
+            res.setString(str(worksheet[cell].value))
+
             return Db.FieldEvalStatus.kSuccess
         except Exception as err:
             traceback.print_exception(err)
             return Db.FieldEvalStatus.kOtherError
 
-    # create your cache here
     def beginEvaluateFields(self, ctx: int, db: Db.Database):
         try:
-            print("beginEvaluateFields")
+            self.cache.clear()
         except Exception as err:
             traceback.print_exception(err)
 
     # clear your cache here
     def endEvaluateFields(self, ctx: int, db: Db.Database):
         try:
-            print("endEvaluateFields")
+            self.cache.clear()
         except Exception as err:
             traceback.print_exception(err)
 
 
-# name qand evalname, is the fieldcode
 evaluator = FieldEvaluator("XLSX Field", "XLSXField")
 
 
