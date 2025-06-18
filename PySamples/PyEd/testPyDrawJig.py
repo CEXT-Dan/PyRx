cb21907bec7dca5f6a22ffed3f21096736d39912
Daniel <dwmcabinets@yahoo.com>
2025-06-18 21:19:53 +0800

    update jig sample

--- PySamples/PyEd/testPyDrawJig.py
+++ PySamples/PyEd/testPyDrawJig.py
@@ -7,6 +7,7 @@ print("command = pydrawjig")
 class MyDrawJig(Ed.DrawJig):
     def __init__(self, basepoint):
         Ed.DrawJig.__init__(self)
+        self.ds = Ed.DragStatus.kNormal
         self.basepoint = basepoint
         self.curpoint = basepoint
         self.tri = [
@@ -19,15 +20,19 @@ class MyDrawJig(Ed.DrawJig):
     # get the mouse point, use base point or not
     def sampler(self):
         self.setUserInputControls(Ed.UserInputControls.kAccept3dCoordinates)
-        ds, self.curpoint = self.acquirePoint(self.basepoint)
-        return ds
+        self.ds, self.curpoint = self.acquirePoint(self.basepoint)
+        return self.ds
 
     def update(self):
+        if self.ds == Ed.DragStatus.kNoChange:
+            return False
         return True
 
-    # draw anything you like, even your block, you can also scale or apply
-    # color
+    # draw anything you like, even your block, you can also scale or apply color
     def worldDraw(self, wd: Gi.WorldDraw):
+        # always draws, do this if you want to skip
+        if self.ds == Ed.DragStatus.kNoChange:
+            return True
         try:
             mat = Ge.Matrix3d.translation(self.curpoint - self.basepoint)
             geo = wd.geometry()
