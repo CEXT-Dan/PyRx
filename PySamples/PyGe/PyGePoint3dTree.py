dbe546bac2f6c1456438cadfc2e8cd87b1ea211b
Daniel <dwmcabinets@yahoo.com>
2025-07-05 09:54:52 +0800

    added sample

---
+++ PySamples/PyGe/PyGePoint3dTree.py
@@ -0,0 +1,47 @@
+import traceback
+from pyrx import Ap, Ax, Db, Ed, Ge, Gi, command
+from timeit import default_timer as timer
+import random
+
+
+@command
+def doit1():
+    start = timer()
+
+    # random point
+    points = Ge.Point3dArray()
+    for i in range(1000):
+        rx = random.uniform(0.0, 100.0)
+        ry = random.uniform(0.0, 100.0)
+        rz = random.uniform(0.0, 100.0)
+        points.append(Ge.Point3d(rx, ry, rz))
+
+    # create the tree
+    tree = Ge.Point3dTree(points)
+
+    # the search is a sphere
+    idxs, dists = tree.radiusSearch(Ge.Point3d(50, 50, 50), 200)
+
+    end = timer()
+    print(len(idxs), end - start)
+
+
+@command
+def doit2():
+    start = timer()
+
+    # random point
+    points = Ge.Point3dArray()
+    for i in range(1000):
+        rx = random.uniform(0.0, 100.0)
+        ry = random.uniform(0.0, 100.0)
+        points.append(Ge.Point3d(rx, ry, 0))
+
+    # create the tree
+    tree = Ge.Point3dTree(points)
+
+    # search the 5 nearest
+    idxs, dists = tree.knnSearch(Ge.Point3d(50, 50, 0), 5)
+
+    end = timer()
+    print(idxs, dists, end - start)
