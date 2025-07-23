72fc4f981d04233cd8355e3d8499f205615c8543
Daniel <dwmcabinets@yahoo.com>
2025-07-23 15:53:56 +0800

    fresh build

--- pyrx/__init__.py
+++ pyrx/__init__.py
@@ -17,7 +17,7 @@
 .. _ARX Documentation: https://help.autodesk.com/view/OARX/2026/ENU/?guid=OARX-RefGuide-ObjectARX_Reference_Guide
 """
 
-__version__ = "2.2.19.4558"
+__version__ = "2.2.19.4583"
 
 import importlib.util
 import warnings
