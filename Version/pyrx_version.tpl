e99ead719fe9bb1bedbd0f06ebe8670d296664fa
Grzegrz Świtek <gswifort@gmail.com>
2025-07-24 09:06:07 +0200

    [#389] generate ``pyrx_version.h`` from ``pyrx_version.tpl``

--- Version/pyrx_version.tpl
+++ Version/pyrx_version.tpl
@@ -1,10 +1,10 @@
 //-----------------------------------------------------
 // auto generated from pyrx_version.tpl 
 
-#define VERSION_MAJOR 2
-#define VERSION_MINOR 2
-#define VERSION_BUILD 19
-#define VERSION_REVISION $WCLOGCOUNT&65535$
+#define VERSION_MAJOR {VERSION_MAJOR}
+#define VERSION_MINOR {VERSION_MINOR}
+#define VERSION_BUILD {VERSION_BUILD}
+#define VERSION_REVISION {VERSION_REVISION}
 
 #define VERSION_STRINGIZE_I(x) #x
 #define VERSION_STRINGIZE(x) VERSION_STRINGIZE_I(x)
--- Version/version.py
+++ Version/version.py
@@ -1,6 +1,11 @@
 import subprocess
+from pathlib import Path
 
 from setuptools_scm import ScmVersion, get_version
+
+BASE_DIR = Path(__file__).parent
+PYRX_VERSION_H_TPL = BASE_DIR / "pyrx_version.tpl"
+PYRX_VERSION_H = BASE_DIR / "../pyrx_version.h"
 
 
 def custom_version_scheme(version: ScmVersion) -> str:
@@ -23,5 +28,23 @@ def get_project_version():
     )
 
 
+def override_pyrx_version_h(version: str) -> None:
+    major, minor, build, revision = version.split(".")
+    content = (
+        PYRX_VERSION_H_TPL.read_text("utf-8")
+        .replace("{VERSION_MAJOR}", major)
+        .replace("{VERSION_MINOR}", minor)
+        .replace("{VERSION_BUILD}", build)
+        .replace("{VERSION_REVISION}", revision)
+    )
+    PYRX_VERSION_H.write_text(content, "utf-8")
+
+
+def main():
+    version = get_project_version()
+    override_pyrx_version_h(version)
+    print(f"PyRx version set to: {version}")
+
+
 if __name__ == "__main__":
-    print(get_project_version())
+    main()
