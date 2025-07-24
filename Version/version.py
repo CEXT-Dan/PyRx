e5686bfa17b059f7604644cc3ca005cefb8bb581
Grzegrz Świtek <gswifort@gmail.com>
2025-07-24 12:57:24 +0200

    [#389] MAJOR, MINOR, BUILD manually; update _version.py when building dll

--- Version/version.py
+++ Version/version.py
@@ -2,8 +2,10 @@ import subprocess
 from pathlib import Path
 
 from setuptools_scm import ScmVersion, get_version
+from setuptools_scm._integration.dump_version import write_version_to_path
 
 BASE_DIR = Path(__file__).parent
+PYRX_VERSION_PY = BASE_DIR / "../pyrx/_version.py"
 PYRX_VERSION_H_TPL = BASE_DIR / "pyrx_version.tpl"
 PYRX_VERSION_H = BASE_DIR / "../pyrx_version.h"
 
@@ -28,22 +30,28 @@ def get_project_version():
     )
 
 
-def override_pyrx_version_h(version: str) -> None:
-    major, minor, build, revision = version.split(".")
-    content = (
-        PYRX_VERSION_H_TPL.read_text("utf-8")
-        .replace("{VERSION_MAJOR}", major)
-        .replace("{VERSION_MINOR}", minor)
-        .replace("{VERSION_BUILD}", build)
-        .replace("{VERSION_REVISION}", revision)
-    )
+def override_pyrx_version_h(version: str) -> str:
+    _, _, _, revision = version.split(".")
+    revision_inc = f"{int(revision) + 1}"
+    content = PYRX_VERSION_H_TPL.read_text("utf-8").replace("{VERSION_REVISION}", revision_inc)
     PYRX_VERSION_H.write_text(content, "utf-8")
+    return revision_inc
+
+
+def override_pyrx_version_py(version: str) -> str:
+    major, minor, build, revision = version.split(".")
+    revision_inc = f"{int(revision) + 1}"
+    version_inc = f"{major}.{minor}.{build}.{revision_inc}"
+    write_version_to_path(PYRX_VERSION_PY, template=None, version=version_inc, scm_version=None)
+    return version_inc
 
 
 def main():
     version = get_project_version()
-    override_pyrx_version_h(version)
-    print(f"PyRx version set to: {version}")
+    revision_inc = override_pyrx_version_h(version)
+    print(f"The revision number in the file {PYRX_VERSION_H.name} has been set to: {revision_inc}")
+    version_inc = override_pyrx_version_py(version)
+    print(f"The version in the file {PYRX_VERSION_PY.name} has been set to: {version_inc}")
 
 
 if __name__ == "__main__":
