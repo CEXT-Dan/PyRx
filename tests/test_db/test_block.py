e00d7481a8c78cfeb4a690e83b58ce7b76f9cb3f
Grzegrz Świtek <gswifort@gmail.com>
2025-07-27 12:09:29 +0200

    [#392] add get_block_reference function

--- tests/test_db/test_block.py
+++ tests/test_db/test_block.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+from datetime import datetime
 from pathlib import Path
-from pyrx import Db
+from pyrx import Db, Ge
 from pyrx.db.block import (
     add_block_definition,
     BlockAlreadyExistsError,
     get_block_by_name,
     BlockNotFoundError,
+    get_block_reference,
 )
 import re
 import pytest
@@ -216,3 +218,207 @@ class Test_get_block_by_name:
                 BlockNotFoundError, match="Block definition 'MissingBlock' not found"
             ):
                 get_block_by_name("MissingBlock", db=db)
+
+
+class Test_get_block_reference:
+    def test_get_block_reference_by_name(self, db_with_block: Db.Database):
+        btr_id = Db.BlockTable(db_with_block.blockTableId()).getAt("TestBlock")
+        res = get_block_reference("TestBlock", db=db_with_block)
+        assert isinstance(res, Db.ObjectId)
+        bref = Db.BlockReference(res)
+        assert bref.blockTableRecord() == btr_id
+        assert bref.getBlockName() == "TestBlock"
+
+    def test_get_block_reference_by_id(self, db_with_block: Db.Database):
+        btr_id = Db.BlockTable(db_with_block.blockTableId()).getAt("TestBlock")
+        res = get_block_reference(btr_id, db=db_with_block)
+        assert isinstance(res, Db.ObjectId)
+        bref = Db.BlockReference(res)
+        assert bref.blockTableRecord() == btr_id
+        assert bref.getBlockName() == "TestBlock"
+
+    def test_raises_if_block_not_found(self, db_with_block: Db.Database):
+        with pytest.raises(BlockNotFoundError, match="Block definition 'MissingBlock' not found"):
+            get_block_reference("MissingBlock", db=db_with_block)
+
+    def test_position_rotation_scale(self, db_with_block: Db.Database):
+        bref_id = get_block_reference(
+            "TestBlock", db=db_with_block, position=(10, 20, 30), rotation=1.57, scale=2
+        )
+        bref = Db.BlockReference(bref_id)
+        assert bref.position() == Ge.Point3d(10, 20, 30)
+        assert bref.rotation() == 1.57
+        assert bref.scaleFactors() == Ge.Scale3d(2, 2, 2)
+
+    def test_invalid_btr_id_or_name(self):
+        with pytest.raises(
+            TypeError, match="btr_id_or_name must be a string or Db.ObjectId, not int"
+        ):
+            get_block_reference(123)
+
+    def test_scale_dyn_properties_with_non_uniform_scale(self, db_with_block: Db.Database):
+        with pytest.raises(ValueError, match="scale_dyn_properties requires uniform scale"):
+            get_block_reference(
+                "TestBlock", db=db_with_block, scale=Ge.Scale3d(2, 3, 4), scale_dyn_properties=True
+            )
+
+    def test_transaction_success(self, db_with_block: Db.Database):
+        obj_ids_before = db_with_block.objectIds(Db.BlockReference.desc())
+        get_block_reference("TestBlock", db=db_with_block)
+        obj_ids_after = db_with_block.objectIds(Db.BlockReference.desc())
+        assert len(obj_ids_after) == len(obj_ids_before) + 1
+
+    def test_transaction_error(self, db_with_block: Db.Database):
+        obj_ids_before = db_with_block.objectIds(Db.BlockReference.desc())
+        with patch.object(Db.BlockTableRecord, "hasAttributeDefinitions") as mock_btr:
+            mock_btr.side_effect = Exception("Simulated error")
+            with pytest.raises(Exception, match="Simulated error"):
+                get_block_reference("TestBlock", db=db_with_block)
+        obj_ids_after = db_with_block.objectIds(Db.BlockReference.desc())
+        assert len(obj_ids_after) == len(obj_ids_before)
+
+    def test_complex(self, db_dyn_block_ref: Db.Database):
+        bref_id = get_block_reference(
+            "TEST DYN BLOCK REF",
+            position=(10, 20, 30),
+            rotation=1.57,
+            scale=2,
+            attributes={
+                "ATTR_1": "Value 1",
+            },
+            dyn_properties={
+                "Distance1": 200.0,  # should be scaled to 400.0
+                "Distance2": 300.0,  # should be scaled to 600.0
+                "Angle1": Db.EvalVariant(1.0),  # should be 1
+                "Visibility1": "VisibilityState1",
+            },
+            scale_dyn_properties=True,
+            db=db_dyn_block_ref,
+        )
+        bref = Db.BlockReference(bref_id)
+
+        assert bref.position() == Ge.Point3d(10, 20, 30)
+        assert bref.rotation() == 1.57
+        assert bref.scaleFactors() == Ge.Scale3d(2, 2, 2)
+        assert bref.getBlockName() == "TEST DYN BLOCK REF"
+
+        attrs = {
+            attr.tag(): attr
+            for attr in (Db.AttributeReference(attr_id) for attr_id in bref.attributeIds())
+        }
+        assert len(attrs) == 4  # ATTR_2 is constant
+        assert not set(attrs).symmetric_difference({"ATTR_1", "ATTR_3", "ATTR_4", "ATTR_5"})
+
+        tst = Db.TextStyleTable(db_dyn_block_ref.textStyleTableId())
+        tst_standard = tst.getAt("STANDARD")
+        tst_1 = tst.getAt("TEXT_STYLE_1")
+        tst_2 = tst.getAt("TEXT_STYLE_2")
+        tst.dispose()
+
+        attr_1 = attrs["ATTR_1"]
+        assert attr_1.textString() == "Value 1"
+        assert attr_1.isInvisible() is True
+        assert attr_1.isConstant() is False
+        assert attr_1.isVerifiable() is False
+        assert attr_1.lockPositionInBlock() is False
+        assert attr_1.isPreset() is False
+        assert attr_1.height() == pytest.approx(11.0, abs=0.01)
+        assert attr_1.rotation() == pytest.approx(1.57, abs=0.01)
+        assert attr_1.textStyle() == tst_1
+        assert attr_1.verticalMode() == Db.TextVertMode.kTextTop
+        assert attr_1.horizontalMode() == Db.TextHorzMode.kTextCenter
+        assert attr_1.isMTextAttribute() is False
+
+        attr_3 = attrs["ATTR_3"]
+        assert attr_3.isInvisible() is False
+        assert attr_3.isConstant() is False
+        assert attr_3.isVerifiable() is True
+        assert attr_3.lockPositionInBlock() is True
+        assert attr_3.isPreset() is False
+        assert attr_3.height() == pytest.approx(15.0, abs=0.01)
+        attr_3_rotation = (
+            0.785  # base
+            + 1.57  # bref rotation
+            + 1.0  # Angle1 rotation
+        )
+        assert attr_3.rotation() == pytest.approx(attr_3_rotation, abs=0.01)
+        assert attr_3.textStyle() == tst_1
+        assert attr_3.verticalMode() == Db.TextVertMode.kTextTop
+        assert attr_3.horizontalMode() == Db.TextHorzMode.kTextRight
+        assert attr_3.isMTextAttribute() is True
+        attr_3_mtext = attr_3.getMTextAttribute()
+        assert attr_3_mtext.text() == "ATTR\r\n3\r\nvalue"
+
+        attr_4 = attrs["ATTR_4"]
+        assert attr_4.isInvisible() is False
+        assert attr_4.isConstant() is False
+        assert attr_4.isVerifiable() is False
+        assert attr_4.lockPositionInBlock() is False
+        assert attr_4.isPreset() is True
+        assert attr_4.height() == pytest.approx(16.6, abs=0.01)
+        assert attr_4.rotation() == pytest.approx(3.14, abs=0.01)
+        assert attr_4.textStyle() == tst_2
+        assert attr_4.verticalMode() == Db.TextVertMode.kTextBottom
+        assert attr_4.horizontalMode() == Db.TextHorzMode.kTextLeft
+        assert attr_4.isMTextAttribute() is False
+        assert attr_4.textString() == "Block location: 10.0000, 20.0000, 30.0000"
+
+        attr_5 = attrs["ATTR_5"]
+        assert attr_5.isInvisible() is False
+        assert attr_5.isConstant() is False
+        assert attr_5.isVerifiable() is False
+        assert attr_5.lockPositionInBlock() is True
+        assert attr_5.isPreset() is False
+        assert attr_5.height() == pytest.approx(30.0, abs=0.01)
+        assert attr_5.rotation() == pytest.approx(1.57, abs=0.01)
+        assert attr_5.textStyle() == tst_standard
+        assert attr_5.verticalMode() == Db.TextVertMode.kTextVertMid
+        assert attr_5.horizontalMode() == Db.TextHorzMode.kTextCenter
+        assert attr_5.isMTextAttribute() is True
+        attr_5_mtext = attr_5.getMTextAttribute()
+        datetime_str = datetime.now().strftime("%Y%m%d")
+        expr_value = f"Expr: {datetime_str}"
+        assert attr_5_mtext.text().startswith(expr_value)
+
+        bref.dispose()
+        dyn_bref = Db.DynBlockReference(bref_id)
+        props = {prop.propertyName(): prop for prop in dyn_bref.getBlockProperties()}
+        assert len(props) >= 3
+        distance_1 = props["Distance1"]
+        assert distance_1.value().getDouble() == pytest.approx(400.0, abs=0.01)
+        distance_2 = props["Distance2"]
+        assert distance_2.value().getDouble() == pytest.approx(600.0, abs=0.01)
+        angle_1 = props["Angle1"]
+        assert angle_1.value().getDouble() == pytest.approx(1.0, abs=0.01)
+        visibility_1 = props["Visibility1"]
+        assert visibility_1.value().getString() == "VisibilityState1"
+        circle = Db.Circle(db_dyn_block_ref.objectIds(Db.Circle.desc())[0])
+        arc = Db.Arc(db_dyn_block_ref.objectIds(Db.Arc.desc())[0])
+        assert circle.visibility() == Db.Visibility.kVisible
+        assert arc.visibility() == Db.Visibility.kInvisible
+
+    def test_attr_position(self, db_dyn_block_ref: Db.Database):
+        bref_id = get_block_reference(
+            "SIMPLE_BLOCK",
+            position=(10, 20, 30),
+            rotation=1.57,
+            scale=2,
+            dyn_properties={
+                "Distance1": 10.0,  # should be scaled to 20.0
+            },
+            scale_dyn_properties=True,
+            db=db_dyn_block_ref,
+        )
+        bref = Db.BlockReference(bref_id)
+        attrs = {
+            attr.tag(): attr
+            for attr in (Db.AttributeReference(attr_id) for attr_id in bref.attributeIds())
+        }
+        assert len(attrs) == 1
+        attr_1 = attrs["ATTR_1"]
+        # att_def position is (0, 0, 0), the attribute is moved by
+        # "Distance1" parameter (base length 5.0), the reference
+        # insertion point is (10, 20, 30) and the rotation is 90 degrees
+        # (relative to the insertion point), therefore the final
+        # position of the attribute should be (10, 30, 30)
+        assert attr_1.position() == pytest.approx((10, 30, 30), abs=0.01)
