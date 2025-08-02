293ae6258f1a905044a600cf195695b4127be8dd
Daniel <dwmcabinets@yahoo.com>
2025-08-02 22:26:56 +0800

    [#404] PipManager

--- pyrx/_commands.py
+++ pyrx/_commands.py
@@ -1,4 +1,8 @@
 import wx
+import subprocess
+import sys
+import traceback
+
 
 from . import command
 from .PyRxDebug import startListener
@@ -40,3 +44,93 @@ def pydebug_port():
         "You can use the following configuration in VS Code:\n"
         f"{_VSCODE_DEBUG_CONFIG}"
     )
+
+
+class PipManager:
+    """
+    A utility class for managing Python packages using pip.
+    The PipManager class provides static methods to install, update, uninstall, and list Python packages
+    by invoking pip commands through subprocess. Each method handles exceptions and prints the output or errors.
+    Methods
+    -------
+    install(package: str) -> None
+        Installs the specified Python package using pip.
+    update(package: str) -> None
+        Upgrades the specified Python package to the latest version using pip.
+    uninstall(package: str) -> None
+        Uninstalls the specified Python package using pip.
+    list() -> None
+        Lists all installed Python packages using pip.
+    """
+    @staticmethod
+    def install(package):
+        try:
+            result = subprocess.run(
+                [sys.executable, "-m", "pip", "install", package],
+                capture_output=True,
+                text=True,
+                check=True,
+                shell=True,
+            )
+            print(f"{result.stdout}\n")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to install {package}: {e}")
+        except Exception as e:
+            print(f"An error occurred while installing {package}: {e}")
+            traceback.print_exc()
+
+    @staticmethod
+    def update(package):
+        try:
+            result = subprocess.run(
+                [sys.executable, "-m", "pip", "install", "--upgrade", package],
+                capture_output=True,
+                text=True,
+                check=True,
+                shell=True,
+            )
+            print(f"{result.stdout}\n")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to upgrade {package}: {e}")
+        except Exception as e:
+            print(f"An error occurred while upgrading {package}: {e}")
+            traceback.print_exc()
+
+    @staticmethod
+    def uninstall(package):
+        try:
+            result = subprocess.run(
+                [sys.executable, "-m", "pip", "uninstall", "-y", package],
+                capture_output=True,
+                text=True,
+                check=True,
+                shell=True,
+            )
+            print(f"{result.stdout}\n")
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to uninstall {package}: {e}")
+        except Exception as e:
+            print(f"An error occurred while uninstalling {package}: {e}")
+            traceback.print_exc()
+
+    @staticmethod
+    def list():
+        try:
+            result = subprocess.run(
+                [sys.executable, "-m", "pip", "list"],
+                capture_output=True,
+                text=True,
+                check=True,
+                shell=True,
+            )
+            print(f"{result.stdout}\n")
+        except subprocess.CalledProcessError as e:
+            print(f"Command failed with error: {e}")
+        except Exception as e:
+            print("An error occurred while calling list:")
+            traceback.print_exc()
+
+
+# @command
+# def pypip() -> None:
+#     PipManager.list()
