58a3e22bbf30ad33dd2bafcb89e74b9e9a6c08a3
Grzegrz Świtek <gswifort@gmail.com>
2025-08-03 21:58:56 +0200

    [#290] unique version for each commit

--- Version/version.py
+++ Version/version.py
@@ -1,84 +1,98 @@
+import re
 import subprocess
 from pathlib import Path
 
-from setuptools_scm import ScmVersion, get_version
 from setuptools_scm._integration.dump_version import write_version_to_path
 
 BASE_DIR = Path(__file__).parent
 REPO_DIR = BASE_DIR / ".."
-PYRX_INIT_PY_TPL = BASE_DIR / "__init__.tpl"
-PYRX_INIT_PY = BASE_DIR / "../pyrx/__init__.py"
+BASE_VERSION_FILE = BASE_DIR / "version.txt"
+PYRX_VERSION_PY = BASE_DIR / "../pyrx/_version.py"
 PYRX_VERSION_H_TPL = BASE_DIR / "pyrx_version.tpl"
 PYRX_VERSION_H = BASE_DIR / "../pyrx_version.h"
 PYRX_PACKAGE_CONTENTS_TPL = BASE_DIR / "PackageContents.tpl"
 PYRX_PACKAGE_CONTENTS = BASE_DIR / "../PyRxAcSetup/Include/PackageContents.xml"
 
-MAJOR = 2
-MINOR = 2
-REVISION= 23
+BASE_VERSION_PATTERN = re.compile(r"^(\d+)\.(\d+)\.(\d+)$")
 
-def custom_version_scheme(version: ScmVersion) -> str:
-    base_version = version.format_with("{tag}")
+
+def get_base_version() -> str:
+    version = BASE_VERSION_FILE.read_text("utf-8").strip()
+    if not BASE_VERSION_PATTERN.fullmatch(version):
+        raise ValueError(f"Invalid version format: {version}")
+    return version
+
+
+def get_revision_number() -> str:
+    number = subprocess.check_output(
+        ["git", "rev-list", "--count", "--first-parent", "main"], text=True
+    ).strip()
+    if not number.isdigit():
+        raise ValueError(f"Revision number is not a digit: {number}")
+    return number
+
+
+def get_project_version() -> tuple[str, tuple[str, str, str, str]]:
+    base_version = get_base_version()
     try:
-        count = subprocess.check_output(
-            ["git", "rev-list", "--count", "--first-parent", "main"], text=True, cwd=REPO_DIR
-        ).strip()
+        revision = get_revision_number()
     except Exception as err:
         err.add_note("Failed to get commit count from git.")
         raise err
+    version_tuple = parse_version(base_version)
 
-    return f"{base_version}.{count}"
+    return f"{base_version}.{revision}", (*version_tuple, revision)
 
 
-def get_project_version():
-    return get_version(
-        root=REPO_DIR,
-        version_scheme=custom_version_scheme,
-        local_scheme="no-local-version",
+def parse_version(version: str) -> tuple:
+    match = BASE_VERSION_PATTERN.fullmatch(version)
+    if not match:
+        raise ValueError(f"Invalid version format: {version}")
+    return match.groups()
+
+
+def override_pyrx_version_h(major: str, minor: str, revision: str, version_revision: str) -> None:
+    content = PYRX_VERSION_H_TPL.read_text("utf-8")
+    content = (
+        content.replace("{MAJOR}", str(major))
+        .replace("{MINOR}", str(minor))
+        .replace("{REVISION}", str(revision))
+        .replace("{VERSION_REVISION}", version_revision)
+    )
+    PYRX_VERSION_H.write_text(content, "utf-8")
+    print(
+        f"The version in the file {PYRX_VERSION_H.name} "
+        f"has been set to: {major}.{minor}.{revision}.{version_revision}"
     )
 
-def override_pyrx_version_h(version: str) -> str:
-    _, _, _, revision = version.split(".")
-    revision_inc = f"{int(revision) + 1}"
-    content = PYRX_VERSION_H_TPL.read_text("utf-8")
-    content = content.replace("{MAJOR}", str(MAJOR))
-    content = content.replace("{MINOR}", str(MINOR))
-    content = content.replace("{REVISION}", str(REVISION))
-    content = content.replace("{VERSION_REVISION}", revision_inc)
-    PYRX_VERSION_H.write_text(content, "utf-8")
-    return revision_inc
+
+def override_pyrx_version_py(major: str, minor: str, revision: str, version_revision: str) -> None:
+    version = f"{major}.{minor}.{revision}.{version_revision}"
+    write_version_to_path(PYRX_VERSION_PY, template=None, version=version, scm_version=None)
+    print(f"The version in the file {PYRX_VERSION_PY.name} has been set to: {version}")
 
 
-def override_pyrx_init_h(version: str) -> str:
-    _, _, _, revision = version.split(".")
-    revision_inc = f"{int(revision) + 1}"
-    content = PYRX_INIT_PY_TPL.read_text("utf-8")
-    content = content.replace("{MAJOR}", str(MAJOR))
-    content = content.replace("{MINOR}", str(MINOR))
-    content = content.replace("{REVISION}", str(REVISION))
-    content = content.replace("{VERSION_REVISION}", revision_inc)
-    PYRX_INIT_PY.write_text(content, "utf-8")
-    return revision_inc
-
-def override_package_contents(version: str) -> str:
-    _, _, _, revision = version.split(".")
-    revision_inc = f"{int(revision) + 1}"
-    content = PYRX_PACKAGE_CONTENTS_TPL.read_text("utf-8")
-    content = content.replace("{MAJOR}", str(MAJOR))
-    content = content.replace("{MINOR}", str(MINOR))
-    content = content.replace("{REVISION}", str(REVISION))
+def override_package_contents(major: str, minor: str, revision: str) -> None:
+    content = (
+        PYRX_PACKAGE_CONTENTS_TPL.read_text("utf-8")
+        .replace("{MAJOR}", str(major))
+        .replace("{MINOR}", str(minor))
+        .replace("{REVISION}", str(revision))
+    )
     PYRX_PACKAGE_CONTENTS.write_text(content, "utf-8")
-    return revision_inc
+    print(
+        f"The version in the file {PYRX_PACKAGE_CONTENTS.name} "
+        f"has been set to: {major}.{minor}.{revision}"
+    )
 
 
 def main():
-    version = get_project_version()
-    revision_inc = override_pyrx_version_h(version)
-    print(f"The revision number in the file {PYRX_VERSION_H.name} has been set to: {revision_inc}")
-    version_inc = override_pyrx_init_h(version)
-    print(f"The version in the file {PYRX_INIT_PY.name} has been set to: {version_inc}")
-    version_inc = override_package_contents(version)
-    print(f"The version in the file {PYRX_PACKAGE_CONTENTS.name} has been set to: {version_inc}")
+    _, version_tuple = get_project_version()
+    major, minor, revision, version_revision = version_tuple
+    version_revision_inc = f"{int(version_revision) + 1}"
+    override_pyrx_version_h(major, minor, revision, version_revision_inc)
+    override_pyrx_version_py(major, minor, revision, version_revision_inc)
+    override_package_contents(major, minor, revision)
 
 
 if __name__ == "__main__":
