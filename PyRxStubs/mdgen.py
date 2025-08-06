3f9d186ada8fe2dacd6235df8a0bb5c947cbe4c4
Daniel <dwmcabinets@yahoo.com>
2025-08-06 17:56:53 +0800

    move files

---
+++ PyRxStubs/mdgen.py
@@ -0,0 +1,49 @@
+import traceback
+from typing import no_type_check
+from pyrx import Ap, Ed, Db
+
+src_path = "M:/Dev/Projects/PyRxGit/pyrx/"
+all_modules = [
+    ("PyRx", "PyRx.pyi"),
+    ("PyGe", "PyGe.pyi"),
+    ("PyGi", "PyGi.pyi"),
+    ("PyGs", "PyGs.pyi"),
+    ("PyDb", "PyDb.pyi"),
+    ("PyAp", "PyAp.pyi"),
+    ("PyEd", "PyEd.pyi"),
+    ("PyPl", "PyPl.pyi"),
+    ("PySm", "PySm.pyi"),
+    ("PyBr", "PyBr.pyi"),
+    ("PyAx", "PyAx.pyi"),
+]
+
+
+def generate_md(moduleName, moduleFile):
+    src_file = "{}{}".format(src_path,moduleFile)
+    dst_file = "{}{}".format(moduleName,".md")
+    
+    ignore = False
+    with open(src_file, 'r') as infile:
+        with open(dst_file, 'w') as outfile:
+            outfile.write('# {}\n'.format(moduleName))
+            outfile.write('\n```PY\n')
+            for line in infile:
+                if '"""' in line:
+                    ignore = not ignore
+                    continue
+                if ignore:
+                    continue
+                if 'import' in line:
+                    continue
+                outfile.write(line.replace('...',' '))
+            outfile.write('```\n')
+                    
+                      
+@Ap.Command()
+def mdgen() -> None:
+    try:
+        for name, module in all_modules:
+            generate_md(name, module)
+
+    except Exception as err:
+        traceback.print_exception(err)
