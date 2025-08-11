1ee6bc53e4243f9c31c20bbbb3c289a892159087
Daniel <dwmcabinets@yahoo.com>
2025-08-11 10:16:45 +0800

    added new test_Document.py

--- tests/test_core/test_PyAp/test_Application.py
+++ tests/test_core/test_PyAp/test_Application.py
@@ -5,40 +5,36 @@ from tests import MEDIA_DIR
 
 
 class TestApplication:
-    
+
     def test_loadPythonModule(self):
-        moddict = Ap.Application.loadPythonModule(str(MEDIA_DIR/'loadtest.py'))
-        assert moddict['GLOBAL_VAL'] == 1
-        func = moddict['loadtestfunc']
-        assert func() == 'YIPPIE'
-         
-        dummydict = Ap.Application.loadPythonModule(str(MEDIA_DIR/'submedia/dummy.py'))
-        assert dummydict['test253']() == True
-        
-        
+        moddict = Ap.Application.loadPythonModule(str(MEDIA_DIR / "loadtest.py"))
+        assert moddict["GLOBAL_VAL"] == 1
+        func = moddict["loadtestfunc"]
+        assert func() == "YIPPIE"
+
+        dummydict = Ap.Application.loadPythonModule(str(MEDIA_DIR / "submedia/dummy.py"))
+        assert dummydict["test253"]() == True
+
     def test_getLocalAppDataPath(self):
         path = Ap.Application.getLocalAppDataPath()
         assert path.find("PyRx") != -1
         assert path.find("Local") != -1
         assert path.find("AppData") != -1
-        
+
     def test_getAppDataPath(self):
         path = Ap.Application.getAppDataPath()
         assert path.find("PyRx") != -1
         assert path.find("Roaming") != -1
         assert path.find("AppData") != -1
-        
+
     def test_listFilesInPath(self):
         path = Ap.Application.listFilesInPath(str(MEDIA_DIR), ".dwg")
         assert len(path) == 13
         path = Ap.Application.listFilesInPath(str(MEDIA_DIR), ".dWg")
         assert len(path) == 13
-        
+
     def test_listFilesInPathRecursive(self):
         path = Ap.Application.listFilesInPathRecursive(str(MEDIA_DIR), ".dwg")
         assert len(path) == 14
         path = Ap.Application.listFilesInPathRecursive(str(MEDIA_DIR), ".dWg")
         assert len(path) == 14
-
-
-
