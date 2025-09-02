6c862cf8aa7d54cce4741b143f85119bac702c8d
Daniel <dwmcabinets@yahoo.com>
2025-09-02 10:34:06 +0800

    embedded setup

---
+++ PyRxAcSetupEB/CustomAction_Impersonate.js
@@ -0,0 +1,54 @@
+// CustomAction_Impersonate.js <msi-file>
+// Performs a post-build fixup of an msi to change all deferred custom actions to Impersonate
+// Constant values from Windows Installer
+var msiOpenDatabaseModeTransact = 1;
+
+var msiViewModifyInsert         = 1
+var msiViewModifyUpdate         = 2
+var msiViewModifyAssign         = 3
+var msiViewModifyReplace        = 4
+var msiViewModifyDelete         = 6
+
+var msidbCustomActionTypeInScript       = 0x00000400;
+var msidbCustomActionTypeNoImpersonate  = 0x00000800
+
+if (WScript.Arguments.Length != 1)
+{
+       WScript.StdErr.WriteLine(WScript.ScriptName + " file");
+       WScript.Quit(1);
+}
+
+var filespec = WScript.Arguments(0);
+var installer = WScript.CreateObject("WindowsInstaller.Installer");
+var database = installer.OpenDatabase(filespec, msiOpenDatabaseModeTransact);
+
+var sql
+var view
+var record
+
+try
+{
+       sql = "SELECT `Action`, `Type`, `Source`, `Target` FROM `CustomAction`";
+       view = database.OpenView(sql);
+       view.Execute();
+       record = view.Fetch();
+    //Loop through all the Custom Actions
+       while (record)
+       {
+           if (record.IntegerData(2) & msidbCustomActionTypeInScript)
+           {
+               //We must flip the msidbCustomActionTypeNoImpersonate bit only for deferred custom actions
+               record.IntegerData(2) = record.IntegerData(2) & ~msidbCustomActionTypeNoImpersonate;
+              view.Modify(msiViewModifyReplace, record);
+           }
+        record = view.Fetch();
+       }
+
+       view.Close();
+       database.Commit();
+}
+catch(e)
+{
+       WScript.StdErr.WriteLine(e);
+       WScript.Quit(1);
+}
