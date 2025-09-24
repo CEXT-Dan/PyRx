bb5fbc8c19396c28d75c1dd9c51bda2e1c3553cc
Daniel <dwmcabinets@yahoo.com>
2025-09-24 15:27:27 +0800

    added Ed.Core.lineWeightDialog

---
+++ PySamples/PyEd/lineWeightDialog.py
@@ -0,0 +1,19 @@
+from pyrx import Ap, Db, Ed, Rx
+
+
+@Ap.Command()
+def doit():
+    db = Db.curDb()
+    print(Ed.Core.lineWeightDialog(db.celweight(), True))
+
+
+# (ads_lineWeightDialog)
+@Ap.LispFunction()
+def ads_lineWeightDialog(args):
+    db = Db.curDb()
+    buff = []
+    flag, lw = Ed.Core.lineWeightDialog(db.celweight(), True)
+    if flag:
+        return(Rx.LispType.kInt32, lw)
+    else:
+       return (Rx.LispType.kNil, 0)
