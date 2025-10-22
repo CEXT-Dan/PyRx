5c2896c59407b2d27044e3fa3184218573e00008
Daniel <dwmcabinets@yahoo.com>
2025-10-23 06:17:09 +0800

    added TestBlockTableRecord.test_visibleObjectIds

--- tests/test_core/test_PyDb/test_BlockTableRecord.py
+++ tests/test_core/test_PyDb/test_BlockTableRecord.py
@@ -12,3 +12,15 @@ class TestBlockTableRecord:
             if btr.isLayout():
                 continue
             assert "*" not in btr.effectiveName()
+            
+    def test_visibleObjectIds(self, db_dynblock: Db.Database):
+        objHnd = Db.Handle("70c")
+        objId = db_dynblock.getObjectId(False, objHnd)
+        assert objId.isValid() is True
+        dynref = Db.DynBlockReference(objId)
+        assert dynref.isDynamicBlock() is True
+        btr = Db.BlockTableRecord(dynref.anonymousBlockTableRecord())
+        assert len(btr.objectIds()) == 8
+        assert len(btr.visibleObjectIds()) == 5
+
+
