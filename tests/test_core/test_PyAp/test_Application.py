4c53df8eaef424aa512876f49714f0fbaaf1f1c0
Daniel <dwmcabinets@yahoo.com>
2025-10-30 06:59:13 +0800

    add testTestDatabase::test_using_decorator

--- tests/test_core/test_PyAp/test_Application.py
+++ tests/test_core/test_PyAp/test_Application.py
@@ -29,12 +29,12 @@ class TestApplication:
 
     def test_listFilesInPath(self):
         path = Ap.Application.listFilesInPath(str(MEDIA_DIR), ".dwg")
-        assert len(path) == 14
+        assert len(path) == 15
         path = Ap.Application.listFilesInPath(str(MEDIA_DIR), ".dWg")
-        assert len(path) == 14
+        assert len(path) == 15
 
     def test_listFilesInPathRecursive(self):
         path = Ap.Application.listFilesInPathRecursive(str(MEDIA_DIR), ".dwg")
-        assert len(path) == 15
+        assert len(path) == 16
         path = Ap.Application.listFilesInPathRecursive(str(MEDIA_DIR), ".dWg")
-        assert len(path) == 15
+        assert len(path) == 16
