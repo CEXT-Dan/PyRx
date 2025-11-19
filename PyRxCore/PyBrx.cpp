8d87fab8cd20d452d3d3b99abc9d069180f7bb61
Daniel <dwmcabinets@yahoo.com>
2025-11-19 11:55:41 +0800

    [#432]classes MechanicalPropertySet, MechanicalPropertyDefinition

--- PyRxCore/PyBrx.cpp
+++ PyRxCore/PyBrx.cpp
@@ -3,12 +3,13 @@
 #include "PyBrxDbProperties.h"
 #include "PyBrxCore.h"
 #include "PyBrxPanel.h"
+#include "PyBrxComponents.h"
 
 #ifdef BRXAPP
 
 using namespace boost::python;
 
-BOOST_PYTHON_MODULE(PyBrx)
+static BOOST_PYTHON_MODULE(PyBrx)
 {
     docstring_options local_docstring_options(py_show_user_defined, py_show_py_signatures, py_show_cpp_signatures);
 
@@ -17,6 +18,9 @@ BOOST_PYTHON_MODULE(PyBrx)
     makePyBrxDbPropertiesWrapper();
 #endif
     makePyBrxPanelWrapper();
+    makePyBrxMechanicalPropertySet();
+    makePyBrxMechanicalPropertyDefinition();
+
     enum_<BricsCAD::LicensedFeature>("LicensedFeature")
         .value("eCore", BricsCAD::LicensedFeature::eCore)
         .value("eCommunicator", BricsCAD::LicensedFeature::eCommunicator)
@@ -24,7 +28,17 @@ BOOST_PYTHON_MODULE(PyBrx)
         .value("eMechanical", BricsCAD::LicensedFeature::eMechanical)
         .export_values()
         ;
+
+    enum_<BrxMechanicalPropertyType>("MechanicalPropertyType")
+        .value("eUndefined", BrxMechanicalPropertyType::eUndefined)
+        .value("eInteger", BrxMechanicalPropertyType::eInteger)
+        .value("eReal", BrxMechanicalPropertyType::eReal)
+        .value("eBoolean", BrxMechanicalPropertyType::eBoolean)
+        .value("eString", BrxMechanicalPropertyType::eString)
+        .export_values()
+        ;
 }
+
 
 void initPyBrxModule()
 {
