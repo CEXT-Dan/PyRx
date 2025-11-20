7a0d1bd8ca9133cdf6e1e45621f871f92717622b
Daniel <dwmcabinets@yahoo.com>
2025-11-20 09:07:27 +0800

    [#432] PyBrxConstraintArgument

--- PyRxCore/PyBrxConstraints3d.h
+++ PyRxCore/PyBrxConstraints3d.h
@@ -2,4 +2,27 @@
 
 #ifdef BRXAPP
 #include "AcConstraints3d.h"
+
+//---------------------------------------------------------------------
+//PyBrxConstraintArgument
+void makePyBrxConstraintArgument();
+
+class PyBrxConstraintArgument
+{
+public:
+    PyBrxConstraintArgument();
+    explicit PyBrxConstraintArgument(const PyDbFullSubentPath& path);
+    explicit PyBrxConstraintArgument(AcConstraintArgument::CoordinateSystemObject cs);
+    PyBrxConstraintArgument(const PyDbFullSubentPath& path,AcConstraintArgument::CoordinateSystemObject cs);
+
+    PyDbFullSubentPath path() const;
+    bool isCoordinateSystemObject() const;
+    AcConstraintArgument::CoordinateSystemObject coordinateSystemObject() const;
+
+    static std::string className();
+
+public:
+    AcConstraintArgument m_imp;
+};
+
 #endif
