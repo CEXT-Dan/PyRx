2ea836cb8c99396b044fe0f92d0e59caf8992582
Daniel <dwmcabinets@yahoo.com>
2025-12-09 21:49:38 +0800

    [#440] add test TestDocStringJson

---
+++ tests/test_doc_utils/test_docstring_json.py
@@ -0,0 +1,35 @@
+import json
+
+class TestDocStringJson:
+    def setup_class(self):
+        _path = "../pyrx/doc_utils/resources/docstrings.json"
+        self.json_data = {}
+        with open(_path, "r") as file:
+            data = json.load(file)
+            for item in data["rows"]:
+                self.json_data[item[0]] = item[1:]
+            
+    def lookupDocString(self, row: int):
+        return self.json_data[row][-1]
+
+    def lookupId(self, row: int):
+        return self.json_data[row][0]
+
+    def test_datbase_valid(self):
+        assert self.lookupDocString(12) == "Description"
+        assert self.lookupDocString(1035) == "Destructor."
+        assert self.lookupDocString(2024) == "Description"
+        assert self.lookupDocString(4025) == "Destructor."
+        assert self.lookupDocString(5033) == "Destructor."
+        assert self.lookupDocString(6026) == "Destructor."
+        assert self.lookupDocString(7069) == "Destructor."
+        assert self.lookupDocString(8030) == "Destructor."
+        assert self.lookupDocString(9079) == "Destructor."
+        assert self.lookupDocString(10187) == "Destructor."
+        assert self.lookupDocString(10187) == "Destructor."
+        assert self.lookupDocString(12056) == "Destructor."
+        assert self.lookupDocString(14117) == "Destructor."
+        assert self.lookupDocString(16123) == "Destructor."
+        assert self.lookupDocString(18573) == "Destructor."
+        assert self.lookupDocString(19132) == "Description"
+        assert self.lookupDocString(19139) == "TEST19139"
