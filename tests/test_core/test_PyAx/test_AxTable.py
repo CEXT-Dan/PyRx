7c17f633defc7fa339abf064592fa85044e91310
Daniel <dwmcabinets@yahoo.com>
2025-12-18 20:56:48 +0800

    fix test_AxTable.py for grx

--- tests/test_core/test_PyAx/test_AxTable.py
+++ tests/test_core/test_PyAx/test_AxTable.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pytest
+
 from pyrx import Ap, Ax, Ge, Db
 
 
@@ -8,14 +10,18 @@ class TestAxTable:
     def setup_class(self):
         self.axApp = Ap.Application.acadApplication()
         self.axDoc = self.axApp.activeDocument()
-
-    def test_create_table(self):
+        
+    def test_create_table_settext(self):
         axModel = self.axDoc.modelSpace()
         axTable = axModel.addTable(Ge.Point3d(0, 0, 0), 7, 5, 1, 5)
         axTable.setText(0, 0, "woowhoo")
-        axTable.setValue(1,0,0,Db.AcValue(2.1))
-        
         assert axTable.text(0, 0) == "woowhoo"
+
+    @pytest.mark.known_failure_GRX
+    def test_create_table_setvalue(self):
+        axModel = self.axDoc.modelSpace()
+        axTable = axModel.addTable(Ge.Point3d(0, 0, 0), 7, 5, 1, 5)
+        axTable.setValue(1,0,0,Db.AcValue(2.1))
         assert "2.1" in axTable.text(1, 0)
  
         
