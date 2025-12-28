c10d641c7b8872b7a1a22bd74d27493cf331da34
Daniel <dwmcabinets@yahoo.com>
2025-12-29 06:27:24 +0800

    [#443] mark test failures

--- tests/test_core/test_wx/test_wx.py
+++ tests/test_core/test_wx/test_wx.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pytest
 import wx
 
 from wx import xrc
@@ -17,46 +18,47 @@ def do_wx_CallAfter(val):
 do_wx_CallAfter(10)
 
 class TestWxPython:
-    def test_get_app(self):  # just test possible types
+    def test_get_app(self):  
         pyapp: wx.App = Ap.Application.wxApp()
         assert isinstance(pyapp, wx.PyApp)
         topWin: wx.Window = pyapp.TopWindow
         assert topWin != None
 
-    def test_get_hwnd(self):  # just test possible types
+    def test_get_hwnd(self):  
         pyapp: wx.App = Ap.Application.wxApp()
         assert isinstance(pyapp, wx.PyApp)
         topWin: wx.Window = pyapp.TopWindow
         assert topWin.Handle == Ap.Application.mainWnd()
         
-    def test_get_xrc(self):  # just test possible types
+    def test_get_xrc(self):  
         _xrc = xrc.XmlResource()
         assert _xrc != None
         
-    def test_get_wxapp(self):  # just test possible types
+    def test_get_wxapp(self):  
         pyapp: wx.App = wx.App.Get()
         assert isinstance(pyapp, wx.PyApp)
         topWin: wx.Window = pyapp.TopWindow
         assert topWin != None
 
-    def test_get_wxhwnd(self):  # just test possible types
+    def test_get_wxhwnd(self): 
         pyapp: wx.App = wx.App.Get()
         assert isinstance(pyapp, wx.PyApp)
         topWin: wx.Window = pyapp.TopWindow
         assert topWin.Handle == Ap.Application.mainWnd()
 
-    def test_get_wxappget(self):  # just test possible types
+    def test_get_wxappget(self):  
         pyapp: wx.App = wx.GetApp()
         assert isinstance(pyapp, wx.PyApp)
         topWin: wx.Window = pyapp.TopWindow
         assert topWin != None
 
-    def test_get_wxhwndget(self):  # just test possible types
+    def test_get_wxhwndget(self):  
         pyapp: wx.App = wx.GetApp()
         assert isinstance(pyapp, wx.PyApp)
         topWin: wx.Window = pyapp.TopWindow
         assert topWin.Handle == Ap.Application.mainWnd()
         
-    def test_wx_CallAfter(self):  # just test possible types
+    @pytest.mark.known_failure_IRX
+    def test_wx_CallAfter(self): 
         assert wx_CallAfter == 10
 
