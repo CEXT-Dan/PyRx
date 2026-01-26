c108e95e17821375d6c8aa3e0534a976e80bcb5c
Grzegorz <131259064+gswifort@users.noreply.github.com>
2026-01-26 11:37:24 +0100

    fix ``Ge.Matrix3d.__repr__()``

--- PyRxCore/PyAcGe.cpp
+++ PyRxCore/PyAcGe.cpp
@@ -1884,7 +1884,7 @@ static std::string AcGeMatrix3dToString(const AcGeMatrix3d& x)
 
 static std::string AcGeMatrix3dToStringRepr(const AcGeMatrix3d& x)
 {
-    return std::format("{0}.Matrix3d(({1},{2},{3},{4}),({5},{6},{7},{8}),({9},{10},{11},{12}),({12},{14},{15},{16}))",
+    return std::format("{0}.Matrix3d(({1},{2},{3},{4}),({5},{6},{7},{8}),({9},{10},{11},{12}),({13},{14},{15},{16}))",
         PyGeNamespace,
         x.entry[0][0], x.entry[0][1], x.entry[0][2], x.entry[0][3],
         x.entry[1][0], x.entry[1][1], x.entry[1][2], x.entry[1][3],
