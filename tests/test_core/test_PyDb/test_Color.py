eda5bc71c0d8e87f91c4f2b0f20fbbde88a3af02
Daniel <dwmcabinets@yahoo.com>
2026-02-01 08:49:59 +0800

    add test for short HTML color codes like #FFF

--- tests/test_core/test_PyDb/test_Color.py
+++ tests/test_core/test_PyDb/test_Color.py
@@ -4,45 +4,63 @@ import pytest
 
 from pyrx import Db, Ed
 
+
 class TestColor:
-   
+
     def test_Color_1(self):
+        # Standard format: #RRGGBB
         color = Db.Color("#FFA500")
         assert color.red() == 255
         assert color.green() == 165
         assert color.blue() == 0
-        
+
     def test_Color_2(self):
         color = Db.Color(1)
         assert color.isByACI() == True
-        color.colorIndex() ==1
-        
+        color.colorIndex() == 1
+
     def test_Color_3(self):
-        color = Db.Color(255,165,0)
+        color = Db.Color(255, 165, 0)
         assert color.isByACI() == False
         assert color.red() == 255
         assert color.green() == 165
         assert color.blue() == 0
 
-      
+    def test_Color_4(self):
+        # Standard format: #FFF
+        color = Db.Color("#0FA")
+        assert color.red() == 0
+        assert color.green() == 255
+        assert color.blue() == 170
+        assert color.isByACI() == False
+
 
 class TestEntityColor:
-   
+
     def test_Color_1(self):
+        # Standard format: #RRGGBB
         color = Db.EntityColor("#FFA500")
         assert color.isByACI() == False
         assert color.red() == 255
         assert color.green() == 165
         assert color.blue() == 0
-        
+
     def test_Color_2(self):
         color = Db.EntityColor(1)
         assert color.isByACI() == True
-        color.colorIndex() ==1
-        
+        color.colorIndex() == 1
+
     def test_Color_3(self):
-        color = Db.EntityColor(255,165,0)
+        color = Db.EntityColor(255, 165, 0)
         assert color.isByACI() == False
         assert color.red() == 255
         assert color.green() == 165
         assert color.blue() == 0
+
+    def test_Color_4(self):
+        # Standard format: #FFF
+        color = Db.EntityColor("#0FA")
+        assert color.red() == 0
+        assert color.green() == 255
+        assert color.blue() == 170
+        assert color.isByACI() == False
