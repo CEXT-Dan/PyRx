779bec6b065aab4c3e4f036650314f01ff45f39e
Daniel <dwmcabinets@yahoo.com>
2026-02-02 15:25:41 +0800

    update test

--- tests/test_core/test_PyDb/test_Color.py
+++ tests/test_core/test_PyDb/test_Color.py
@@ -16,11 +16,13 @@ class TestColor:
         assert color.toHTMLColor() == "#FFA500"
 
     def test_Color_2(self):
+        # color index
         color = Db.Color(1)
         assert color.isByACI() == True
-        color.colorIndex() == 1
+        assert color.colorIndex() == 1
 
     def test_Color_3(self):
+        # r,g,b
         color = Db.Color(255, 165, 0)
         assert color.isByACI() == False
         assert color.red() == 255
@@ -29,7 +31,7 @@ class TestColor:
         assert color.toHTMLColor() == "#FFA500"
 
     def test_Color_4(self):
-        # Standard format: #FFF
+        # Short format: #FFF
         color = Db.Color("#0FA")
         assert color.red() == 0
         assert color.green() == 255
@@ -38,14 +40,16 @@ class TestColor:
         assert color.toHTMLColor() == "#00FFAA"
 
     def test_Color_5(self):
+        # Standard format: #RRGGBB, leading 00
         color = Db.Color("#00FFAA")
         assert color.red() == 0
         assert color.green() == 255
         assert color.blue() == 170
         assert color.isByACI() == False
         assert color.toHTMLColor() == "#00FFAA"
-        
+
     def test_Color_set(self):
+        # Standard format, get set
         color = Db.Color()
         color.fromHTMLColor("#00FFAA")
         assert color.red() == 0
@@ -53,7 +57,6 @@ class TestColor:
         assert color.blue() == 170
         assert color.isByACI() == False
         assert color.toHTMLColor() == "#00FFAA"
-
 
 
 class TestEntityColor:
@@ -70,7 +73,7 @@ class TestEntityColor:
     def test_Color_2(self):
         color = Db.EntityColor(1)
         assert color.isByACI() == True
-        color.colorIndex() == 1
+        assert color.colorIndex() == 1
 
     def test_Color_3(self):
         color = Db.EntityColor(255, 165, 0)
@@ -96,7 +99,7 @@ class TestEntityColor:
         assert color.blue() == 170
         assert color.isByACI() == False
         assert color.toHTMLColor() == "#00FFAA"
-        
+
     def test_Color_set(self):
         color = Db.EntityColor()
         color.fromHTMLColor("#00FFAA")
@@ -105,4 +108,3 @@ class TestEntityColor:
         assert color.blue() == 170
         assert color.isByACI() == False
         assert color.toHTMLColor() == "#00FFAA"
-
