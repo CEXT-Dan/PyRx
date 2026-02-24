5a7c132e5e08f66e0f42682bda492b14dff0c339
Daniel <dwmcabinets@yahoo.com>
2026-02-24 20:11:34 +0800

    [#463] add test_AxLeader

---
+++ tests/test_core/test_PyAx/test_AxLeader.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+import pytest
+
+from pyrx import Ap, Ax, Ge
+
+
+class TestALeader:
+
+    def setup_class(self):
+        self.axApp = Ap.Application.acadApplication()
+        self.axDoc = self.axApp.activeDocument()
+        
+    def test_create(self):
+        axSpace = self.axDoc.modelSpace()
+        mtextObj = axSpace.addMText(Ge.Point3d(10,10,0),2,"PyRx ActiveX Leader")
+        leaderPoints = [
+            Ge.Point3d(0.0, 0.0, 0.0),    # Arrowhead point
+            Ge.Point3d(4.0, 4.0, 0.0),    # Elbow point
+            Ge.Point3d(10.0, 10.0, 0.0)   # Connection to text
+        ]
+        leaderObj = axSpace.addLeader(leaderPoints, mtextObj, Ax.AcLeaderType.acLineWithArrow)
+        leaderObj.update()
+        mt = leaderObj.annotation()
+        assert mt.objectId() == mtextObj.objectId()
+        assert len(leaderObj.coordinates()) ==len(leaderPoints)
+        assert leaderObj.coordinate(0) == leaderPoints[0]
+        assert leaderObj.coordinate(1) == leaderPoints[1]
+        assert leaderObj.leaderType() == Ax.AcLeaderType.acLineWithArrow
