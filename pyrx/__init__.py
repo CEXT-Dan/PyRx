6da361f676cb25be7e0b305caad982afd57b31c7
Daniel <dwmcabinets@yahoo.com>
2026-02-26 09:23:04 +0800

    v2.2.46

--- pyrx/__init__.py
+++ pyrx/__init__.py
@@ -24,7 +24,7 @@ import importlib.util
 import warnings
 from typing import TYPE_CHECKING
 
-__version__ = "2.2.45.5274"
+__version__ = "2.2.46.5276"
 
 try:
     import PyRx as Rx  # isort: skip  # type: ignore
--- tests/test_core/test_PyAx/test_AxLine.py
+++ tests/test_core/test_PyAx/test_AxLine.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
+
 import math
-import pytest
+
 from pyrx import Ap, Ax, Ge
 
 
