f9c0781e68fac8f895eea912276086275dcebbe0
Daniel <dwmcabinets@yahoo.com>
2026-03-09 09:57:40 +0800

    v2.2.48

--- pyrx/__init__.py
+++ pyrx/__init__.py
@@ -24,7 +24,7 @@ import importlib.util
 import warnings
 from typing import TYPE_CHECKING
 
-__version__ = "2.2.47.5312"
+__version__ = "2.2.48.5317"
 
 try:
     import PyRx as Rx  # isort: skip  # type: ignore
