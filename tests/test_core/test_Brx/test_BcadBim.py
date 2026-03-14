889fd5c9e647dd6e28c0ccf3fe194d89efe46630
Daniel <dwmcabinets@yahoo.com>
2026-03-14 09:48:39 +0800

    add bim tests

---
+++ tests/test_core/test_Brx/test_BcadBim.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+import pytest
+
+from pyrx import Cv, Db, Bim
+
+
+@pytest.mark.known_failure_IRX
+@pytest.mark.known_failure_ARX
+@pytest.mark.known_failure_GRX
+@pytest.mark.known_failure_ZRX
+class TestBCadBim:
+
+    def test_allObjectBuildings(self, db_bim_main: Db.Database):
+        buldings = Bim.BimBuilding.allObjectBuildings(db_bim_main)
+        assert len(buldings) != 0
+
+       
