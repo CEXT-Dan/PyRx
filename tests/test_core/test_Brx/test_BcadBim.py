7edab360ed28e7c0943806038fae9beb7a6f172e
Daniel <dwmcabinets@yahoo.com>
2026-03-14 09:51:31 +0800

    improve TestBCadBim.test_allObjectBuildings

--- tests/test_core/test_Brx/test_BcadBim.py
+++ tests/test_core/test_Brx/test_BcadBim.py
@@ -14,5 +14,7 @@ class TestBCadBim:
     def test_allObjectBuildings(self, db_bim_main: Db.Database):
         buldings = Bim.BimBuilding.allObjectBuildings(db_bim_main)
         assert len(buldings) != 0
+        assert isinstance(buldings[0], Bim.BimBuilding)
+
 
        
