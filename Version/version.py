4400a94a9ff26ba4a87da5fbd6b67490f14087a2
Daniel <dwmcabinets@yahoo.com>
2026-03-16 18:02:47 +0800

    v2.2.49

--- Version/version.py
+++ Version/version.py
@@ -5,7 +5,7 @@ from setuptools_scm import ScmVersion, get_version
 
 MAJOR = 2
 MINOR = 2
-REVISION= 48
+REVISION= 49
 
 BASE_DIR = Path(__file__).parent
 REPO_DIR = BASE_DIR / ".."
--- pyrx/__init__.py
+++ pyrx/__init__.py
@@ -24,7 +24,7 @@ import importlib.util
 import warnings
 from typing import TYPE_CHECKING
 
-__version__ = "2.2.48.5320"
+__version__ = "2.2.49.5339"
 
 try:
     import PyRx as Rx  # isort: skip  # type: ignore
