3bed47af3bae8ec664db798886685a712196379f
Daniel <dwmcabinets@yahoo.com>
2026-05-30 07:33:40 +0800

    cleanup party

---
+++ tests/test_doc_utils/test_stubs/resources/stubs_003.pyi
@@ -0,0 +1,8 @@
+# /* This is a comment that should be ignored by the generator */
+
+import builtins  # noqa
+
+# pyrx-marker: HEADER_END
+
+class Dummy:
+    pass
---
+++ tests/test_doc_utils/test_stubs/resources/stubs_004.pyi
@@ -0,0 +1,17 @@
+# /*
+# This is comment that should be ignored by the generator
+# */
+
+import builtins  # noqa
+
+# pyrx-marker: HEADER_END
+
+class Class:
+    def meth(self):
+        pass
+
+# /* This is comment that should be ignored by the generator */
+
+def func(): ...
+
+# /* This is comment that should be ignored by the generator */
