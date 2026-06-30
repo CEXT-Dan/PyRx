3984c8ae69edd1398132e35daa23b85f24dd04cb
Daniel <dwmcabinets@yahoo.com>
2026-06-30 21:23:17 +0800

    add Application::refreshMainWindow

--- PyRxCore/PyApApplication.h
+++ PyRxCore/PyApApplication.h
@@ -44,6 +44,7 @@ public:
     static void                 appregcommand(const std::string& fullpath, const std::string& modulename, const std::string& name, const boost::python::object& func, int flags);
     static void                 apremovecommand(const std::string& modulename, const std::string& name);
     static int                  getInvisibleBorderWidth(UINT_PTR hwnd);
+    static void                 refreshMainWindow();
 
     static boost::python::list  listFilesInPath(const std::string& spath, const std::string& ext);
     static boost::python::list  listFilesInPathRecursive(const std::string& spath, const std::string& ext);
