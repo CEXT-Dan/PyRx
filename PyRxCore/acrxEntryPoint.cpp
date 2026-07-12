a0c0e4bb373c85e3e3ea621811ee28f3f154af33
Daniel <dwmcabinets@yahoo.com>
2026-07-12 23:23:40 +0800

    test

--- PyRxCore/acrxEntryPoint.cpp
+++ PyRxCore/acrxEntryPoint.cpp
@@ -490,8 +490,181 @@ public:
         return std::make_tuple(res, id, pnt);
     }
 
+    static void setTitleThemeDark(HWND hwnd)
+    {
+        constexpr DWORD DWMWA_USE_IMMERSIVE_DARK_MODE_I20 = 20;
+        BOOL USE_DARK_MODE = true;
+        DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE_I20, &USE_DARK_MODE, sizeof(USE_DARK_MODE));
+        const auto style = GetWindowLong(hwnd, GWL_STYLE);
+        SetWindowLong(hwnd, GWL_STYLE, 0);
+        SetWindowLong(hwnd, GWL_STYLE, style);
+    }
+
+    static void PerformDarkMenuPaint(HWND hTargetWnd, RECT* pMenuBarStripOut = nullptr)
+    {
+        HDC hdc = GetWindowDC(hTargetWnd);
+        if (!hdc) return;
+
+        MENUBARINFO mbi = { 0 };
+        mbi.cbSize = sizeof(MENUBARINFO);
+
+        if (GetMenuBarInfo(hTargetWnd, OBJID_MENU, 0, &mbi))
+        {
+            RECT rcWindow;
+            GetWindowRect(hTargetWnd, &rcWindow);
+            int winWidth = rcWindow.right - rcWindow.left;
+
+            RECT rcMenuBarStrip;
+            rcMenuBarStrip.left = 0;
+            rcMenuBarStrip.right = winWidth;
+            rcMenuBarStrip.top = mbi.rcBar.top - rcWindow.top;
+            rcMenuBarStrip.bottom = mbi.rcBar.bottom - rcWindow.top;
+
+            // Applying your perfect layout alignment offsets
+            rcMenuBarStrip.top += 0;
+            rcMenuBarStrip.bottom += 2;
+
+            if (pMenuBarStripOut) {
+                *pMenuBarStripOut = rcMenuBarStrip;
+            }
+
+            // 1. Draw solid dark background canvas
+            HBRUSH hDarkBrush = CreateSolidBrush(RGB(30, 30, 30));
+            FillRect(hdc, &rcMenuBarStrip, hDarkBrush);
+            DeleteObject(hDarkBrush);
+
+            // 2. Re-render structural strings centered over their true hit-test targets
+            HMENU hMenu = GetMenu(hTargetWnd);
+            if (hMenu)
+            {
+                int count = GetMenuItemCount(hMenu);
+                SetBkMode(hdc, TRANSPARENT);
+                SetTextColor(hdc, RGB(240, 240, 240)); // Clean Off-White
+
+                HFONT hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+                HFONT hOldFont = (HFONT)SelectObject(hdc, hFont);
+
+                for (int i = 0; i < count; i++)
+                {
+                    TCHAR szMenuText[128];
+                    GetMenuString(hMenu, i, szMenuText, 128, MF_BYPOSITION);
+
+                    if (_tcslen(szMenuText) == 0) continue;
+
+                    RECT rcItemScreen;
+                    if (GetMenuItemRect(hTargetWnd, hMenu, i, &rcItemScreen))
+                    {
+                        RECT rcTextSlot;
+                        rcTextSlot.left = rcItemScreen.left - rcWindow.left;
+                        rcTextSlot.top = rcMenuBarStrip.top;
+                        rcTextSlot.right = rcItemScreen.right - rcWindow.left;
+                        rcTextSlot.bottom = rcMenuBarStrip.bottom;
+
+                        DrawText(hdc, szMenuText, -1, &rcTextSlot, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+                    }
+                }
+                SelectObject(hdc, hOldFont);
+            }
+        }
+        ReleaseDC(hTargetWnd, hdc);
+    }
+
+    static LRESULT CALLBACK DarkMenuBarSubclassProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+        UINT_PTR uIdSubclass, DWORD_PTR dwRefData)
+    {
+        // Capture core draw and window focus events
+        if (uMsg == WM_NCPAINT || uMsg == WM_NCACTIVATE || uMsg == WM_MENUSELECT ||
+            uMsg == WM_ENTERMENULOOP || uMsg == WM_EXITMENULOOP || uMsg == WM_MOUSEMOVE)
+        {
+            LRESULT res = DefSubclassProc(hWnd, uMsg, wParam, lParam);
+            PerformDarkMenuPaint(hWnd);
+            return res;
+        }
+
+        // Intercept mouse glide globally across the bar to wipe hot-track artifacts
+        if (uMsg == WM_NCMOUSEMOVE)
+        {
+            if (wParam == HTMENU)
+            {
+                PerformDarkMenuPaint(hWnd);
+                return 0;
+            }
+        }
+
+        // Keep background solid dark when empty bar areas are clicked
+        if (uMsg == WM_NCLBUTTONDOWN || uMsg == WM_NCLBUTTONDBLCLK)
+        {
+            if (wParam == HTMENU)
+            {
+                LRESULT res = DefSubclassProc(hWnd, uMsg, wParam, lParam);
+                PerformDarkMenuPaint(hWnd);
+                return res;
+            }
+        }
+
+        // Stop Windows from executing the default high-priority light hover overlay loop
+        if (uMsg == WM_SETCURSOR)
+        {
+            if (LOWORD(lParam) == HTMENU)
+            {
+                PerformDarkMenuPaint(hWnd);
+                return TRUE;
+            }
+        }
+
+        if (uMsg == WM_NCHITTEST)
+        {
+            return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+        }
+
+        if (uMsg == WM_NCDESTROY)
+        {
+            RemoveWindowSubclass(hWnd, DarkMenuBarSubclassProc, uIdSubclass);
+        }
+
+        return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    static void InitializeDarkMenuBar()
+    {
+        enum PreferredAppMode { Default = 0, AllowDark = 1, ForceDark = 2, ForceLight = 3, Max = 4 };
+        typedef bool (WINAPI* fnAllowDarkModeForWindow)(HWND hWnd, bool allow);
+        typedef PreferredAppMode(WINAPI* fnSetPreferredAppMode)(PreferredAppMode appMode);
+        typedef void (WINAPI* fnFlushMenuThemes)();
+
+        HMODULE hUxTheme = GetModuleHandle(_T("uxtheme.dll"));
+        if (!hUxTheme) return;
+
+        auto SetPreferredAppMode = (fnSetPreferredAppMode)GetProcAddress(hUxTheme, MAKEINTRESOURCEA(135));
+        auto AllowDarkModeForWindow = (fnAllowDarkModeForWindow)GetProcAddress(hUxTheme, MAKEINTRESOURCEA(133));
+        auto FlushMenuThemes = (fnFlushMenuThemes)GetProcAddress(hUxTheme, MAKEINTRESOURCEA(136));
+
+        // Force dropdowns dark using the hidden Win32 APIs
+        if (SetPreferredAppMode && AllowDarkModeForWindow && FlushMenuThemes)
+        {
+            SetPreferredAppMode(PreferredAppMode::ForceDark);
+
+            HWND hMainWnd = adsw_acadMainWnd(); // BricsCAD MFC Frame Window
+            if (hMainWnd != NULL)
+            {
+                AllowDarkModeForWindow(hMainWnd, true);
+                FlushMenuThemes();
+
+                // Inject the window subclass hook to fix the top horizontal menu strip
+                // The parameter '1' is a unique Identifier for this specific subclass hook
+                SetWindowSubclass(hMainWnd, DarkMenuBarSubclassProc, 1, 0);
+
+                // Force a total window repaint to trigger the new paint sequence instantly
+                SetWindowPos(hMainWnd, NULL, 0, 0, 0, 0,
+                    SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+            }
+        }
+    }
+
     static void AcRxPyApp_idoit1(void)
     {
+        setTitleThemeDark(adsw_acadMainWnd());
+        InitializeDarkMenuBar();
     }
 #endif
 };
