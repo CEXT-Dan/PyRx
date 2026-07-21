61d47216f621b2ebde816af97df9fc2cdc768e94
Daniel <dwmcabinets@yahoo.com>
2026-07-21 08:05:01 +0800

    force actions

--- PyRxLoader/acrxEntryPoint.cpp
+++ PyRxLoader/acrxEntryPoint.cpp
@@ -28,7 +28,7 @@
 //-----------------------------------------------------------------------------
 #define szRDS _RXST("")
 
-//FORCEBUILD = 22
+//FORCEBUILD = 23
 constexpr const wchar_t* PATHENV = _T("PATH");
 constexpr const wchar_t* PYTHONNAME = _T("python314");
 constexpr const wchar_t* PYTHONDLLNAME = _T("python314.dll");
